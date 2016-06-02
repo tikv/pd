4af9b64c4d3d01aafd615d1aacb52df267771280
disksing <i@disksing.com>
2016-06-02 10:20:54 +0800

    *: update deps

--- server/leader.go
+++ server/leader.go
@@ -180,7 +180,7 @@ func (s *Server) watchLeader() {
 			}
 
 			for _, ev := range wresp.Events {
-				if ev.Type == mvccpb.EXPIRE || ev.Type == mvccpb.DELETE {
+				if ev.Type == mvccpb.DELETE {
 					log.Info("leader is expired or deleted")
 					return
 				}
