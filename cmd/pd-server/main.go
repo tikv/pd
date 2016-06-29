cba6d4e253f12be37eea3fa76ca3c1b30a0723fe
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2016-06-29 09:46:52 +0800

    *: add config file support.

--- cmd/pd-server/main.go
+++ cmd/pd-server/main.go
@@ -19,7 +19,6 @@ import (
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/juju/errors"
@@ -29,49 +28,74 @@ import (
 )
 
 var (
-	addr            = flag.String("addr", "127.0.0.1:1234", "server listening address")
-	advertiseAddr   = flag.String("advertise-addr", "", "server advertise listening address [127.0.0.1:1234] for client communication")
-	etcdAddrs       = flag.String("etcd", "127.0.0.1:2379", "Etcd endpoints, separated by comma")
-	rootPath        = flag.String("root", "/pd", "pd root path in etcd")
-	leaderLease     = flag.Int64("lease", 3, "leader lease time (second)")
-	logLevel        = flag.String("L", "debug", "log level: info, debug, warn, error, fatal")
-	httpAddr        = flag.String("http-addr", ":9090", "http server listening address")
-	pprofAddr       = flag.String("pprof", ":6060", "pprof HTTP listening address")
-	clusterID       = flag.Uint64("cluster-id", 0, "cluster ID")
-	maxPeerCount    = flag.Uint("max-peer-count", 3, "max peer count for the region")
-	metricAddr      = flag.String("metric-addr", "", "StatsD metric address")
-	metricPrefix    = flag.String("metric-prefix", "pd", "metric prefix")
-	minCapUsedRatio = flag.Float64("min-capacity-used-ratio", 0.4, "min capacity used ratio for choosing store in balance")
-	maxCapUsedRatio = flag.Float64("max-capacity-used-ratio", 0.9, "max capacity used ratio for choosing store in balance")
+	config                 = flag.String("c", "", "config file")
+	addr                   = flag.String("addr", "127.0.0.1:1234", "server listening address")
+	advertiseAddr          = flag.String("advertise-addr", "", "server advertise listening address [127.0.0.1:1234] for client communication")
+	etcdAddrs              = flag.String("etcd-addrs", "127.0.0.1:2379", "Etcd endpoints, separated by comma")
+	httpAddr               = flag.String("http-addr", ":9090", "http server listening address")
+	pprofAddr              = flag.String("pprof-addr", ":6060", "pprof HTTP listening address")
+	rootPath               = flag.String("root", "/pd", "pd root path in etcd")
+	leaderLease            = flag.Int64("lease", 3, "leader lease time (second)")
+	logLevel               = flag.String("log-level", "debug", "log level: info, debug, warn, error, fatal")
+	tsoSaveInterval        = flag.Int64("tso-save-interval", 2000, "the interval time (ms) to save timestamp")
+	clusterID              = flag.Uint64("cluster-id", 0, "cluster ID")
+	maxPeerCount           = flag.Uint64("max-peer-count", 3, "max peer count for the region")
+	metricAddr             = flag.String("metric-addr", "", "StatsD metric address")
+	metricPrefix           = flag.String("metric-prefix", "pd", "metric prefix")
+	minCapUsedRatio        = flag.Float64("min-capacity-used-ratio", 0.4, "min capacity used ratio for choosing store in balance")
+	maxCapUsedRatio        = flag.Float64("max-capacity-used-ratio", 0.9, "max capacity used ratio for choosing store in balance")
+	maxSnapSendCount       = flag.Uint64("max-snap-sending-count", 3, "max sending snapshot count for choosing store in balance")
+	maxSnapRecvCount       = flag.Uint64("max-snap-receiving-count", 3, "max receiving snapshot count for choosing store in balance")
+	maxDiffScoreFrac       = flag.Float64("max-diff-score-fraction", 0.1, "max diff score fraction for choosing store in balance")
+	balanceInterval        = flag.Uint64("balance-interval", 30, "the interval time (s) to do balance")
+	maxBalanceCount        = flag.Uint64("max-balance-count", 16, "the max region count to balance at the same time")
+	maxBalanceRetryPerLoop = flag.Uint64("max-balance-retry-per-loop", 10, "the max retry count to balance in a balance schedule")
+	maxBalanceCountPerLoop = flag.Uint64("max-balance-count-per-loop", 3, "the max region count to balance in a balance schedule")
 )
 
 func main() {
 	flag.Parse()
 
-	if *clusterID == 0 {
-		log.Warn("cluster id is 0, don't use it in production")
+	cfg := &server.Config{}
+	flag.Visit(cfg.VisitArgs)
+
+	if *config != "" {
+		if err := cfg.LoadFromFile(*config); err != nil {
+			log.Fatalf("load config failed - %s", err)
+		}
+
+		log.Infof("PD init config - %v", cfg)
 	}
 
-	log.SetLevelByString(*logLevel)
+	cfg.SetStringFlagConfig(&cfg.Addr, "addr", *addr)
+	cfg.SetStringFlagConfig(&cfg.AdvertiseAddr, "advertise-addr", *advertiseAddr)
+	cfg.SetStringSliceFlagConfig(&cfg.EtcdAddrs, "etcd", *etcdAddrs)
+	cfg.SetStringFlagConfig(&cfg.HTTPAddr, "http-addr", *httpAddr)
+	cfg.SetStringFlagConfig(&cfg.PprofAddr, "pprof", *pprofAddr)
+	cfg.SetStringFlagConfig(&cfg.RootPath, "root", *rootPath)
+	cfg.SetIntFlagConfig(&cfg.LeaderLease, "lease", *leaderLease)
+	cfg.SetStringFlagConfig(&cfg.LogLevel, "L", *logLevel)
+	cfg.SetIntFlagConfig(&cfg.TsoSaveInterval, "tso-save-interval", *tsoSaveInterval)
+	cfg.SetUintFlagConfig(&cfg.ClusterID, "cluster-id", *clusterID)
+	cfg.SetUintFlagConfig(&cfg.MaxPeerCount, "max-peer-count", *maxPeerCount)
+	cfg.SetStringFlagConfig(&cfg.MetricAddr, "metric-addr", *metricAddr)
+	cfg.SetFloatFlagConfig(&cfg.BCfg.MinCapacityUsedRatio, "min-capacity-used-ratio", *minCapUsedRatio)
+	cfg.SetFloatFlagConfig(&cfg.BCfg.MaxCapacityUsedRatio, "max-capacity-used-ratio", *maxCapUsedRatio)
+	cfg.SetUintFlagConfig(&cfg.BCfg.MaxSnapSendingCount, "max-snap-sending-count", *maxSnapSendCount)
+	cfg.SetUintFlagConfig(&cfg.BCfg.MaxSnapReceivingCount, "max-snap-receiving-count", *maxSnapRecvCount)
+	cfg.SetFloatFlagConfig(&cfg.BCfg.MaxDiffScoreFraction, "max-diff-score-fraction", *maxDiffScoreFrac)
+	cfg.SetUintFlagConfig(&cfg.BCfg.BalanceInterval, "balance-interval", *balanceInterval)
+	cfg.SetUintFlagConfig(&cfg.BCfg.MaxBalanceCount, "max-balance-count", *maxBalanceCount)
+	cfg.SetUintFlagConfig(&cfg.BCfg.MaxBalanceRetryPerLoop, "max-balance-retry-per-loop", *maxBalanceRetryPerLoop)
+	cfg.SetUintFlagConfig(&cfg.BCfg.MaxBalanceCountPerLoop, "max-balance-count-per-loop", *maxBalanceCountPerLoop)
+
+	log.SetLevelByString(cfg.LogLevel)
+
+	log.Infof("PD config - %v", cfg)
 
 	go func() {
-		http.ListenAndServe(*pprofAddr, nil)
+		http.ListenAndServe(cfg.PprofAddr, nil)
 	}()
-
-	cfg := &server.Config{
-		Addr:                 *addr,
-		HTTPAddr:             *httpAddr,
-		AdvertiseAddr:        *advertiseAddr,
-		EtcdAddrs:            strings.Split(*etcdAddrs, ","),
-		RootPath:             *rootPath,
-		LeaderLease:          *leaderLease,
-		ClusterID:            *clusterID,
-		MaxPeerCount:         uint32(*maxPeerCount),
-		MetricAddr:           *metricAddr,
-		MetricPrefix:         *metricPrefix,
-		MinCapacityUsedRatio: *minCapUsedRatio,
-		MaxCapacityUsedRatio: *maxCapUsedRatio,
-	}
 
 	svr, err := server.NewServer(cfg)
 	if err != nil {
--- server/config.go
+++ server/config.go
@@ -13,97 +13,232 @@
 
 package server
 
-import "time"
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
 
-const (
-	defaultRootPath        = "pd"
-	defaultLeaderLease     = 3
-	defaultTsoSaveInterval = 2000
-	defaultNextRetryDelay  = time.Second
-	defaultMaxPeerCount    = uint32(3)
-	defaultMetrixPrefix    = "pd"
+	"github.com/BurntSushi/toml"
+	"github.com/juju/errors"
 )
 
 // Config is the pd server configuration.
 type Config struct {
 	// Server listening address.
-	Addr string
-
-	// HTTP server listening address.
-	HTTPAddr string
+	Addr string `toml:"addr"`
 
 	// Server advertise listening address for outer client communication.
 	// If not set, using default Addr instead.
-	AdvertiseAddr string
+	AdvertiseAddr string `toml:"advertise-addr"`
 
 	// Etcd endpoints
-	EtcdAddrs []string
+	EtcdAddrs []string `toml:"etcd-addrs"`
+
+	// HTTP server listening address.
+	HTTPAddr string `toml:"http-addr"`
+
+	// Pprof listening address.
+	PprofAddr string `toml:"pprof-addr"`
 
 	// RootPath in Etcd as the prefix for all keys. If not set, use default "pd".
-	RootPath string
+	RootPath string `toml:"root"`
 
 	// LeaderLease time, if leader doesn't update its TTL
 	// in etcd after lease time, etcd will expire the leader key
 	// and other servers can campaign the leader again.
 	// Etcd onlys support seoncds TTL, so here is second too.
-	LeaderLease int64
+	LeaderLease int64 `toml:"lease"`
+
+	// Log level.
+	LogLevel string `toml:"log-level"`
 
 	// TsoSaveInterval is the interval time (ms) to save timestamp.
 	// When the leader begins to run, it first loads the saved timestamp from etcd, e.g, T1,
 	// and the leader must guarantee that the next timestamp must be > T1 + 2 * TsoSaveInterval.
-	TsoSaveInterval int64
+	TsoSaveInterval int64 `toml:"tso-save-interval"`
 
 	// ClusterID is the cluster ID communicating with other services.
-	ClusterID uint64
+	ClusterID uint64 `toml:"cluster-id"`
 
 	// MaxPeerCount for a region. default is 3.
-	MaxPeerCount uint32
+	MaxPeerCount uint64 `toml:"max-peer-count"`
 
 	// Remote metric address for StatsD.
-	MetricAddr string
+	MetricAddr string `toml:"metric-addr"`
 
 	// Metric prefix.
-	MetricPrefix string
+	MetricPrefix string `toml:"metric-prefix"`
 
-	// For capacity balance.
-	MinCapacityUsedRatio float64
-	MaxCapacityUsedRatio float64
+	BCfg *BalanceConfig `toml:"balance"`
 
 	// Only test can change it.
 	nextRetryDelay time.Duration
+
+	args map[string]bool
+}
+
+func (c *Config) VisitArgs(flag *flag.Flag) {
+	if c.args == nil {
+		c.args = make(map[string]bool)
+	}
+
+	c.args[flag.Name] = true
 }
 
 func (c *Config) adjust() {
 	if len(c.RootPath) == 0 {
-		c.RootPath = defaultRootPath
+		c.RootPath = "pd"
 	}
 	// TODO: Maybe we should do more check for root path, only allow letters?
 
 	if c.LeaderLease <= 0 {
-		c.LeaderLease = defaultLeaderLease
+		c.LeaderLease = 3
 	}
 
 	if c.TsoSaveInterval <= 0 {
-		c.TsoSaveInterval = defaultTsoSaveInterval
+		c.TsoSaveInterval = 2000
 	}
 
 	if c.nextRetryDelay == 0 {
-		c.nextRetryDelay = defaultNextRetryDelay
+		c.nextRetryDelay = time.Second
 	}
 
 	if c.MaxPeerCount == 0 {
-		c.MaxPeerCount = defaultMaxPeerCount
+		c.MaxPeerCount = 3
 	}
 
 	if len(c.MetricPrefix) == 0 {
-		c.MetricPrefix = defaultMetrixPrefix
+		c.MetricPrefix = "pd"
 	}
 
+	c.BCfg.adjust()
+}
+
+func (c *Config) String() string {
+	if c == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("Config(%+v)", *c)
+}
+
+// SetStringFlagConfig sets string flag value to config.
+func (c *Config) SetStringFlagConfig(dest *string, name string, value string) {
+	if c.args[name] {
+		*dest = value
+	}
+}
+
+// SetStringSliceFlagConfig sets string slice value to config.
+func (c *Config) SetStringSliceFlagConfig(dest *[]string, name string, value string) {
+	if c.args[name] {
+		*dest = append([]string{}, strings.Split(value, ",")...)
+	}
+}
+
+// SetIntFlagConfig sets int value to config.
+func (c *Config) SetIntFlagConfig(dest *int64, name string, value int64) {
+	if c.args[name] {
+		*dest = value
+	}
+}
+
+// SetUintFlagConfig sets uint value to config.
+func (c *Config) SetUintFlagConfig(dest *uint64, name string, value uint64) {
+	if c.args[name] {
+		*dest = value
+	}
+}
+
+// SetFloatFlagConfig sets float value to config.
+func (c *Config) SetFloatFlagConfig(dest *float64, name string, value float64) {
+	if c.args[name] {
+		*dest = value
+	}
+}
+
+// LoadFromFile loads config from file.
+func (c *Config) LoadFromFile(path string) error {
+	_, err := toml.DecodeFile(path, c)
+	return errors.Trace(err)
+}
+
+// BalanceConfig is the balance configuration.
+type BalanceConfig struct {
+	// For capacity balance.
+	// If the used ratio of one store is less than this value,
+	// it will never be used as a from store.
+	MinCapacityUsedRatio float64 `toml:"min-capacity-used-ratio"`
+	// If the used ratio of one store is greater than this value,
+	// it will never be used as a to store.
+	MaxCapacityUsedRatio float64 `toml:"max-capacity-used-ratio"`
+
+	// For snapshot balance filter.
+	// If the sending snapshot count of one storage is greater than this value,
+	// it will never be used as a from store.
+	MaxSnapSendingCount uint64 `toml:"max-snap-sending-count"`
+	// If the receiving snapshot count of one storage is greater than this value,
+	// it will never be used as a to store.
+	MaxSnapReceivingCount uint64 `toml:"max-snap-receiving-count"`
+
+	// If the new store and old store's diff scores are not beyond this value,
+	// the balancer will do nothing.
+	MaxDiffScoreFraction float64 `toml:"max-diff-score-fraction"`
+
+	// Balance loop interval time (seconds).
+	BalanceInterval uint64 `toml:"balance-interval"`
+
+	// MaxBalanceCount is the max region count to balance at the same time.
+	MaxBalanceCount uint64 `toml:"max-balance-count"`
+
+	// MaxBalanceRetryPerLoop is the max retry count to balance in a balance schedule.
+	MaxBalanceRetryPerLoop uint64 `toml:"max-balance-retry-per-loop"`
+
+	// MaxBalanceCountPerLoop is the max region count to balance in a balance schedule.
+	MaxBalanceCountPerLoop uint64 `toml:"max-balance-count-per-loop"`
+}
+
+func (c *BalanceConfig) adjust() {
 	if c.MinCapacityUsedRatio == 0 {
-		c.MinCapacityUsedRatio = minCapacityUsedRatio
+		c.MinCapacityUsedRatio = 0.4
 	}
 
 	if c.MaxCapacityUsedRatio == 0 {
-		c.MaxCapacityUsedRatio = maxCapacityUsedRatio
+		c.MaxCapacityUsedRatio = 0.9
 	}
+
+	if c.MaxSnapSendingCount == 0 {
+		c.MaxSnapSendingCount = 3
+	}
+
+	if c.MaxSnapReceivingCount == 0 {
+		c.MaxSnapReceivingCount = 3
+	}
+
+	if c.MaxDiffScoreFraction == 0 {
+		c.MaxDiffScoreFraction = 0.1
+	}
+
+	if c.BalanceInterval == 0 {
+		c.BalanceInterval = 30
+	}
+
+	if c.MaxBalanceCount == 0 {
+		c.MaxBalanceCount = 16
+	}
+
+	if c.MaxBalanceRetryPerLoop == 0 {
+		c.MaxBalanceRetryPerLoop = 10
+	}
+
+	if c.MaxBalanceCountPerLoop == 0 {
+		c.MaxBalanceCountPerLoop = 3
+	}
+}
+
+func (c *BalanceConfig) String() string {
+	if c == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("BalanceConfig(%+v)", *c)
 }
