22dd34d44de68b862563e80b7b430360baba3499
nolouch <nolouch@gmail.com>
2017-03-20 00:12:31 +0800

    pd-ctl: add config replicate and show all config

--- pdctl/command/config_command.go
+++ pdctl/command/config_command.go
@@ -17,15 +17,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	configPrefix   = "pd/api/v1/config"
-	schedulePrefix = "pd/api/v1/config/schedule"
+	configPrefix    = "pd/api/v1/config"
+	schedulePrefix  = "pd/api/v1/config/schedule"
+	replicatePrefix = "pd/api/v1/config/replicate"
 )
 
 // NewConfigCommand return a config subcommand of rootCmd
@@ -46,6 +49,17 @@ func NewShowConfigCommand() *cobra.Command {
 		Short: "show config of PD",
 		Run:   showConfigCommandFunc,
 	}
+	sc.AddCommand(NewShowAllConfigCommand())
+	return sc
+}
+
+// NewShowAllConfigCommand return a show all subcommand of show subcommand
+func NewShowAllConfigCommand() *cobra.Command {
+	sc := &cobra.Command{
+		Use:   "all",
+		Short: "show all config of PD",
+		Run:   showAllConfigCommandFunc,
+	}
 	return sc
 }
 
@@ -56,11 +70,31 @@ func NewSetConfigCommand() *cobra.Command {
 		Short: "set the option with value",
 		Run:   setConfigCommandFunc,
 	}
+	sc.AddCommand(NewSetReplicationCommand())
+	return sc
+}
+
+// NewSetReplicationCommand return  a set replication subcommand of setCmd
+func NewSetReplicationCommand() *cobra.Command {
+	sc := &cobra.Command{
+		Use:   "replicate <option> <value>",
+		Short: "set the replication option with value",
+		Run:   setReplicateConfigCommandFunc,
+	}
 	return sc
 }
 
 func showConfigCommandFunc(cmd *cobra.Command, args []string) {
 	r, err := doRequest(cmd, schedulePrefix, http.MethodGet)
+	if err != nil {
+		fmt.Printf("Failed to get config: %s", err)
+		return
+	}
+	fmt.Println(r)
+}
+
+func showAllConfigCommandFunc(cmd *cobra.Command, args []string) {
+	r, err := doRequest(cmd, configPrefix, http.MethodGet)
 	if err != nil {
 		fmt.Printf("Failed to get config: %s", err)
 		return
@@ -113,5 +147,52 @@ func setConfigCommandFunc(cmd *cobra.Command, args []string) {
 		fmt.Println("Success!")
 	} else {
 		printResponseError(r)
+	}
+}
+
+func setReplicateConfigCommandFunc(cmd *cobra.Command, args []string) {
+	if len(args) != 2 {
+		fmt.Println(cmd.UsageString())
+		return
+	}
+
+	var value interface{}
+	data := make(map[string]interface{})
+	r, err := doRequest(cmd, replicatePrefix, http.MethodGet)
+	if err != nil {
+		fmt.Printf("Failed to set replication config:[%s]\n", err)
+		return
+	}
+	err = json.Unmarshal([]byte(r), &data)
+	if err != nil {
+		fmt.Printf("Failed to set replication config:[%s]\n", err)
+		return
+	}
+	value, err = strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		value = args[1]
+		if strings.Contains(args[1], ",") {
+			value = strings.Split(args[1], ",")
+		}
+	}
+	data[args[0]] = value
+	reqData, err := json.Marshal(&data)
+	if err != nil {
+		fmt.Printf("Failed to set replication config:[%s]\n", err)
+		return
+	}
+	url := getAddressFromCmd(cmd, replicatePrefix)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqData))
+	if err != nil {
+		fmt.Printf("Failed to set replication config:[%s]\n", err)
+	}
+	defer func() {
+		ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+	}()
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("Success!")
+	} else {
+		printResponseError(resp)
 	}
 }
