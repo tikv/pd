86b7bb02e2e549f0d9aae0783699dd6bc319afa7
yulai.li <yulai.li@speedycloud.cn>
2017-09-07 14:00:16 +0800

    Refactor add threadSafeCache and remove Lock related code from LRU

--- server/cache/cache.go
+++ server/cache/cache.go
@@ -13,6 +13,10 @@
 
 package cache
 
+import (
+	"sync"
+)
+
 // Cache is an interface for cache system
 type Cache interface {
 	// Put puts an item into cache.
@@ -29,26 +33,83 @@ type Cache interface {
 	Len() int
 }
 
-type CacheType int
+// Type is cache's type such as LRUCache and etc.
+type Type int
 
 const (
-	LRUCache CacheType = 1
+	// LRUCache is LRU cache type
+	LRUCache Type = 1
 )
 
 var (
-	// DefaultCache set default cache type for NewDefaultCache function
-	DefaultCache = LRUCache
+	// DefaultCacheType set default cache type for NewDefaultCache function
+	DefaultCacheType = LRUCache
 )
 
-func NewCache(size int, cacheType CacheType) Cache {
+type threadSafeCache struct {
+	cache Cache
+	lock  sync.RWMutex
+}
+
+func newThreadSafeCache(cache Cache) Cache {
+	return &threadSafeCache{
+		cache: cache,
+	}
+}
+
+// Put puts an item into cache.
+func (c *threadSafeCache) Put(key uint64, value interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.cache.Put(key, value)
+}
+
+// Get retrives an item from cache.
+func (c *threadSafeCache) Get(key uint64) (interface{}, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return c.cache.Get(key)
+}
+
+// Peek reads an item from cache. The action is no considered 'Use'.
+func (c *threadSafeCache) Peek(key uint64) (interface{}, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return c.cache.Peek(key)
+}
+
+// Remove eliminates an item from cache.
+func (c *threadSafeCache) Remove(key uint64) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.cache.Remove(key)
+}
+
+// Elems return all items in cache.
+func (c *threadSafeCache) Elems() []*Item {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return c.cache.Elems()
+}
+
+// Len returns current cache size
+func (c *threadSafeCache) Len() int {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return c.cache.Len()
+}
+
+// NewCache create Cache instance by CacheType
+func NewCache(size int, cacheType Type) Cache {
 	switch cacheType {
 	case LRUCache:
-		return NewLRU(size)
+		return newThreadSafeCache(newLRU(size))
 	default:
 		panic("Unknown cache type")
 	}
 }
 
+// NewDefaultCache create Cache instance by default cache type
 func NewDefaultCache(size int) Cache {
-	return NewCache(size, DefaultCache)
+	return NewCache(size, DefaultCacheType)
 }
