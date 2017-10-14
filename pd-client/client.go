91937940b15ab5bc060f125b94f9d245df840830
disksing <i@disksing.com>
2017-10-14 13:00:29 +0800

    *: use buildin context.

--- pd-client/client.go
+++ pd-client/client.go
@@ -14,6 +14,7 @@
 package pd
 
 import (
+	"context"
 	"strings"
 	"sync"
 	"time"
@@ -22,7 +23,6 @@ import (
 	"github.com/juju/errors"
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/kvproto/pkg/pdpb"
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
