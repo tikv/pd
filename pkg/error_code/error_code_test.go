357ee010d83e8a3ca8b0d5c374d159e8fba530a8
Greg Weber <greg@gregweber.info>
2018-07-19 12:30:58 -0700

    error codes: add hierarchy
    Hierarchy is useful for documentation/classification purposes.
    The hierarchy can also be traversed to find find meta data on ancestors.
    This is now used in the HTTP Code implementation to inherit HTTP codes.
    
    Note that Previously HTTP codes were specified on the go error type, which
    doesn't make sense. Now HTTP codes are attached to an errcode.Code
    value via metadata.
    
    Previously I had already created error codes strings with a dot separation
    hiearchy. However, that was informal. Now that relationship is validated
    and modeled in code. The API still involves giving a dot separated hierachy.
    This requires some simple trivial parsing/validation in the implementation
    but made for a nicer API than specifying a struct with a parent link.
    
    Some inspiration here was taken from the upspin error codes which are
    classified by Kind. When I started adding kinds, I realized I just
    wanted to represent them as Codes, which naturally led to hierarchy.
    Kind classification can be though of as having many
    error hierarchies with a depth of two and a high branching factor.
    In our implementation the top level of the hierarchy can be used like a kind, but
    we can also create as much or little depth in the tree as we would like.
    https://commandcenter.blogspot.com/2017/12/error-handling-in-upspin.htm

--- pkg/error_code/error_code_test.go
+++ pkg/error_code/error_code_test.go
@@ -22,40 +22,93 @@ import (
 	"github.com/pingcap/pd/pkg/error_code"
 )
 
+// Test setting the HTTP code
+type HTTPError struct{}
+
+func (e HTTPError) Error() string { return "error" }
+
+var codeHttp900 = errcode.InvalidInputCode.Child(codeString).SetHTTP(900)
+
+func (e HTTPError) Code() errcode.Code {
+	return codeHttp900
+}
+
+func TestHttpErrorCode(t *testing.T) {
+	http := HTTPError{}
+	AssertHTTPCode(t, http, 900)
+	ErrorEquals(t, http, "error")
+	ClientDataEquals(t, http, http)
+}
+
+// Test a very simple error
 type MinimalError struct{}
+
+func (e MinimalError) Error() string { return "error" }
 
 var _ errcode.ErrorCode = (*MinimalError)(nil) // assert implements interface
 
-const registeredCode errcode.RegisteredCode = "registeredCode"
+const codeString errcode.CodeStr = "input.testcode"
 
-func (e MinimalError) Code() errcode.RegisteredCode {
-	return registeredCode
-}
-func (e MinimalError) Error() string {
-	return "error"
-}
+var registeredCode errcode.Code = errcode.InvalidInputCode.Child(codeString)
 
-type HTTPError struct{}
+func (e MinimalError) Code() errcode.Code { return registeredCode }
 
-var _ errcode.ErrorCode = (*HTTPError)(nil)   // assert implements interface
-var _ errcode.HasHTTPCode = (*HTTPError)(nil) // assert implements interface
-
-func (e HTTPError) Code() errcode.RegisteredCode {
-	return registeredCode
-}
-func (e HTTPError) Error() string {
-	return "error"
-}
-func (e HTTPError) GetHTTPCode() int {
-	return 900
+func TestMinimalErrorCode(t *testing.T) {
+	minimal := MinimalError{}
+	AssertCodes(t, minimal)
+	ErrorEquals(t, minimal, "error")
+	ClientDataEquals(t, minimal, minimal)
 }
 
+// Test a top-level error
+type TopError struct{}
+
+func (e TopError) Error() string { return "error" }
+
+var _ errcode.ErrorCode = (*TopError)(nil) // assert implements interface
+
+const topCodeStr errcode.CodeStr = "top"
+
+var topCode errcode.Code = errcode.NewCode(topCodeStr)
+
+func (e TopError) Code() errcode.Code { return topCode }
+
+func TestTopErrorCode(t *testing.T) {
+	top := TopError{}
+	AssertCodes(t, top, topCodeStr)
+	ErrorEquals(t, top, "error")
+	ClientDataEquals(t, top, top, topCodeStr)
+}
+
+// Test a deep hierarchy
+type DeepError struct{}
+
+func (e DeepError) Error() string { return "error" }
+
+var _ errcode.ErrorCode = (*DeepError)(nil) // assert implements interface
+
+const deepCodeStr errcode.CodeStr = "input.testcode.very.very.deep"
+
+var intermediateCode = registeredCode.Child("input.testcode.very").SetHTTP(800)
+var deepCode errcode.Code = intermediateCode.Child("input.testcode.very.very").Child(deepCodeStr)
+
+func (e DeepError) Code() errcode.Code { return deepCode }
+
+func TestDeepErrorCode(t *testing.T) {
+	deep := DeepError{}
+	AssertHTTPCode(t, deep, 800)
+	AssertCode(t, deep, deepCodeStr)
+	ErrorEquals(t, deep, "error")
+	ClientDataEquals(t, deep, deep, deepCodeStr)
+}
+
+// Test an ErrorWrapper that has different error types placed into it
 type ErrorWrapper struct{ Err error }
 
 var _ errcode.ErrorCode = (*ErrorWrapper)(nil)     // assert implements interface
 var _ errcode.HasClientData = (*ErrorWrapper)(nil) // assert implements interface
 
-func (e ErrorWrapper) Code() errcode.RegisteredCode {
+func (e ErrorWrapper) Code() errcode.Code {
 	return registeredCode
 }
 func (e ErrorWrapper) Error() string {
@@ -85,22 +138,6 @@ func (e Struct2) Error() string {
 	return fmt.Sprintf("error A & B %s & %s", e.A, e.B)
 }
 
-var emptyStruct struct{}
-
-func TestHttpErrorCode(t *testing.T) {
-	http := HTTPError{}
-	AssertHTTPCode(t, http, 900)
-	ErrorEquals(t, http, "error")
-	ClientDataEquals(t, http, http)
-}
-
-func TestMinimalErrorCode(t *testing.T) {
-	minimal := MinimalError{}
-	AssertCodes(t, minimal)
-	ErrorEquals(t, minimal, "error")
-	ClientDataEquals(t, minimal, minimal)
-}
-
 func TestErrorWrapperCode(t *testing.T) {
 	wrapped := ErrorWrapper{Err: errors.New("error")}
 	AssertCodes(t, wrapped)
@@ -117,17 +154,27 @@ func TestErrorWrapperCode(t *testing.T) {
 	ClientDataEquals(t, ErrorWrapper{Err: sconst}, sconst)
 }
 
-func AssertCodes(t *testing.T, code errcode.ErrorCode) {
-	t.Helper()
-	if code.Code() != registeredCode {
-		t.Error("bad code")
-	}
+func AssertCodes(t *testing.T, code errcode.ErrorCode, codeStrs ...errcode.CodeStr) {
+	AssertCode(t, code, codeStrs...)
 	AssertHTTPCode(t, code, 400)
 }
 
+func AssertCode(t *testing.T, code errcode.ErrorCode, codeStrs ...errcode.CodeStr) {
+	t.Helper()
+	codeStr := codeString
+	if len(codeStrs) > 0 {
+		codeStr = codeStrs[0]
+	}
+	if code.Code().CodeStr != codeStr {
+		t.Error("bad code")
+	}
+}
+
 func AssertHTTPCode(t *testing.T, code errcode.ErrorCode, httpCode int) {
-	if errcode.HTTPCode(code) != httpCode {
-		t.Errorf("excpected HTTP Code %v", httpCode)
+	t.Helper()
+	expected := code.Code().HTTPCode()
+	if expected != httpCode {
+		t.Errorf("excpected HTTP Code %v but got %v", httpCode, expected)
 	}
 }
 
@@ -137,12 +184,20 @@ func ErrorEquals(t *testing.T, err error, msg string) {
 	}
 }
 
-func ClientDataEquals(t *testing.T, code errcode.ErrorCode, data interface{}) {
+func ClientDataEquals(t *testing.T, code errcode.ErrorCode, data interface{}, codeStrs ...errcode.CodeStr) {
+	codeStr := codeString
+	if len(codeStrs) > 0 {
+		codeStr = codeStrs[0]
+	}
 	t.Helper()
 	if !reflect.DeepEqual(errcode.ClientData(code), data) {
 		t.Errorf("\nClientData expected: %#v\n ClientData but got: %#v", data, errcode.ClientData(code))
 	}
-	jsonExpected := errcode.JSONFormat{Data: data, Msg: code.Error(), Code: registeredCode}
+	jsonExpected := errcode.JSONFormat{
+		Data: data,
+		Msg:  code.Error(),
+		Code: codeStr,
+	}
 	if !reflect.DeepEqual(errcode.NewJSONFormat(code), jsonExpected) {
 		t.Errorf("\nJSON expected: %+v\n JSON but got: %+v", jsonExpected, errcode.NewJSONFormat(code))
 	}
