eb6781cc97e7a9498fbc9543c9e02308e5a8ab45
nolouch <nolouch@gmail.com>
2018-08-13 17:19:18 +0800

    server: remove extra region clone when handle region heartbeat

--- server/cluster_info.go
+++ server/cluster_info.go
@@ -446,7 +446,6 @@ func (c *clusterInfo) updateStoreStatusLocked(id uint64) {
 
 // handleRegionHeartbeat updates the region information.
 func (c *clusterInfo) handleRegionHeartbeat(region *core.RegionInfo) error {
-	region = region.Clone()
 	c.RLock()
 	origin := c.core.Regions.GetRegion(region.GetId())
 	isWriteUpdate, writeItem := c.core.CheckWriteStatus(region)
