3c0cbabba25073cd2ebf791767546956a8368166
nolouch <nolouch@gmail.com>
2018-09-27 19:53:29 +0800

    address comment

--- pkg/logutil/log.go
+++ pkg/logutil/log.go
@@ -223,8 +223,16 @@ type wrapLogrus struct {
 	*log.Logger
 }
 
+// V provides the functionality that returns whether a particular log level is at
+// least l - this is needed to meet the LoggerV2 interface.  GRPC's logging levels
+// are: https://github.com/grpc/grpc-go/blob/master/grpclog/loggerv2.go#L71
+// 0=info, 1=warning, 2=error, 3=fatal
+// logrus's are: https://github.com/sirupsen/logrus/blob/master/logrus.go
+// 0=panic, 1=fatal, 2=error, 3=warn, 4=info, 5=debug
 func (lg *wrapLogrus) V(l int) bool {
-	return true
+	// translate to logrus level
+	logrusLevel := 4 - l
+	return int(lg.Logger.Level) <= logrusLevel
 }
 
 var once sync.Once
