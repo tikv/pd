6a0c3d2ce3e698ce8b2758289fdd53ebf1654147
nolouch <nolouch@gmail.com>
2018-11-06 20:10:25 +0800

    address comments

--- server/region_syncer/history_buffer_test.go
+++ server/region_syncer/history_buffer_test.go
@@ -1,15 +1,15 @@
-// copyright 2018 pingcap, inc.
+// Copyright 2018 PingCAP, Inc.
 //
-// licensed under the apache license, version 2.0 (the "license");
-// you may not use this file except in compliance with the license.
-// you may obtain a copy of the license at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/license-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// unless required by applicable law or agreed to in writing, software
-// distributed under the license is distributed on an "as is" basis,
-// see the license for the specific language governing permissions and
-// limitations under the license.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package syncer
 
