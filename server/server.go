74526c7263ac393f74c3b79914f556e11365ff0b
webb.shi <bradyjoestar@gmail.com>
2019-05-05 05:05:01 -0400

    gofmt

--- server/server.go
+++ server/server.go
@@ -309,7 +309,7 @@ func (s *Server) Run(ctx context.Context) error {
 	}
 
 	s.watchProxyServer = NewWatchProxyServer(s.client)
-	go s.runWatchProxy(s.watchProxyServer,s.cfg.LeaderLease)
+	go s.runWatchProxy(s.watchProxyServer, s.cfg.LeaderLease)
 	s.startServerLoop()
 
 	return nil
