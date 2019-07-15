e4b0b7f92cdc13e8d9ca9b8b274ae1b9ad70b0f5
Ryan Leung <rleungx@gmail.com>
2019-07-15 17:03:06 +0800

    core: use hex format for region meta key (#1627)
    Signed-off-by: Ryan Leung <rleungx@gmail.com>=

--- server/schedule/merge_checker.go
+++ server/schedule/merge_checker.go
@@ -110,7 +110,7 @@ func (m *MergeChecker) Check(region *core.RegionInfo) (*Operator, *Operator) {
 	}
 
 	checkerCounter.WithLabelValues("merge_checker", "new_operator").Inc()
-	log.Debug("try to merge region", zap.Reflect("from", core.HexRegionMeta(region.GetMeta())), zap.Reflect("to", core.HexRegionMeta(target.GetMeta())))
+	log.Debug("try to merge region", zap.Stringer("from", core.RegionToHexMeta(region.GetMeta())), zap.Stringer("to", core.RegionToHexMeta(target.GetMeta())))
 	op1, op2, err := CreateMergeRegionOperator("merge-region", m.cluster, region, target, OpMerge)
 	if err != nil {
 		return nil, nil
