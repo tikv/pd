51abff0d1c1e8fb547b62c07bc78c31b52f0c495
nolouch <nolouch@gmail.com>
2019-10-12 13:40:09 +0800

    fix test
    Signed-off-by: nolouch <nolouch@gmail.com>

--- server/server.go
+++ server/server.go
@@ -201,7 +201,7 @@ func (s *Server) startEtcd(ctx context.Context) error {
 	}
 	s.client = client
 	failpoint.Inject("memberNil", func() {
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(1500 * time.Millisecond)
 	})
 	s.member = member.NewMember(etcd, client, etcdServerID)
 	return nil
@@ -515,7 +515,11 @@ func (s *Server) GetConfig() *config.Config {
 	cfg.LabelProperty = s.scheduleOpt.LoadLabelPropertyConfig().Clone()
 	cfg.ClusterVersion = *s.scheduleOpt.LoadClusterVersion()
 	cfg.PDServerCfg = *s.scheduleOpt.LoadPDServerConfig()
-	sches, configs, err := s.GetStorage().LoadAllScheduleConfig()
+	storage := s.GetStorage()
+	if storage == nil {
+		return cfg
+	}
+	sches, configs, err := storage.LoadAllScheduleConfig()
 	if err != nil {
 		return cfg
 	}
