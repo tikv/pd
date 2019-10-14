8e82a0ab58618f2936e11d7555a2ddd0eade5ead
nolouch <nolouch@gmail.com>
2019-10-14 11:59:06 +0800

    address comments
    Signed-off-by: nolouch <nolouch@gmail.com>

--- server/core/store.go
+++ server/core/store.go
@@ -19,7 +19,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
 	"github.com/pingcap/errcode"
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/kvproto/pkg/pdpb"
@@ -60,9 +59,8 @@ func NewStoreInfo(store *metapb.Store, opts ...StoreCreateOption) *StoreInfo {
 
 // Clone creates a copy of current StoreInfo.
 func (s *StoreInfo) Clone(opts ...StoreCreateOption) *StoreInfo {
-	meta := proto.Clone(s.meta).(*metapb.Store)
 	store := &StoreInfo{
-		meta:             meta,
+		meta:             s.meta,
 		stats:            s.stats,
 		blocked:          s.blocked,
 		leaderCount:      s.leaderCount,
