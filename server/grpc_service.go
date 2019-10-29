27e50c19b37057aee8e6e10f599350ca514ed2e6
Ryan Leung <rleungx@gmail.com>
2019-10-29 19:48:17 +0800

    add log for get timestamps
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/grpc_service.go
+++ server/grpc_service.go
@@ -83,6 +83,11 @@ func (s *Server) Tso(stream pdpb.PD_TsoServer) error {
 		if err != nil {
 			return status.Errorf(codes.Unknown, err.Error())
 		}
+
+		elapsed := time.Since(start)
+		if elapsed > time.Second {
+			log.Warn("get timestamp too slow", zap.Duration("cost", elapsed))
+		}
 		response := &pdpb.TsoResponse{
 			Header:    s.header(),
 			Timestamp: &ts,
