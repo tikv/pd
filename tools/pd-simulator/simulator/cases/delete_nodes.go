e8bd6906b42fa010d8032b5647c8edde034963c5
lhy1024 <admin@liudos.us>
2019-10-31 11:59:00 +0800

    tools: config for cases in simulator (#1731)
    * add balance region case
    
    * Update task.go
    
    remove unnecessary comment
    
    * add comment for transfer region counter
    
    * add unit test for balance region case
    
    * add comment and rename var
    
    * update comment
    
    * update license
    
    * format comment
    
    * Update task.go
    
    * clean compaction
    
    * update task.go
    
    * rename case
    
    * remove var `available` and add new param
    
    * add config for flag
    
    * fix cycle import and add config for flags
    
    * rename flags and format log
    
    * add judge for avoiding to influence other case
    
    * remove default config for all cases
    
    * add config for add_nodes.go
    
    * add config for balance_leader.go
    
    * add random rate
    
    * add config for add_nodes_dynamic.go and rebuild
    
    * fix panic when delete_nodes.go
    
    * add config for delete_nodes.go
    
    * rename case and format log
    
    * add config for hot_read.go
    
    * add config for hot_write.go
    
    * add config for region_split.go
    
    * add config for region_merge.go
    
    * use better checker for region_merge
    
    * split transfer region count
    
    * add config for makeup_down_replica
    
    * rename package
    
    * add support for sparse stores
    
    * format redundant balance region log
    
    * add solve for file error
    
    * format comment and rename package
    
    * rename package
    
    * rename package
    
    * add isReady
    
    * use singleton
    
    * format
    
    * refactor
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    * fix store num
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    * Update tools/pd-simulator/simulator/cases/makeup_down_replica.go
    
    Co-Authored-By: Ryan Leung <rleungx@gmail.com>
    
    * address comment
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    * address comment
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    (cherry picked from commit fb6ab25e94b647002fe66cb2d3232c16d4ef0134)

--- tools/pd-simulator/simulator/cases/delete_nodes.go
+++ tools/pd-simulator/simulator/cases/delete_nodes.go
@@ -26,7 +26,9 @@ import (
 func newDeleteNodes() *Case {
 	var simCase Case
 
-	for i := 1; i <= 8; i++ {
+	storeNum, regionNum := getStoreNum(), getRegionNum()
+	noEmptyStoreNum := storeNum - 1
+	for i := 1; i <= storeNum; i++ {
 		simCase.Stores = append(simCase.Stores, &Store{
 			ID:        IDAllocator.nextID(),
 			Status:    metapb.StoreState_Up,
@@ -36,11 +38,11 @@ func newDeleteNodes() *Case {
 		})
 	}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < regionNum*storeNum/3; i++ {
 		peers := []*metapb.Peer{
-			{Id: IDAllocator.nextID(), StoreId: uint64(i)%8 + 1},
-			{Id: IDAllocator.nextID(), StoreId: uint64(i+1)%8 + 1},
-			{Id: IDAllocator.nextID(), StoreId: uint64(i+2)%8 + 1},
+			{Id: IDAllocator.nextID(), StoreId: uint64(i%storeNum) + 1},
+			{Id: IDAllocator.nextID(), StoreId: uint64((i+1)%storeNum) + 1},
+			{Id: IDAllocator.nextID(), StoreId: uint64((i+2)%storeNum) + 1},
 		}
 		simCase.Regions = append(simCase.Regions, Region{
 			ID:     IDAllocator.nextID(),
@@ -56,10 +58,10 @@ func newDeleteNodes() *Case {
 		ids = append(ids, store.ID)
 	}
 
-	numNodes := 8
+	numNodes := storeNum
 	e := &DeleteNodesDescriptor{}
 	e.Step = func(tick int64) uint64 {
-		if numNodes > 7 && tick%100 == 0 {
+		if numNodes > noEmptyStoreNum && tick%100 == 0 {
 			idx := rand.Intn(numNodes)
 			numNodes--
 			nodeID := ids[idx]
@@ -70,6 +72,7 @@ func newDeleteNodes() *Case {
 	}
 	simCase.Events = []EventDescriptor{e}
 
+	threshold := 0.05
 	simCase.Checker = func(regions *core.RegionsInfo, stats []info.StoreStats) bool {
 		res := numNodes == noEmptyStoreNum
 		leaderCounts := make([]int, 0, numNodes)
