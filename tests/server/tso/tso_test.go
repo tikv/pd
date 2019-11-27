ffe3805c39d23da18e78c52242e33f82fe86337e
nolouch <nolouch@gmail.com>
2019-11-27 18:42:44 +0800

    address comments
    Signed-off-by: nolouch <nolouch@gmail.com>

--- tests/server/tso/tso_test.go
+++ tests/server/tso/tso_test.go
@@ -116,6 +116,41 @@ func (s *testTsoSuite) TestTso(c *C) {
 	wg.Wait()
 }
 
+func (s *testTsoSuite) TestConcurrcyRequest(c *C) {
+	cluster, err := tests.NewTestCluster(1)
+	defer cluster.Destroy()
+	c.Assert(err, IsNil)
+
+	err = cluster.RunInitialServers(s.ctx)
+	c.Assert(err, IsNil)
+	cluster.WaitLeader()
+
+	leader := cluster.GetServer(cluster.GetLeader())
+
+	c.Assert(leader, NotNil)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	now := time.Now()
+	go func() {
+		defer wg.Done()
+		for i := 0; i <= 100; i++ {
+			physical := now.Add(time.Duration(2*i)*time.Minute).UnixNano() / int64(time.Millisecond)
+			ts := uint64(physical << 18)
+			leader.GetServer().GetHandler().ResetTS(ts)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i <= 100; i++ {
+			physical := now.Add(time.Duration(2*i)*time.Minute).UnixNano() / int64(time.Millisecond)
+			ts := uint64(physical << 18)
+			leader.GetServer().GetHandler().ResetTS(ts)
+		}
+	}()
+	wg.Wait()
+}
+
 func (s *testTsoSuite) TestTsoCount0(c *C) {
 	var err error
 	cluster, err := tests.NewTestCluster(1)
@@ -282,39 +317,4 @@ func (s *testFollowerTsoSuite) TestRequest(c *C) {
 	c.Assert(err, NotNil)
 	c.Assert(strings.Contains(err.Error(), "can not get timestamp"), IsTrue)
 	failpoint.Disable("github.com/pingcap/pd/server/tso/skipRetryGetTS")
-}
-
-func (s *testFollowerTsoSuite) TestConcurrcyRequest(c *C) {
-	cluster, err := tests.NewTestCluster(1)
-	defer cluster.Destroy()
-	c.Assert(err, IsNil)
-
-	err = cluster.RunInitialServers(s.ctx)
-	c.Assert(err, IsNil)
-	cluster.WaitLeader()
-
-	leader := cluster.GetServer(cluster.GetLeader())
-
-	c.Assert(leader, NotNil)
-	var wg sync.WaitGroup
-	wg.Add(2)
-	now := time.Now()
-	go func() {
-		defer wg.Done()
-		for i := 0; i <= 100; i++ {
-			physical := now.Add(time.Duration(2*i)*time.Minute).UnixNano() / int64(time.Millisecond)
-			ts := uint64(physical << 18)
-			leader.GetServer().GetHandler().ResetTS(ts)
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		for i := 0; i <= 100; i++ {
-			physical := now.Add(time.Duration(2*i)*time.Minute).UnixNano() / int64(time.Millisecond)
-			ts := uint64(physical << 18)
-			leader.GetServer().GetHandler().ResetTS(ts)
-		}
-	}()
-	wg.Wait()
 }
