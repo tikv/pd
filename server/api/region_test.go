cbaa554bd6e77cf5adf00a998b603a3b3665d951
Neil Shen <overvenus@gmail.com>
2019-12-12 23:19:33 +0800

    server/api: add test
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- server/api/region_test.go
+++ server/api/region_test.go
@@ -277,6 +277,16 @@ func (s *testGetRegionSuite) SetUpSuite(c *C) {
 	s.urlPrefix = fmt.Sprintf("%s%s/api/v1", addr, apiPrefix)
 
 	mustBootstrapCluster(c, s.svr)
+	r1 := newTestRegionInfo(2, 1, []byte("a"), []byte("b"))
+	r2 := newTestRegionInfo(3, 1, []byte("b"), []byte("c"))
+	r3 := newTestRegionInfo(4, 2, []byte("c"), []byte("e"))
+	r4 := newTestRegionInfo(5, 2, []byte("x"), []byte("z"))
+	r := newTestRegionInfo(99, 1, []byte{0xFF, 0xFF, 0xAA}, []byte{0xFF, 0xFF, 0xCC}, core.SetWrittenBytes(500), core.SetReadBytes(800), core.SetRegionConfVer(3), core.SetRegionVersion(2))
+	mustRegionHeartbeat(c, s.svr, r1)
+	mustRegionHeartbeat(c, s.svr, r2)
+	mustRegionHeartbeat(c, s.svr, r3)
+	mustRegionHeartbeat(c, s.svr, r4)
+	mustRegionHeartbeat(c, s.svr, r)
 }
 
 func (s *testGetRegionSuite) TearDownSuite(c *C) {
@@ -294,17 +304,6 @@ func (s *testGetRegionSuite) TestRegionKey(c *C) {
 }
 
 func (s *testGetRegionSuite) TestScanRegionByKey(c *C) {
-	r1 := newTestRegionInfo(2, 1, []byte("a"), []byte("b"))
-	r2 := newTestRegionInfo(3, 1, []byte("b"), []byte("c"))
-	r3 := newTestRegionInfo(4, 2, []byte("c"), []byte("e"))
-	r4 := newTestRegionInfo(5, 2, []byte("x"), []byte("z"))
-	r := newTestRegionInfo(99, 1, []byte{0xFF, 0xFF, 0xAA}, []byte{0xFF, 0xFF, 0xCC}, core.SetWrittenBytes(500), core.SetReadBytes(800), core.SetRegionConfVer(3), core.SetRegionVersion(2))
-	mustRegionHeartbeat(c, s.svr, r1)
-	mustRegionHeartbeat(c, s.svr, r2)
-	mustRegionHeartbeat(c, s.svr, r3)
-	mustRegionHeartbeat(c, s.svr, r4)
-	mustRegionHeartbeat(c, s.svr, r)
-
 	url := fmt.Sprintf("%s/regions/key?key=%s", s.urlPrefix, "b")
 	regionIds := []uint64{3, 4, 5, 99}
 	regions := &RegionsInfo{}
@@ -332,6 +331,29 @@ func (s *testGetRegionSuite) TestScanRegionByKey(c *C) {
 	for i, v := range regionIds {
 		c.Assert(v, Equals, regions.Regions[i].ID)
 	}
+}
+
+func (s *testGetRegionSuite) TestRegionCount(c *C) {
+	url := fmt.Sprintf("%s/regions/count", s.urlPrefix)
+	regions := &RegionsInfo{}
+	err := readJSON(url, regions)
+	c.Assert(err, IsNil)
+	c.Assert(5, Equals, regions.Count)
+
+	url = fmt.Sprintf("%s/regions/count?start_key=%s", s.urlPrefix, "b")
+	err = readJSON(url, regions)
+	c.Assert(err, IsNil)
+	c.Assert(4, Equals, regions.Count)
+
+	url = fmt.Sprintf("%s/regions/count?end_key=%s", s.urlPrefix, "c")
+	err = readJSON(url, regions)
+	c.Assert(err, IsNil)
+	c.Assert(2, Equals, regions.Count)
+
+	url = fmt.Sprintf("%s/regions/count?start_key=%s&end_key=%s", s.urlPrefix, "b", "y")
+	err = readJSON(url, regions)
+	c.Assert(err, IsNil)
+	c.Assert(3, Equals, regions.Count)
 }
 
 // Create n regions (0..n) of n stores (0..n).
