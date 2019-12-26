22b91de194d9c8f3855ba77d7f55ccf10c8c722a
mantuliu <240951888@qq.com>
2019-12-26 15:11:36 +0800

    add test

--- tools/pd-backup/main.go
+++ tools/pd-backup/main.go
@@ -14,22 +14,13 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"path"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/pingcap/pd/pkg/etcdutil"
-	"github.com/pingcap/pd/pkg/typeutil"
-	"github.com/pingcap/pd/server/config"
+	"github.com/pingcap/pd/tools/pd-backup/pdbackup"
 	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/pkg/transport"
 )
@@ -43,18 +34,8 @@ var (
 )
 
 const (
-	etcdTimeout     = 3 * time.Second
-	pdRootPath      = "/pd"
-	pdClusterIDPath = "/pd/cluster_id"
-	pdConfigAPIPath = "/pd/api/v1/config"
+	etcdTimeout = 3 * time.Second
 )
-
-type backupInfo struct {
-	ClusterID         uint64         `json:"clusterID"`
-	AllocIDMax        uint64         `json:"allocIDMax"`
-	AllocTimestampMax uint64         `json:"allocTimestampMax"`
-	Config            *config.Config `json:"config"`
-}
 
 func main() {
 	flag.Parse()
@@ -78,36 +59,10 @@ func main() {
 	})
 	checkErr(err)
 
-	backInfo := getBackupInfo(client)
-	outputToFile(backInfo, f)
+	backInfo, err := pdbackup.GetBackupInfo(client, *pdAddr)
+	checkErr(err)
+	pdbackup.OutputToFile(backInfo, f)
 	fmt.Println("pd backup successful! dump file is:", *filePath)
-}
-
-func getBackupInfo(client *clientv3.Client) *backupInfo {
-	backInfo := &backupInfo{}
-	resp, err := etcdutil.EtcdKVGet(client, pdClusterIDPath)
-	checkErr(err)
-	clusterID, err := typeutil.BytesToUint64(resp.Kvs[0].Value)
-	checkErr(err)
-	backInfo.ClusterID = clusterID
-
-	rootPath := path.Join(pdRootPath, strconv.FormatUint(clusterID, 10))
-	allocIDPath := path.Join(rootPath, "alloc_id")
-	resp, err = etcdutil.EtcdKVGet(client, allocIDPath)
-	checkErr(err)
-	allocIDMax, err := typeutil.BytesToUint64(resp.Kvs[0].Value)
-	checkErr(err)
-	backInfo.AllocIDMax = allocIDMax
-
-	timestampPath := path.Join(rootPath, "timestamp")
-	resp, err = etcdutil.EtcdKVGet(client, timestampPath)
-	checkErr(err)
-	allocTimestampMax, err := typeutil.BytesToUint64(resp.Kvs[0].Value)
-	checkErr(err)
-	backInfo.AllocTimestampMax = allocTimestampMax
-
-	backInfo.Config = getConfig()
-	return backInfo
 }
 
 func checkErr(err error) {
@@ -115,26 +70,4 @@ func checkErr(err error) {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-}
-
-func outputToFile(backInfo *backupInfo, f *os.File) {
-	w := bufio.NewWriter(f)
-	defer w.Flush()
-	backBytes, err := json.Marshal(backInfo)
-	checkErr(err)
-	var formatBuffer bytes.Buffer
-	checkErr(json.Indent(&formatBuffer, []byte(backBytes), "", "    "))
-	fmt.Fprintln(w, formatBuffer.String())
-}
-
-func getConfig() *config.Config {
-	resp, err := http.Get(*pdAddr + pdConfigAPIPath)
-	checkErr(err)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	checkErr(err)
-	conf := &config.Config{}
-	err = json.Unmarshal(body, conf)
-	checkErr(err)
-	return conf
 }
