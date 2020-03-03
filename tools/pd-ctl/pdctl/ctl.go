ec4e0f6311b7954ef61195c93d6e614d0ed11bb0
loxp <loxppqqxol@gmail.com>
2020-03-03 17:50:26 +0800

    remove package pflag in pd-ctl

--- tools/pd-ctl/pdctl/ctl.go
+++ tools/pd-ctl/pdctl/ctl.go
@@ -14,10 +14,14 @@
 package pdctl
 
 import (
-	"os"
-
+	"fmt"
+	"github.com/chzyer/readline"
+	"github.com/mattn/go-shellwords"
+	"github.com/pingcap/pd/v4/server"
 	"github.com/pingcap/pd/v4/tools/pd-ctl/pdctl/command"
 	"github.com/spf13/cobra"
+	"io"
+	"os"
 )
 
 // CommandFlags are flags that used in all Commands
@@ -31,23 +35,38 @@ type CommandFlags struct {
 
 var (
 	commandFlags = CommandFlags{}
+
+	detach   bool
+	interact bool
+	version  bool
 )
 
 func init() {
 	cobra.EnablePrefixMatching = true
 }
 
-// Start run Command
-func Start(args []string) {
+func pdctlRun(cmd *cobra.Command, args []string) {
+	if version {
+		server.PrintPDInfo()
+		return
+	}
+	if interact {
+		loop()
+	}
+}
+
+func getBasicCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "pd-ctl",
 		Short: "Placement Driver control",
 	}
+
 	rootCmd.PersistentFlags().StringVarP(&commandFlags.URL, "pd", "u", "http://127.0.0.1:2379", "pd address")
 	rootCmd.Flags().StringVar(&commandFlags.CAPath, "cacert", "", "path of file that contains list of trusted SSL CAs.")
 	rootCmd.Flags().StringVar(&commandFlags.CertPath, "cert", "", "path of file that contains X509 certificate in PEM format.")
 	rootCmd.Flags().StringVar(&commandFlags.KeyPath, "key", "", "path of file that contains X509 key in PEM format.")
 	rootCmd.PersistentFlags().BoolVarP(&commandFlags.Help, "help", "h", false, "Help message.")
+
 	rootCmd.AddCommand(
 		command.NewConfigCommand(),
 		command.NewRegionCommand(),
@@ -68,11 +87,40 @@ func Start(args []string) {
 		command.NewComponentConfigCommand(),
 	)
 
-	rootCmd.SetArgs(args)
+	rootCmd.Flags().ParseErrorsWhitelist.UnknownFlags = true
 	rootCmd.SilenceErrors = true
+
+	return rootCmd
+}
+
+func getRootCmd(args []string) *cobra.Command {
+	rootCmd := getBasicCmd()
+
+	rootCmd.SetArgs(args)
 	rootCmd.ParseFlags(args)
 	rootCmd.SetOutput(os.Stdout)
 
+	return rootCmd
+}
+
+func getMainCmd(args []string) *cobra.Command {
+	rootCmd := getBasicCmd()
+
+	rootCmd.Flags().BoolVarP(&detach, "detach", "d", true, "Run pdctl without readline.")
+	rootCmd.Flags().BoolVarP(&interact, "interact", "i", false, "Run pdctl with readline.")
+	rootCmd.Flags().BoolVarP(&version, "version", "V", false, "Print version information and exit.")
+	rootCmd.Run = pdctlRun
+
+	rootCmd.SetArgs(args)
+	rootCmd.ParseFlags(args)
+	rootCmd.SetOutput(os.Stdout)
+
+	return rootCmd
+}
+
+// Start main command
+func MainStart(args []string) {
+	rootCmd := getMainCmd(args)
 	if len(commandFlags.CAPath) != 0 {
 		if err := command.InitHTTPSClient(commandFlags.CAPath, commandFlags.CertPath, commandFlags.KeyPath); err != nil {
 			rootCmd.Println(err)
@@ -82,5 +130,59 @@ func Start(args []string) {
 
 	if err := rootCmd.Execute(); err != nil {
 		rootCmd.Println(err)
+	}
+}
+
+// Start interact command
+func Start(args []string) {
+	rootCmd := getRootCmd(args)
+	if len(commandFlags.CAPath) != 0 {
+		if err := command.InitHTTPSClient(commandFlags.CAPath, commandFlags.CertPath, commandFlags.KeyPath); err != nil {
+			rootCmd.Println(err)
+			return
+		}
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		rootCmd.Println(err)
+	}
+}
+
+func loop() {
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            "\033[31m»\033[0m ",
+		HistoryFile:       "/tmp/readline.tmp",
+		InterruptPrompt:   "^C",
+		EOFPrompt:         "^D",
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	for {
+		line, err := l.Readline()
+		if err != nil {
+			if err == readline.ErrInterrupt {
+				break
+			} else if err == io.EOF {
+				break
+			}
+			continue
+		}
+		if line == "exit" {
+			os.Exit(0)
+		}
+		args, err := shellwords.Parse(line)
+		if err != nil {
+			fmt.Printf("parse command err: %v\n", err)
+			continue
+		}
+		args = append(args, "-u", commandFlags.URL)
+		if commandFlags.CAPath != "" && commandFlags.CertPath != "" && commandFlags.KeyPath != "" {
+			args = append(args, "--cacert", commandFlags.CAPath, "--cert", commandFlags.CertPath, "--key", commandFlags.KeyPath)
+		}
+		Start(args)
 	}
 }
