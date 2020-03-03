pingcap/tidb-dashboard

*: reset metrics after the leader steps down (#1790)
Signed-off-by: Ryan Leung <rleungx@gmail.com>

server/statistics/hot_cache.go
@@ -106,10 +106,15 @@ func (w *HotCache) IsRegionHot(region *core.RegionInfo, hotDegree int) bool {
 		w.readFlow.IsRegionHot(region, hotDegree)
 }
 
-// CollectMetrics collect the hot cache metrics
+// CollectMetrics collects the hot cache metrics.
 func (w *HotCache) CollectMetrics(stats *StoresStats) {
 	w.writeFlow.CollectMetrics(stats, "write")
 	w.readFlow.CollectMetrics(stats, "read")
+}
+
+// ResetMetrics resets the hot cache metrics.
+func (w *HotCache) ResetMetrics() {
+	hotCacheStatusGauge.Reset()
 }
 
 func (w *HotCache) incMetrics(name string, storeID uint64, kind FlowKind) {

tikv/pd

server/schedule/hot_cache.go
@@ -217,6 +217,11 @@ func (w *HotSpotCache) CollectMetrics(stores *core.StoresInfo) {
 	hotCacheStatusGauge.WithLabelValues("hotThreshold", "read").Set(float64(threshold))
 }
 
+// ResetMetrics reset the hot cache metrics
+func (w *HotSpotCache) ResetMetrics() {
+	hotCacheStatusGauge.Reset()
+}
+
 func (w *HotSpotCache) isRegionHot(id uint64, hotThreshold int) bool {
 	if stat, ok := w.writeFlow.Peek(id); ok {
 		if stat.(*core.RegionStat).HotDegree >= hotThreshold {