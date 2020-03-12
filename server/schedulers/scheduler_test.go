pingcap/tidb-dashboard

schedulers: add role config to shuffleRegionScheduler (#2219)
Signed-off-by: disksing <i@disksing.com>

server/schedulers/scheduler_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/pingcap/pd/v4/server/schedule"
 	"github.com/pingcap/pd/v4/server/schedule/operator"
 	"github.com/pingcap/pd/v4/server/schedule/opt"
+	"github.com/pingcap/pd/v4/server/schedule/placement"
 	"github.com/pingcap/pd/v4/server/statistics"
 )
 
@@ -486,6 +487,60 @@ func (s *testShuffleRegionSuite) TestShuffle(c *C) {
 		c.Assert(op, NotNil)
 		c.Assert(op[0].Kind(), Equals, operator.OpRegion|operator.OpAdmin)
 	}
+}
+
+func (s *testShuffleRegionSuite) TestRole(c *C) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	opt := mockoption.NewScheduleOptions()
+	tc := mockcluster.NewCluster(opt)
+
+	// update rule to 1leader+1follower+1learner
+	opt.EnablePlacementRules = true
+	tc.RuleManager.SetRule(&placement.Rule{
+		GroupID: "pd",
+		ID:      "default",
+		Role:    placement.Voter,
+		Count:   2,
+	})
+	tc.RuleManager.SetRule(&placement.Rule{
+		GroupID: "pd",
+		ID:      "learner",
+		Role:    placement.Learner,
+		Count:   1,
+	})
+
+	// Add stores 1, 2, 3, 4
+	tc.AddRegionStore(1, 6)
+	tc.AddRegionStore(2, 7)
+	tc.AddRegionStore(3, 8)
+	tc.AddRegionStore(4, 9)
+
+	// Put a region with 1leader + 1follower + 1learner
+	peers := []*metapb.Peer{
+		{Id: 1, StoreId: 1},
+		{Id: 2, StoreId: 2},
+		{Id: 3, StoreId: 3, IsLearner: true},
+	}
+	region := core.NewRegionInfo(&metapb.Region{
+		Id:          1,
+		RegionEpoch: &metapb.RegionEpoch{ConfVer: 1, Version: 1},
+		Peers:       peers,
+	}, peers[0])
+	tc.PutRegion(region)
+
+	sl, err := schedule.CreateScheduler(ShuffleRegionType, schedule.NewOperatorController(ctx, nil, nil), core.NewStorage(kv.NewMemoryKV()), schedule.ConfigSliceDecoder(ShuffleRegionType, []string{"", ""}))
+	c.Assert(err, IsNil)
+
+	conf := sl.(*shuffleRegionScheduler).conf
+	conf.Roles = []string{"follower"}
+	ops := sl.Schedule(tc)
+	c.Assert(ops, HasLen, 1)
+	testutil.CheckTransferPeer(c, ops[0], operator.OpRegion, 2, 4) // transfer follower
+	conf.Roles = []string{"learner"}
+	ops = sl.Schedule(tc)
+	c.Assert(ops, HasLen, 1)
+	testutil.CheckTransferLearner(c, ops[0], operator.OpRegion, 3, 4) // transfer learner
 }
 
 var _ = Suite(&testSpecialUseSuite{})

server/schedulers/shuffle_region_config.go
@@ -1,0 +1,112 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schedulers
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/pingcap/pd/v4/pkg/apiutil"
+	"github.com/pingcap/pd/v4/pkg/slice"
+	"github.com/pingcap/pd/v4/server/core"
+	"github.com/pingcap/pd/v4/server/schedule"
+	"github.com/pingcap/pd/v4/server/schedule/placement"
+	"github.com/unrolled/render"
+)
+
+const (
+	roleLeader   = string(placement.Leader)
+	roleFollower = string(placement.Follower)
+	roleLearner  = string(placement.Learner)
+)
+
+var allRoles = []string{roleLeader, roleFollower, roleLearner}
+
+type shuffleRegionSchedulerConfig struct {
+	sync.RWMutex
+	storage *core.Storage
+
+	Ranges []core.KeyRange `json:"ranges"`
+	Roles  []string        `json:"roles"` // can include `leader`, `follower`, `learner`.
+}
+
+func (conf *shuffleRegionSchedulerConfig) EncodeConfig() ([]byte, error) {
+	conf.RLock()
+	defer conf.RUnlock()
+	return schedule.EncodeConfig(conf)
+}
+
+func (conf *shuffleRegionSchedulerConfig) GetRoles() []string {
+	conf.RLock()
+	defer conf.RUnlock()
+	return conf.Roles
+}
+
+func (conf *shuffleRegionSchedulerConfig) GetRanges() []core.KeyRange {
+	conf.RLock()
+	defer conf.RUnlock()
+	return conf.Ranges
+}
+
+func (conf *shuffleRegionSchedulerConfig) IsRoleAllow(role string) bool {
+	conf.RLock()
+	defer conf.RUnlock()
+	return slice.AnyOf(conf.Roles, func(i int) bool { return conf.Roles[i] == role })
+}
+
+func (conf *shuffleRegionSchedulerConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/roles", conf.handleGetRoles).Methods("GET")
+	router.HandleFunc("/roles", conf.handleSetRoles).Methods("POST")
+	router.ServeHTTP(w, r)
+}
+
+func (conf *shuffleRegionSchedulerConfig) handleGetRoles(w http.ResponseWriter, r *http.Request) {
+	rd := render.New(render.Options{IndentJSON: true})
+	rd.JSON(w, http.StatusOK, conf.GetRoles())
+}
+
+func (conf *shuffleRegionSchedulerConfig) handleSetRoles(w http.ResponseWriter, r *http.Request) {
+	rd := render.New(render.Options{IndentJSON: true})
+	var roles []string
+	if err := apiutil.ReadJSONRespondError(rd, w, r.Body, &roles); err != nil {
+		return
+	}
+	for _, r := range roles {
+		if slice.NoneOf(allRoles, func(i int) bool { return allRoles[i] == r }) {
+			rd.Text(w, http.StatusBadRequest, "invalid role:"+r)
+			return
+		}
+	}
+
+	conf.Lock()
+	defer conf.Unlock()
+	old := conf.Roles
+	conf.Roles = roles
+	if err := conf.persist(); err != nil {
+		conf.Roles = old // revert
+		rd.Text(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	rd.Text(w, http.StatusOK, "")
+}
+
+func (conf *shuffleRegionSchedulerConfig) persist() error {
+	data, err := schedule.EncodeConfig(conf)
+	if err != nil {
+		return err
+	}
+	return conf.storage.SaveScheduleConfig(ShuffleRegionName, data)
+}

tikv/pd

server/schedulers/scheduler_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/pingcap/pd/v3/server/schedule"
 	"github.com/pingcap/pd/v3/server/schedule/operator"
 	"github.com/pingcap/pd/v3/server/schedule/opt"
+	"github.com/pingcap/pd/v3/server/schedule/placement"
 	"github.com/pingcap/pd/v3/server/statistics"
 )
 
@@ -486,4 +487,58 @@ func (s *testShuffleRegionSuite) TestShuffle(c *C) {
 		c.Assert(op, NotNil)
 		c.Assert(op[0].Kind(), Equals, operator.OpRegion|operator.OpAdmin)
 	}
+}
+
+func (s *testShuffleRegionSuite) TestRole(c *C) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	opt := mockoption.NewScheduleOptions()
+	tc := mockcluster.NewCluster(opt)
+
+	// update rule to 1leader+1follower+1learner
+	opt.EnablePlacementRules = true
+	tc.RuleManager.SetRule(&placement.Rule{
+		GroupID: "pd",
+		ID:      "default",
+		Role:    placement.Voter,
+		Count:   2,
+	})
+	tc.RuleManager.SetRule(&placement.Rule{
+		GroupID: "pd",
+		ID:      "learner",
+		Role:    placement.Learner,
+		Count:   1,
+	})
+
+	// Add stores 1, 2, 3, 4
+	tc.AddRegionStore(1, 6)
+	tc.AddRegionStore(2, 7)
+	tc.AddRegionStore(3, 8)
+	tc.AddRegionStore(4, 9)
+
+	// Put a region with 1leader + 1follower + 1learner
+	peers := []*metapb.Peer{
+		{Id: 1, StoreId: 1},
+		{Id: 2, StoreId: 2},
+		{Id: 3, StoreId: 3, IsLearner: true},
+	}
+	region := core.NewRegionInfo(&metapb.Region{
+		Id:          1,
+		RegionEpoch: &metapb.RegionEpoch{ConfVer: 1, Version: 1},
+		Peers:       peers,
+	}, peers[0])
+	tc.PutRegion(region)
+
+	sl, err := schedule.CreateScheduler(ShuffleRegionType, schedule.NewOperatorController(ctx, nil, nil), core.NewStorage(kv.NewMemoryKV()), schedule.ConfigSliceDecoder(ShuffleRegionType, []string{"", ""}))
+	c.Assert(err, IsNil)
+
+	conf := sl.(*shuffleRegionScheduler).conf
+	conf.Roles = []string{"follower"}
+	ops := sl.Schedule(tc)
+	c.Assert(ops, HasLen, 1)
+	testutil.CheckTransferPeer(c, ops[0], operator.OpRegion, 2, 4) // transfer follower
+	conf.Roles = []string{"learner"}
+	ops = sl.Schedule(tc)
+	c.Assert(ops, HasLen, 1)
+	testutil.CheckTransferLearner(c, ops[0], operator.OpRegion, 3, 4) // transfer learner
 }

server/schedulers/shuffle_region_config.go
@@ -1,0 +1,112 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schedulers
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/pingcap/pd/v3/pkg/apiutil"
+	"github.com/pingcap/pd/v3/pkg/slice"
+	"github.com/pingcap/pd/v3/server/core"
+	"github.com/pingcap/pd/v3/server/schedule"
+	"github.com/pingcap/pd/v3/server/schedule/placement"
+	"github.com/unrolled/render"
+)
+
+const (
+	roleLeader   = string(placement.Leader)
+	roleFollower = string(placement.Follower)
+	roleLearner  = string(placement.Learner)
+)
+
+var allRoles = []string{roleLeader, roleFollower, roleLearner}
+
+type shuffleRegionSchedulerConfig struct {
+	sync.RWMutex
+	storage *core.Storage
+
+	Ranges []core.KeyRange `json:"ranges"`
+	Roles  []string        `json:"roles"` // can include `leader`, `follower`, `learner`.
+}
+
+func (conf *shuffleRegionSchedulerConfig) EncodeConfig() ([]byte, error) {
+	conf.RLock()
+	defer conf.RUnlock()
+	return schedule.EncodeConfig(conf)
+}
+
+func (conf *shuffleRegionSchedulerConfig) GetRoles() []string {
+	conf.RLock()
+	defer conf.RUnlock()
+	return conf.Roles
+}
+
+func (conf *shuffleRegionSchedulerConfig) GetRanges() []core.KeyRange {
+	conf.RLock()
+	defer conf.RUnlock()
+	return conf.Ranges
+}
+
+func (conf *shuffleRegionSchedulerConfig) IsRoleAllow(role string) bool {
+	conf.RLock()
+	defer conf.RUnlock()
+	return slice.AnyOf(conf.Roles, func(i int) bool { return conf.Roles[i] == role })
+}
+
+func (conf *shuffleRegionSchedulerConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/roles", conf.handleGetRoles).Methods("GET")
+	router.HandleFunc("/roles", conf.handleSetRoles).Methods("POST")
+	router.ServeHTTP(w, r)
+}
+
+func (conf *shuffleRegionSchedulerConfig) handleGetRoles(w http.ResponseWriter, r *http.Request) {
+	rd := render.New(render.Options{IndentJSON: true})
+	rd.JSON(w, http.StatusOK, conf.GetRoles())
+}
+
+func (conf *shuffleRegionSchedulerConfig) handleSetRoles(w http.ResponseWriter, r *http.Request) {
+	rd := render.New(render.Options{IndentJSON: true})
+	var roles []string
+	if err := apiutil.ReadJSONRespondError(rd, w, r.Body, &roles); err != nil {
+		return
+	}
+	for _, r := range roles {
+		if slice.NoneOf(allRoles, func(i int) bool { return allRoles[i] == r }) {
+			rd.Text(w, http.StatusBadRequest, "invalid role:"+r)
+			return
+		}
+	}
+
+	conf.Lock()
+	defer conf.Unlock()
+	old := conf.Roles
+	conf.Roles = roles
+	if err := conf.persist(); err != nil {
+		conf.Roles = old // revert
+		rd.Text(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	rd.Text(w, http.StatusOK, "")
+}
+
+func (conf *shuffleRegionSchedulerConfig) persist() error {
+	data, err := schedule.EncodeConfig(conf)
+	if err != nil {
+		return err
+	}
+	return conf.storage.SaveScheduleConfig(ShuffleRegionName, data)
+}