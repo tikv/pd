c5a5efeeb041e5c92105cbd761ce92a196c2b355
loxp <loxppqqxol@gmail.com>
2020-03-30 21:49:51 +0800

    remote useless code in docs.go

--- docs/swagger/docs.go
+++ docs/swagger/docs.go
@@ -4,57 +4,13 @@
 package swagger
 
 import (
-	"bytes"
-	"encoding/json"
-	"strings"
-
-	"github.com/alecthomas/template"
 	"github.com/swaggo/swag"
 )
-
-var doc = ""
-
-type swaggerInfo struct {
-	Version     string
-	Host        string
-	BasePath    string
-	Schemes     []string
-	Title       string
-	Description string
-}
-
-// SwaggerInfo holds exported Swagger Info so clients can modify it
-var SwaggerInfo = swaggerInfo{
-	Version:     "1.0",
-	Host:        "",
-	BasePath:    "/pd/api/v1",
-	Schemes:     []string{},
-	Title:       "Placement Driver Core API",
-	Description: "This is placement driver.",
-}
 
 type s struct{}
 
 func (s *s) ReadDoc() string {
-	sInfo := SwaggerInfo
-	sInfo.Description = strings.Replace(sInfo.Description, "\n", "\\n", -1)
-
-	t, err := template.New("swagger_info").Funcs(template.FuncMap{
-		"marshal": func(v interface{}) string {
-			a, _ := json.Marshal(v)
-			return string(a)
-		},
-	}).Parse(doc)
-	if err != nil {
-		return doc
-	}
-
-	var tpl bytes.Buffer
-	if err := t.Execute(&tpl, sInfo); err != nil {
-		return doc
-	}
-
-	return tpl.String()
+	return ""
 }
 
 func init() {
