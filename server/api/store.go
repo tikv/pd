pingcap/tidb-dashboard

Support rate control for removing peer (#2226)

server/api/store.go
@@ -28,6 +28,7 @@ import (
 	"github.com/pingcap/pd/v4/server/config"
 	"github.com/pingcap/pd/v4/server/core"
 	"github.com/pingcap/pd/v4/server/schedule"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 	"github.com/pkg/errors"
 	"github.com/unrolled/render"
 )
@@ -366,7 +367,13 @@ func (h *storeHandler) SetLimit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := h.SetStoreLimit(storeID, rate/schedule.StoreBalanceBaseTime); err != nil {
+	typeValue, err := getStoreLimitType(input)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.SetStoreLimit(storeID, rate/schedule.StoreBalanceBaseTime, typeValue); err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -430,7 +437,13 @@ func (h *storesHandler) SetAllLimit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := h.SetAllStoresLimit(rate / schedule.StoreBalanceBaseTime); err != nil {
+	typeValue, err := getStoreLimitType(input)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.SetAllStoresLimit(rate/schedule.StoreBalanceBaseTime, typeValue); err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -446,7 +459,13 @@ func (h *storesHandler) SetAllLimit(w http.ResponseWriter, r *http.Request) {
 // @Failure 500 {string} string "PD server failed to proceed the request."
 // @Router /stores/limit [get]
 func (h *storesHandler) GetAllLimit(w http.ResponseWriter, r *http.Request) {
-	limits, err := h.GetAllStoresLimit()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	limits, err := h.GetAllStoresLimit(typeValue)
 	if err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
@@ -469,18 +488,24 @@ func (h *storesHandler) GetAllLimit(w http.ResponseWriter, r *http.Request) {
 // @Tags store
 // @Summary Set limit scene in the cluster.
 // @Accept json
-// @Param body body schedule.StoreLimitScene true "Store limit scene"
+// @Param body body storelimit.Scene true "Store limit scene"
 // @Produce json
 // @Success 200 {string} string "Set store limit scene success."
 // @Failure 400 {string} string "The input is invalid."
 // @Failure 500 {string} string "PD server failed to proceed the request."
 // @Router /stores/limit/scene [post]
 func (h *storesHandler) SetStoreLimitScene(w http.ResponseWriter, r *http.Request) {
-	scene := h.Handler.GetStoreLimitScene()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	scene := h.Handler.GetStoreLimitScene(typeValue)
 	if err := apiutil.ReadJSONRespondError(h.rd, w, r.Body, &scene); err != nil {
 		return
 	}
-	h.Handler.SetStoreLimitScene(scene)
+	h.Handler.SetStoreLimitScene(scene, typeValue)
 	h.rd.JSON(w, http.StatusOK, nil)
 }
 
@@ -490,7 +515,13 @@ func (h *storesHandler) SetStoreLimitScene(w http.ResponseWriter, r *http.Reques
 // @Success 200 {string} string "Set store limit scene success."
 // @Router /stores/limit/scene [get]
 func (h *storesHandler) GetStoreLimitScene(w http.ResponseWriter, r *http.Request) {
-	scene := h.Handler.GetStoreLimitScene()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	scene := h.Handler.GetStoreLimitScene(typeValue)
 	h.rd.JSON(w, http.StatusOK, scene)
 }
 
@@ -574,4 +605,32 @@ func (filter *storeStateFilter) filter(stores []*metapb.Store) []*metapb.Store {
 		}
 	}
 	return ret
+}
+
+func getStoreLimitType(input map[string]interface{}) (storelimit.Type, error) {
+	typeNameIface, ok := input["type"]
+	typeValue := storelimit.RegionAdd
+	var err error
+	if ok {
+		typeName, ok := typeNameIface.(string)
+		if !ok {
+			err = errors.New("bad format type")
+		} else {
+			return parseStoreLimitType(typeName)
+		}
+	}
+	return typeValue, err
+}
+
+func parseStoreLimitType(typeName string) (storelimit.Type, error) {
+	typeValue := storelimit.RegionAdd
+	var err error
+	if typeName != "" {
+		if value, ok := storelimit.TypeNameValue[typeName]; ok {
+			typeValue = value
+		} else {
+			err = errors.New("unknown type")
+		}
+	}
+	return typeValue, err
 }

server/cluster/cluster.go
@@ -40,6 +40,7 @@ import (
 	"github.com/pingcap/pd/v4/server/schedule/checker"
 	"github.com/pingcap/pd/v4/server/schedule/opt"
 	"github.com/pingcap/pd/v4/server/schedule/placement"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 	"github.com/pingcap/pd/v4/server/statistics"
 	"github.com/pkg/errors"
 	"go.etcd.io/etcd/clientv3"
@@ -991,8 +992,8 @@ func (c *RaftCluster) UnblockStore(storeID uint64) {
 }
 
 // AttachAvailableFunc attaches an available function to a specific store.
-func (c *RaftCluster) AttachAvailableFunc(storeID uint64, f func() bool) {
-	c.core.AttachAvailableFunc(storeID, f)
+func (c *RaftCluster) AttachAvailableFunc(storeID uint64, limitType storelimit.Type, f func() bool) {
+	c.core.AttachAvailableFunc(storeID, limitType, f)
 }
 
 // SetConfigCheck sets a flag for preventing outdated config.

server/cluster/coordinator_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/pingcap/pd/v4/server/schedule"
 	"github.com/pingcap/pd/v4/server/schedule/operator"
 	"github.com/pingcap/pd/v4/server/schedule/opt"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 	"github.com/pingcap/pd/v4/server/schedulers"
 	"github.com/pingcap/pd/v4/server/statistics"
 )
@@ -936,7 +937,9 @@ func (s *testOperatorControllerSuite) TestStoreOverloaded(c *C) {
 
 	// reset all stores' limit
 	// scheduling one time needs 1/10 seconds
-	oc.SetAllStoresLimit(10, schedule.StoreLimitManual)
+	oc.SetAllStoresLimit(10, storelimit.Manual, storelimit.RegionAdd)
+	oc.SetAllStoresLimit(10, storelimit.Manual, storelimit.RegionRemove)
+
 	for i := 0; i < 10; i++ {
 		op1 := lb.Schedule(tc)[0]
 		c.Assert(op1, NotNil)

server/cluster/store_limiter.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/pdpb"
 	"github.com/pingcap/log"
 	"github.com/pingcap/pd/v4/server/schedule"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 	"go.uber.org/zap"
 )
 
@@ -26,17 +27,22 @@ import (
 type StoreLimiter struct {
 	m       sync.RWMutex
 	oc      *schedule.OperatorController
-	scene   *schedule.StoreLimitScene
+	scene   map[storelimit.Type]*storelimit.Scene
 	state   *State
 	current LoadState
 }
 
 // NewStoreLimiter builds a store limiter object using the operator controller
 func NewStoreLimiter(c *schedule.OperatorController) *StoreLimiter {
+	defaultScene := map[storelimit.Type]*storelimit.Scene{
+		storelimit.RegionAdd:    storelimit.DefaultScene(storelimit.RegionAdd),
+		storelimit.RegionRemove: storelimit.DefaultScene(storelimit.RegionRemove),
+	}
+
 	return &StoreLimiter{
 		oc:      c,
 		state:   NewState(),
-		scene:   schedule.DefaultStoreLimitScene(),
+		scene:   defaultScene,
 		current: LoadStateNone,
 	}
 }
@@ -49,22 +55,19 @@ func (s *StoreLimiter) Collect(stats *pdpb.StoreStats) {
 	log.Debug("collected statistics", zap.Reflect("stats", stats))
 	s.state.Collect((*StatEntry)(stats))
 
-	rate := float64(0)
 	state := s.state.State()
-	switch state {
-	case LoadStateIdle:
-		rate = float64(s.scene.Idle) / schedule.StoreBalanceBaseTime
-	case LoadStateLow:
-		rate = float64(s.scene.Low) / schedule.StoreBalanceBaseTime
-	case LoadStateNormal:
-		rate = float64(s.scene.Normal) / schedule.StoreBalanceBaseTime
-	case LoadStateHigh:
-		rate = float64(s.scene.High) / schedule.StoreBalanceBaseTime
-	}
+	rateRegionAdd := s.calculateRate(storelimit.RegionAdd, state)
+	rateRegionRemove := s.calculateRate(storelimit.RegionRemove, state)
 
-	if rate > 0 {
-		s.oc.SetAllStoresLimitAuto(rate)
-		log.Info("change store limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rate))
+	if rateRegionAdd > 0 || rateRegionRemove > 0 {
+		if rateRegionAdd > 0 {
+			s.oc.SetAllStoresLimitAuto(rateRegionAdd, storelimit.RegionAdd)
+			log.Info("change store region add limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rateRegionAdd))
+		}
+		if rateRegionRemove > 0 {
+			s.oc.SetAllStoresLimitAuto(rateRegionAdd, storelimit.RegionRemove)
+			log.Info("change store region remove limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rateRegionRemove))
+		}
 		s.current = state
 		collectClusterStateCurrent(state)
 	}
@@ -80,16 +83,34 @@ func collectClusterStateCurrent(state LoadState) {
 	}
 }
 
+func (s *StoreLimiter) calculateRate(limitType storelimit.Type, state LoadState) float64 {
+	rate := float64(0)
+	switch state {
+	case LoadStateIdle:
+		rate = float64(s.scene[limitType].Idle) / schedule.StoreBalanceBaseTime
+	case LoadStateLow:
+		rate = float64(s.scene[limitType].Low) / schedule.StoreBalanceBaseTime
+	case LoadStateNormal:
+		rate = float64(s.scene[limitType].Normal) / schedule.StoreBalanceBaseTime
+	case LoadStateHigh:
+		rate = float64(s.scene[limitType].High) / schedule.StoreBalanceBaseTime
+	}
+	return rate
+}
+
 // ReplaceStoreLimitScene replaces the store limit values for different scenes
-func (s *StoreLimiter) ReplaceStoreLimitScene(scene *schedule.StoreLimitScene) {
+func (s *StoreLimiter) ReplaceStoreLimitScene(scene *storelimit.Scene, limitType storelimit.Type) {
 	s.m.Lock()
 	defer s.m.Unlock()
-	s.scene = scene
+	if s.scene == nil {
+		s.scene = make(map[storelimit.Type]*storelimit.Scene)
+	}
+	s.scene[limitType] = scene
 }
 
 // StoreLimitScene returns the current limit for different scenes
-func (s *StoreLimiter) StoreLimitScene() *schedule.StoreLimitScene {
+func (s *StoreLimiter) StoreLimitScene(limitType storelimit.Type) *storelimit.Scene {
 	s.m.RLock()
 	defer s.m.RUnlock()
-	return s.scene
+	return s.scene[limitType]
 }

server/cluster/store_limiter_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/pingcap/pd/v4/pkg/mock/mockcluster"
 	"github.com/pingcap/pd/v4/pkg/mock/mockoption"
 	"github.com/pingcap/pd/v4/server/schedule"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 )
 
 var _ = Suite(&testStoreLimiterSuite{})
@@ -52,14 +53,18 @@ func (s *testStoreLimiterSuite) TestCollect(c *C) {
 
 func (s *testStoreLimiterSuite) TestStoreLimitScene(c *C) {
 	limiter := NewStoreLimiter(s.oc)
-	c.Assert(limiter.scene, DeepEquals, schedule.DefaultStoreLimitScene())
+	c.Assert(limiter.scene[storelimit.RegionAdd], DeepEquals, storelimit.DefaultScene(storelimit.RegionAdd))
+	c.Assert(limiter.scene[storelimit.RegionRemove], DeepEquals, storelimit.DefaultScene(storelimit.RegionRemove))
 }
 
 func (s *testStoreLimiterSuite) TestReplaceStoreLimitScene(c *C) {
 	limiter := NewStoreLimiter(s.oc)
 
-	scene := &schedule.StoreLimitScene{Idle: 4, Low: 3, Normal: 2, High: 1}
-	limiter.ReplaceStoreLimitScene(scene)
+	sceneRegionAdd := &storelimit.Scene{Idle: 4, Low: 3, Normal: 2, High: 1}
+	limiter.ReplaceStoreLimitScene(sceneRegionAdd, storelimit.RegionAdd)
 
-	c.Assert(limiter.scene, DeepEquals, scene)
+	c.Assert(limiter.scene[storelimit.RegionAdd], DeepEquals, sceneRegionAdd)
+
+	sceneRegionRemove := &storelimit.Scene{Idle: 5, Low: 4, Normal: 3, High: 2}
+	limiter.ReplaceStoreLimitScene(sceneRegionRemove, storelimit.RegionRemove)
 }

server/core/basic_cluster.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/log"
 	"github.com/pingcap/pd/v4/pkg/slice"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 	"go.uber.org/zap"
 )
 
@@ -141,10 +142,10 @@ func (bc *BasicCluster) UnblockStore(storeID uint64) {
 }
 
 // AttachAvailableFunc attaches an available function to a specific store.
-func (bc *BasicCluster) AttachAvailableFunc(storeID uint64, f func() bool) {
+func (bc *BasicCluster) AttachAvailableFunc(storeID uint64, limitType storelimit.Type, f func() bool) {
 	bc.Lock()
 	defer bc.Unlock()
-	bc.Stores.AttachAvailableFunc(storeID, f)
+	bc.Stores.AttachAvailableFunc(storeID, limitType, f)
 }
 
 // UpdateStoreStatus updates the information of the store.
@@ -396,7 +397,7 @@ type StoreSetController interface {
 	BlockStore(id uint64) error
 	UnblockStore(id uint64)
 
-	AttachAvailableFunc(id uint64, f func() bool)
+	AttachAvailableFunc(id uint64, limitType storelimit.Type, f func() bool)
 }
 
 // KeyRange is a key range.

server/schedule/operator/operator_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/pingcap/pd/v4/pkg/mock/mockoption"
 	"github.com/pingcap/pd/v4/server/core"
 	"github.com/pingcap/pd/v4/server/schedule/opt"
+	"github.com/pingcap/pd/v4/server/schedule/storelimit"
 )
 
 func Test(t *testing.T) {
@@ -155,7 +156,7 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: 0,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	TransferLeader{FromStore: 1, ToStore: 2}.Influence(opInfluence, region)
@@ -164,14 +165,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  0,
 		RegionCount: 0,
-		StepCost:    0,
+		StepCost:    nil,
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	RemovePeer{FromStore: 1}.Influence(opInfluence, region)
@@ -180,14 +181,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -50,
 		RegionCount: -1,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	MergeRegion{IsPassive: false}.Influence(opInfluence, region)
@@ -196,14 +197,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -50,
 		RegionCount: -1,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	MergeRegion{IsPassive: true}.Influence(opInfluence, region)
@@ -212,14 +213,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -2,
 		RegionSize:  -50,
 		RegionCount: -2,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 0,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 }
 

tikv/pd

server/api/store.go
@@ -28,6 +28,7 @@ import (
 	"github.com/pingcap/pd/v3/server/config"
 	"github.com/pingcap/pd/v3/server/core"
 	"github.com/pingcap/pd/v3/server/schedule"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 	"github.com/pkg/errors"
 	"github.com/unrolled/render"
 )
@@ -307,7 +308,13 @@ func (h *storeHandler) SetLimit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := h.SetStoreLimit(storeID, rate/schedule.StoreBalanceBaseTime); err != nil {
+	typeValue, err := getStoreLimitType(input)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.SetStoreLimit(storeID, rate/schedule.StoreBalanceBaseTime, typeValue); err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -355,7 +362,13 @@ func (h *storesHandler) SetAllLimit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := h.SetAllStoresLimit(rate / schedule.StoreBalanceBaseTime); err != nil {
+	typeValue, err := getStoreLimitType(input)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.SetAllStoresLimit(rate/schedule.StoreBalanceBaseTime, typeValue); err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -364,7 +377,13 @@ func (h *storesHandler) SetAllLimit(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *storesHandler) GetAllLimit(w http.ResponseWriter, r *http.Request) {
-	limits, err := h.GetAllStoresLimit()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	limits, err := h.GetAllStoresLimit(typeValue)
 	if err != nil {
 		h.rd.JSON(w, http.StatusInternalServerError, err.Error())
 		return
@@ -385,16 +404,28 @@ func (h *storesHandler) GetAllLimit(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *storesHandler) SetStoreLimitScene(w http.ResponseWriter, r *http.Request) {
-	scene := h.Handler.GetStoreLimitScene()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	scene := h.Handler.GetStoreLimitScene(typeValue)
 	if err := apiutil.ReadJSONRespondError(h.rd, w, r.Body, &scene); err != nil {
 		return
 	}
-	h.Handler.SetStoreLimitScene(scene)
+	h.Handler.SetStoreLimitScene(scene, typeValue)
 	h.rd.JSON(w, http.StatusOK, nil)
 }
 
 func (h *storesHandler) GetStoreLimitScene(w http.ResponseWriter, r *http.Request) {
-	scene := h.Handler.GetStoreLimitScene()
+	typeName := r.URL.Query().Get("type")
+	typeValue, err := parseStoreLimitType(typeName)
+	if err != nil {
+		h.rd.JSON(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	scene := h.Handler.GetStoreLimitScene(typeValue)
 	h.rd.JSON(w, http.StatusOK, scene)
 }
 
@@ -471,4 +502,32 @@ func (filter *storeStateFilter) filter(stores []*metapb.Store) []*metapb.Store {
 		}
 	}
 	return ret
+}
+
+func getStoreLimitType(input map[string]interface{}) (storelimit.Type, error) {
+	typeNameIface, ok := input["type"]
+	typeValue := storelimit.RegionAdd
+	var err error
+	if ok {
+		typeName, ok := typeNameIface.(string)
+		if !ok {
+			err = errors.New("bad format type")
+		} else {
+			return parseStoreLimitType(typeName)
+		}
+	}
+	return typeValue, err
+}
+
+func parseStoreLimitType(typeName string) (storelimit.Type, error) {
+	typeValue := storelimit.RegionAdd
+	var err error
+	if typeName != "" {
+		if value, ok := storelimit.TypeNameValue[typeName]; ok {
+			typeValue = value
+		} else {
+			err = errors.New("unknown type")
+		}
+	}
+	return typeValue, err
 }

server/cluster/cluster.go
@@ -38,6 +38,7 @@ import (
 	"github.com/pingcap/pd/v3/server/schedule/checker"
 	"github.com/pingcap/pd/v3/server/schedule/opt"
 	"github.com/pingcap/pd/v3/server/schedule/placement"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 	"github.com/pingcap/pd/v3/server/statistics"
 	"github.com/pkg/errors"
 	"go.etcd.io/etcd/clientv3"
@@ -941,8 +942,8 @@ func (c *RaftCluster) UnblockStore(storeID uint64) {
 }
 
 // AttachAvailableFunc attaches an available function to a specific store.
-func (c *RaftCluster) AttachAvailableFunc(storeID uint64, f func() bool) {
-	c.core.AttachAvailableFunc(storeID, f)
+func (c *RaftCluster) AttachAvailableFunc(storeID uint64, limitType storelimit.Type, f func() bool) {
+	c.core.AttachAvailableFunc(storeID, limitType, f)
 }
 
 // SetStoreState sets up a store's state.

server/cluster/coordinator_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/pingcap/pd/v3/server/schedule"
 	"github.com/pingcap/pd/v3/server/schedule/operator"
 	"github.com/pingcap/pd/v3/server/schedule/opt"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 	"github.com/pingcap/pd/v3/server/schedulers"
 	"github.com/pingcap/pd/v3/server/statistics"
 )
@@ -932,7 +933,9 @@ func (s *testOperatorControllerSuite) TestStoreOverloaded(c *C) {
 
 	// reset all stores' limit
 	// scheduling one time needs 1/10 seconds
-	oc.SetAllStoresLimit(10, schedule.StoreLimitManual)
+	oc.SetAllStoresLimit(10, storelimit.Manual, storelimit.RegionAdd)
+	oc.SetAllStoresLimit(10, storelimit.Manual, storelimit.RegionRemove)
+
 	for i := 0; i < 10; i++ {
 		op1 := lb.Schedule(tc)[0]
 		c.Assert(op1, NotNil)

server/cluster/store_limiter.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/pdpb"
 	"github.com/pingcap/log"
 	"github.com/pingcap/pd/v3/server/schedule"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 	"go.uber.org/zap"
 )
 
@@ -26,17 +27,22 @@ import (
 type StoreLimiter struct {
 	m       sync.RWMutex
 	oc      *schedule.OperatorController
-	scene   *schedule.StoreLimitScene
+	scene   map[storelimit.Type]*storelimit.Scene
 	state   *State
 	current LoadState
 }
 
 // NewStoreLimiter builds a store limiter object using the operator controller
 func NewStoreLimiter(c *schedule.OperatorController) *StoreLimiter {
+	defaultScene := map[storelimit.Type]*storelimit.Scene{
+		storelimit.RegionAdd:    storelimit.DefaultScene(storelimit.RegionAdd),
+		storelimit.RegionRemove: storelimit.DefaultScene(storelimit.RegionRemove),
+	}
+
 	return &StoreLimiter{
 		oc:      c,
 		state:   NewState(),
-		scene:   schedule.DefaultStoreLimitScene(),
+		scene:   defaultScene,
 		current: LoadStateNone,
 	}
 }
@@ -49,22 +55,19 @@ func (s *StoreLimiter) Collect(stats *pdpb.StoreStats) {
 	log.Debug("collected statistics", zap.Reflect("stats", stats))
 	s.state.Collect((*StatEntry)(stats))
 
-	rate := float64(0)
 	state := s.state.State()
-	switch state {
-	case LoadStateIdle:
-		rate = float64(s.scene.Idle) / schedule.StoreBalanceBaseTime
-	case LoadStateLow:
-		rate = float64(s.scene.Low) / schedule.StoreBalanceBaseTime
-	case LoadStateNormal:
-		rate = float64(s.scene.Normal) / schedule.StoreBalanceBaseTime
-	case LoadStateHigh:
-		rate = float64(s.scene.High) / schedule.StoreBalanceBaseTime
-	}
+	rateRegionAdd := s.calculateRate(storelimit.RegionAdd, state)
+	rateRegionRemove := s.calculateRate(storelimit.RegionRemove, state)
 
-	if rate > 0 {
-		s.oc.SetAllStoresLimitAuto(rate)
-		log.Info("change store limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rate))
+	if rateRegionAdd > 0 || rateRegionRemove > 0 {
+		if rateRegionAdd > 0 {
+			s.oc.SetAllStoresLimitAuto(rateRegionAdd, storelimit.RegionAdd)
+			log.Info("change store region add limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rateRegionAdd))
+		}
+		if rateRegionRemove > 0 {
+			s.oc.SetAllStoresLimitAuto(rateRegionAdd, storelimit.RegionRemove)
+			log.Info("change store region remove limit for cluster", zap.Stringer("state", state), zap.Float64("rate", rateRegionRemove))
+		}
 		s.current = state
 		collectClusterStateCurrent(state)
 	}
@@ -80,16 +83,34 @@ func collectClusterStateCurrent(state LoadState) {
 	}
 }
 
+func (s *StoreLimiter) calculateRate(limitType storelimit.Type, state LoadState) float64 {
+	rate := float64(0)
+	switch state {
+	case LoadStateIdle:
+		rate = float64(s.scene[limitType].Idle) / schedule.StoreBalanceBaseTime
+	case LoadStateLow:
+		rate = float64(s.scene[limitType].Low) / schedule.StoreBalanceBaseTime
+	case LoadStateNormal:
+		rate = float64(s.scene[limitType].Normal) / schedule.StoreBalanceBaseTime
+	case LoadStateHigh:
+		rate = float64(s.scene[limitType].High) / schedule.StoreBalanceBaseTime
+	}
+	return rate
+}
+
 // ReplaceStoreLimitScene replaces the store limit values for different scenes
-func (s *StoreLimiter) ReplaceStoreLimitScene(scene *schedule.StoreLimitScene) {
+func (s *StoreLimiter) ReplaceStoreLimitScene(scene *storelimit.Scene, limitType storelimit.Type) {
 	s.m.Lock()
 	defer s.m.Unlock()
-	s.scene = scene
+	if s.scene == nil {
+		s.scene = make(map[storelimit.Type]*storelimit.Scene)
+	}
+	s.scene[limitType] = scene
 }
 
 // StoreLimitScene returns the current limit for different scenes
-func (s *StoreLimiter) StoreLimitScene() *schedule.StoreLimitScene {
+func (s *StoreLimiter) StoreLimitScene(limitType storelimit.Type) *storelimit.Scene {
 	s.m.RLock()
 	defer s.m.RUnlock()
-	return s.scene
+	return s.scene[limitType]
 }

server/cluster/store_limiter_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/pingcap/pd/v3/pkg/mock/mockcluster"
 	"github.com/pingcap/pd/v3/pkg/mock/mockoption"
 	"github.com/pingcap/pd/v3/server/schedule"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 )
 
 var _ = Suite(&testStoreLimiterSuite{})
@@ -52,14 +53,18 @@ func (s *testStoreLimiterSuite) TestCollect(c *C) {
 
 func (s *testStoreLimiterSuite) TestStoreLimitScene(c *C) {
 	limiter := NewStoreLimiter(s.oc)
-	c.Assert(limiter.scene, DeepEquals, schedule.DefaultStoreLimitScene())
+	c.Assert(limiter.scene[storelimit.RegionAdd], DeepEquals, storelimit.DefaultScene(storelimit.RegionAdd))
+	c.Assert(limiter.scene[storelimit.RegionRemove], DeepEquals, storelimit.DefaultScene(storelimit.RegionRemove))
 }
 
 func (s *testStoreLimiterSuite) TestReplaceStoreLimitScene(c *C) {
 	limiter := NewStoreLimiter(s.oc)
 
-	scene := &schedule.StoreLimitScene{Idle: 4, Low: 3, Normal: 2, High: 1}
-	limiter.ReplaceStoreLimitScene(scene)
+	sceneRegionAdd := &storelimit.Scene{Idle: 4, Low: 3, Normal: 2, High: 1}
+	limiter.ReplaceStoreLimitScene(sceneRegionAdd, storelimit.RegionAdd)
 
-	c.Assert(limiter.scene, DeepEquals, scene)
+	c.Assert(limiter.scene[storelimit.RegionAdd], DeepEquals, sceneRegionAdd)
+
+	sceneRegionRemove := &storelimit.Scene{Idle: 5, Low: 4, Normal: 3, High: 2}
+	limiter.ReplaceStoreLimitScene(sceneRegionRemove, storelimit.RegionRemove)
 }

server/core/basic_cluster.go
@@ -19,6 +19,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/log"
 	"github.com/pingcap/pd/v3/pkg/slice"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 	"go.uber.org/zap"
 )
 
@@ -141,10 +142,10 @@ func (bc *BasicCluster) UnblockStore(storeID uint64) {
 }
 
 // AttachAvailableFunc attaches an available function to a specific store.
-func (bc *BasicCluster) AttachAvailableFunc(storeID uint64, f func() bool) {
+func (bc *BasicCluster) AttachAvailableFunc(storeID uint64, limitType storelimit.Type, f func() bool) {
 	bc.Lock()
 	defer bc.Unlock()
-	bc.Stores.AttachAvailableFunc(storeID, f)
+	bc.Stores.AttachAvailableFunc(storeID, limitType, f)
 }
 
 // UpdateStoreStatus updates the information of the store.
@@ -396,7 +397,7 @@ type StoreSetController interface {
 	BlockStore(id uint64) error
 	UnblockStore(id uint64)
 
-	AttachAvailableFunc(id uint64, f func() bool)
+	AttachAvailableFunc(id uint64, limitType storelimit.Type, f func() bool)
 }
 
 // KeyRange is a key range.

server/schedule/operator/operator_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/pingcap/pd/v3/pkg/mock/mockoption"
 	"github.com/pingcap/pd/v3/server/core"
 	"github.com/pingcap/pd/v3/server/schedule/opt"
+	"github.com/pingcap/pd/v3/server/schedule/storelimit"
 )
 
 func Test(t *testing.T) {
@@ -155,7 +156,7 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: 0,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	TransferLeader{FromStore: 1, ToStore: 2}.Influence(opInfluence, region)
@@ -164,14 +165,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  0,
 		RegionCount: 0,
-		StepCost:    0,
+		StepCost:    nil,
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	RemovePeer{FromStore: 1}.Influence(opInfluence, region)
@@ -180,14 +181,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -50,
 		RegionCount: -1,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	MergeRegion{IsPassive: false}.Influence(opInfluence, region)
@@ -196,14 +197,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -1,
 		RegionSize:  -50,
 		RegionCount: -1,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 1,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 
 	MergeRegion{IsPassive: true}.Influence(opInfluence, region)
@@ -212,14 +213,14 @@ func (s *testOperatorSuite) TestInfluence(c *C) {
 		LeaderCount: -2,
 		RegionSize:  -50,
 		RegionCount: -2,
-		StepCost:    0,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionRemove: 1000},
 	})
 	c.Assert(*storeOpInfluence[2], DeepEquals, StoreInfluence{
 		LeaderSize:  50,
 		LeaderCount: 1,
 		RegionSize:  50,
 		RegionCount: 0,
-		StepCost:    1000,
+		StepCost:    map[storelimit.Type]int64{storelimit.RegionAdd: 1000},
 	})
 }
 