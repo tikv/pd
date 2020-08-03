501e84b2d2dead91ed749e0098b76c34463f2bc8
JmPotato <ghzpotato@gmail.com>
2020-08-04 00:10:18 +0800

    Redefine the client errors
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- client/client.go
+++ client/client.go
@@ -180,7 +180,7 @@ func (c *client) tsCancelLoop() {
 		case d := <-c.tsDeadlineCh:
 			select {
 			case <-d.timer:
-				log.Error("tso request is canceled due to timeout", zap.Error(errs.ErrGRPCTso.FastGenByArgs()))
+				log.Error("tso request is canceled due to timeout", zap.Error(errs.ErrGetTSO.FastGenByArgs()))
 				d.cancel()
 			case <-d.done:
 			case <-ctx.Done():
@@ -235,7 +235,7 @@ func (c *client) tsLoop() {
 					return
 				default:
 				}
-				log.Error("[pd] create tso stream error", zap.Error(errs.ErrGRPCTso.FastGenByArgs()), zap.NamedError("cause", err))
+				log.Error("[pd] create tso stream error", zap.Error(errs.ErrCreateTSOStream.FastGenByArgs()), zap.NamedError("cause", err))
 				c.ScheduleCheckLeader()
 				cancel()
 				c.revokeTSORequest(errors.WithStack(err))
@@ -282,7 +282,7 @@ func (c *client) tsLoop() {
 				return
 			default:
 			}
-			log.Error("[pd] getTS error", zap.Error(errs.ErrGRPCTso.FastGenByArgs()), zap.NamedError("cause", err))
+			log.Error("[pd] getTS error", zap.Error(errs.ErrGetTSO.FastGenByArgs()), zap.NamedError("cause", err))
 			c.ScheduleCheckLeader()
 			cancel()
 			stream, cancel = nil, nil
@@ -379,7 +379,7 @@ func (c *client) Close() {
 	defer c.connMu.Unlock()
 	for _, cc := range c.connMu.clientConns {
 		if err := cc.Close(); err != nil {
-			log.Error("[pd] failed close grpc clientConn", zap.Error(errs.ErrGRPCClose.FastGenByArgs()), zap.NamedError("cause", err))
+			log.Error("[pd] failed to close gRPC clientConn", zap.Error(errs.ErrCloseGRPCConn.FastGenByArgs()), zap.NamedError("cause", err))
 		}
 	}
 }
