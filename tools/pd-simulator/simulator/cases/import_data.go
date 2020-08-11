3f488763cfeb7bcda900a5b8ace77d9c70e5a114
ZenoTan <zenotan1998@gmail.com>
2020-08-11 11:59:13 +0800

    Address comment

--- tools/pd-simulator/simulator/cases/import_data.go
+++ tools/pd-simulator/simulator/cases/import_data.go
@@ -40,7 +40,7 @@ func newImportData() *Case {
 		})
 	}
 
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < getRegionNum(); i++ {
 		storeIDs := rand.Perm(10)
 		peers := []*metapb.Peer{
 			{Id: IDAllocator.nextID(), StoreId: uint64(storeIDs[0] + 1)},
@@ -112,7 +112,7 @@ func newImportData() *Case {
 		regionTotal := regions.GetRegionCount()
 		totalLeaderLog := fmt.Sprintf("%d leader:", regionTotal)
 		totalPeerLog := fmt.Sprintf("%d peer:", regionTotal*3)
-		isEnd := checkCount > 1000
+		isEnd := checkCount > uint64(getRegionNum()) / 10
 		var regionProps []float64
 		for storeID := uint64(1); storeID <= 10; storeID++ {
 			regions.GetStoreRegionCount(storeID)
