6ecc96338c53a7acb7f66dbc228c5b7aa734e3c9
Song Gao <disxiaofei@163.com>
2020-09-14 15:25:09 +0800

    fix lint
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- server/schedule/placement/rule_manager.go
+++ server/schedule/placement/rule_manager.go
@@ -357,7 +357,7 @@ const (
 // distinguished by the field `Action`.
 type RuleOp struct {
 	*Rule                       // information of the placement rule to add/delete
-	Action           RuleOpType `json:"action"` // the operation type
+	Action           RuleOpType `json:"action"`              // the operation type
 	DeleteByIDPrefix bool       `json:"delete_by_id_prefix"` // if action == delete, delete by the prefix of id
 }
 
@@ -596,6 +596,7 @@ func (m *RuleManager) DeleteGroupBundle(id string, regex bool) error {
 	return nil
 }
 
+// IsInitialized returns whether the rule manager is initialized.
 func (m *RuleManager) IsInitialized() bool {
 	m.RLock()
 	defer m.RUnlock()
