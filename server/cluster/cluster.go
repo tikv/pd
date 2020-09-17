80db8d5a120e085ded933f74f0205ea85fbfdbc5
disksing <i@disksing.com>
2020-09-17 19:55:37 +0800

    cherry pick #2987 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -1226,6 +1226,7 @@ func (c *RaftCluster) resetMetrics() {
 	c.coordinator.resetSchedulerMetrics()
 	c.coordinator.resetHotSpotMetrics()
 	c.resetClusterMetrics()
+	c.resetHealthStatus()
 }
 
 func (c *RaftCluster) collectClusterMetrics() {
@@ -1257,14 +1258,26 @@ func (c *RaftCluster) collectHealthStatus() {
 	if err != nil {
 		log.Error("get members error", errs.ZapError(err))
 	}
+<<<<<<< HEAD
 	unhealth := CheckHealth(c.httpClient, members)
 	for _, member := range members {
 		if _, ok := unhealth[member.GetMemberId()]; ok {
 			healthStatusGauge.WithLabelValues(member.GetName()).Set(0)
 			continue
+=======
+	healthy := CheckHealth(c.httpClient, members)
+	for _, member := range members {
+		var v float64
+		if _, ok := healthy[member.GetMemberId()]; ok {
+			v = 1
+>>>>>>> 66c26169... cluster: fix health metrics (#2987)
 		}
-		healthStatusGauge.WithLabelValues(member.GetName()).Set(1)
+		healthStatusGauge.WithLabelValues(member.GetName()).Set(v)
 	}
+}
+
+func (c *RaftCluster) resetHealthStatus() {
+	healthStatusGauge.Reset()
 }
 
 // GetRegionStatsByType gets the status of the region by types.
