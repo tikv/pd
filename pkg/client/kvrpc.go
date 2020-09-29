c9b036fff74017ccc07b09798705d82598dfe4d0
Song Gao <disxiaofei@163.com>
2020-09-29 14:52:58 +0800

    fix lint
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- pkg/client/kvrpc.go
+++ pkg/client/kvrpc.go
@@ -28,6 +28,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
 // RegionRequestSender send the region request to Store.
 type RegionRequestSender struct {
 	client   rpc.Client
