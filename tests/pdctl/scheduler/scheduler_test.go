7d009a98ba93e842b6b88245c28eb0f14204988a
lhy1024 <admin@liudos.us>
2020-10-16 18:08:03 +0800

    fix test
    Signed-off-by: lhy1024 <admin@liudos.us>

--- tests/pdctl/scheduler/scheduler_test.go
+++ tests/pdctl/scheduler/scheduler_test.go
@@ -263,6 +263,7 @@ func (s *schedulerTestSuite) TestScheduler(c *C) {
 		"minor-dec-ratio":           0.99,
 		"src-tolerance-ratio":       1.05,
 		"dst-tolerance-ratio":       1.05,
+		"restart-interval":          10,
 	}
 	c.Assert(conf, DeepEquals, expected1)
 	mustExec([]string{"-u", pdAddr, "scheduler", "config", "balance-hot-region-scheduler", "set", "src-tolerance-ratio", "1.02"}, nil)
