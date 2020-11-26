848bdf198f34949c14f37664a8b2ebedb208285a
qupeng <qupeng@pingcap.com>
2020-11-26 15:19:58 +0800

    cherry pick #3196 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -1051,6 +1051,7 @@ func (c *RaftCluster) BuryStore(storeID uint64, force bool) error {
 		zap.Uint64("store-id", newStore.GetID()),
 		zap.String("store-address", newStore.GetAddress()))
 	err := c.putStoreLocked(newStore)
+	c.onStoreVersionChangeLocked()
 	if err == nil {
 		c.RemoveStoreLimit(storeID)
 	}
@@ -1304,11 +1305,19 @@ func (c *RaftCluster) AllocID() (uint64, error) {
 func (c *RaftCluster) OnStoreVersionChange() {
 	c.RLock()
 	defer c.RUnlock()
+<<<<<<< HEAD
 	var (
 		minVersion     *semver.Version
 		clusterVersion *semver.Version
 	)
 
+=======
+	c.onStoreVersionChangeLocked()
+}
+
+func (c *RaftCluster) onStoreVersionChangeLocked() {
+	var minVersion *semver.Version
+>>>>>>> 7445c235... check cluster version on store tombstone (#3196)
 	stores := c.GetStores()
 	for _, s := range stores {
 		if s.IsTombstone() {
