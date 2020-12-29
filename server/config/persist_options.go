25b8fa95d77866f32b5785a9c0841b6d9957530a
Zheng Xiangsheng <hundundm@gmail.com>
2020-12-29 12:48:37 +0800

    fix test
    Signed-off-by: Zheng Xiangsheng <hundundm@gmail.com>

--- server/config/persist_options.go
+++ server/config/persist_options.go
@@ -490,6 +490,12 @@ func (o *PersistOptions) IsUseJointConsensus() bool {
 	return o.GetScheduleConfig().EnableJointConsensus
 }
 
+func (o *PersistOptions) SetEnableJointConsensus(enableJointConsensus bool) {
+	v := o.GetScheduleConfig().Clone()
+	v.EnableJointConsensus = enableJointConsensus
+	o.SetScheduleConfig(v)
+}
+
 // GetHotRegionCacheHitsThreshold is a threshold to decide if a region is hot.
 func (o *PersistOptions) GetHotRegionCacheHitsThreshold() int {
 	return int(o.GetScheduleConfig().HotRegionCacheHitsThreshold)
