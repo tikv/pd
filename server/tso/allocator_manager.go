3495b1b9f385ae4631ae845fd4114ec595a6f1d5
Song Gao <disxiaofei@163.com>
2021-02-19 14:07:35 +0800

    fix lint
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- server/tso/allocator_manager.go
+++ server/tso/allocator_manager.go
@@ -185,7 +185,7 @@ func (am *AllocatorManager) SetLocalTSOConfig(dcLocation string) error {
 }
 
 func (am *AllocatorManager) checkDCLocationUpperLimit(dcLocation string) error {
-	clusterDCLocations, err := am.getClusterDCLocationsFromEtcd()
+	clusterDCLocations, err := am.GetClusterDCLocationsFromEtcd()
 	if err != nil {
 		return err
 	}
@@ -201,7 +201,8 @@ func (am *AllocatorManager) checkDCLocationUpperLimit(dcLocation string) error {
 	return nil
 }
 
-func (am *AllocatorManager) getClusterDCLocationsFromEtcd() (clusterDCLocations map[string][]uint64, err error) {
+// GetClusterDCLocationsFromEtcd fetches dcLocation topology from etcd
+func (am *AllocatorManager) GetClusterDCLocationsFromEtcd() (clusterDCLocations map[string][]uint64, err error) {
 	resp, err := etcdutil.EtcdKVGet(
 		am.member.Client(),
 		am.member.GetDCLocationPathPrefix(),
@@ -620,7 +621,7 @@ func (am *AllocatorManager) ClusterDCLocationChecker() {
 	if am.member.GetLeader() == nil {
 		return
 	}
-	newClusterDCLocations, err := am.getClusterDCLocationsFromEtcd()
+	newClusterDCLocations, err := am.GetClusterDCLocationsFromEtcd()
 	if err != nil {
 		log.Error("get cluster dc-locations from etcd failed", errs.ZapError(err))
 		return
@@ -1133,9 +1134,4 @@ func (am *AllocatorManager) transferLocalAllocator(dcLocation string, serverID u
 
 func (am *AllocatorManager) nextLeaderKey(dcLocation string) string {
 	return path.Join(am.rootPath, dcLocation, "next-leader")
-}
-
-// GetClusterDCLocationsFromEtcd fetches the dcLocation topology from etcd
-func (am *AllocatorManager) GetClusterDCLocationsFromEtcd() (map[string][]uint64, error) {
-	return am.getClusterDCLocationsFromEtcd()
 }
