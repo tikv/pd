6066989d3f3ca8b282427fc97ae3ac7c3f162be3
JmPotato <ghzpotato@gmail.com>
2021-02-20 10:57:51 +0800

    Address the comment
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- server/server.go
+++ server/server.go
@@ -1206,12 +1206,12 @@ func (s *Server) campaignLeader() {
 	go s.member.KeepLeader(ctx)
 	log.Info("campaign pd leader ok", zap.String("campaign-pd-leader-name", s.Name()))
 
-	log.Info("initializing the global TSO allocator")
 	alllocator, err := s.tsoAllocatorManager.GetAllocator(tso.GlobalDCLocation)
 	if err != nil {
 		log.Error("failed to get the global TSO allocator", errs.ZapError(err))
 		return
 	}
+	log.Info("initializing the global TSO allocator")
 	if err := alllocator.Initialize(0); err != nil {
 		log.Error("failed to initialize the global TSO allocator", errs.ZapError(err))
 		return
