24c2a4791938d540ef11345fe87294c8928c7dad
JmPotato <ghzpotato@gmail.com>
2021-04-25 21:04:48 +0800

    Add more comments
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- server/tso/tso.go
+++ server/tso/tso.go
@@ -33,7 +33,7 @@ import (
 
 const (
 	timestampKey = "timestamp"
-	// updateTimestampGuard is the min physical timestamp interval.
+	// updateTimestampGuard is the min physical timestamp updating interval for the TSO.
 	updateTimestampGuard = time.Millisecond
 	// maxLogical is the max upper limit for logical time.
 	// When a TSO's logical time reaches this limit,
@@ -49,9 +49,15 @@ type tsoObject struct {
 =======
 	sync.RWMutex
 	// time.Time has a minimum unit of nanoseconds,
+<<<<<<< HEAD
 	// however the minimum unit of the physical part of TSO is milliseconds,
 	// so you MUST use updateTimestampGuard or millisecond as the minimum unit when comparing the Duration.
 >>>>>>> Fix the TSO fallback caused by resetUserTimestamp
+=======
+	// however the minimum unit of the physical part of TSO is one millisecond, a.k.a 1ms,
+	// so you MUST use millisecond as the minimum unit when comparing two TSOs' physical times,
+	// and `typeutil.SubTSOPhysicalByWallClock` is used to do this SPECIFICALLY.
+>>>>>>> Add more comments
 	physical time.Time
 	logical  int64
 }
