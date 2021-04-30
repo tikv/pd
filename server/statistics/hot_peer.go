5e25d31ab14270c33d5f30439194f91aa8c1d513
yisaer <disxiaofei@163.com>
2021-04-30 14:14:09 +0800

    revise
    Signed-off-by: yisaer <disxiaofei@163.com>

--- server/statistics/hot_peer.go
+++ server/statistics/hot_peer.go
@@ -35,8 +35,8 @@ type dimStat struct {
 	LastAverage *movingaverage.AvgOverTime // it's used to obtain the average speed in last second as instantaneous speed.
 }
 
-func newDimStat(typ int) *dimStat {
-	reportInterval := HotStatReportInterval * time.Second
+func newDimStat(typ RegionStatKind) *dimStat {
+	reportInterval := RegionHeartBeatReportInterval * time.Second
 	return &dimStat{
 		typ:         typ,
 		Rolling:     movingaverage.NewTimeMedian(DefaultAotSize, rollingWindowsSize, reportInterval),
