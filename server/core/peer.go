27ecc5dc9c4fbe00c99ba60b567307da8bc155d2
yisaer <disxiaofei@163.com>
2021-05-07 14:57:21 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- server/core/peer.go
+++ server/core/peer.go
@@ -75,8 +75,7 @@ func CountInJointState(peers ...*metapb.Peer) int {
 
 // PeerInfo provides peer information
 type PeerInfo struct {
-	peerID       uint64
-	storeID      uint64
+	meta         *metapb.Peer
 	writtenBytes uint64
 	writtenKeys  uint64
 	readBytes    uint64
@@ -105,19 +104,18 @@ func (p *PeerInfo) GetKeysRead() uint64 {
 
 // GetStoreID provides located storeID
 func (p *PeerInfo) GetStoreID() uint64 {
-	return p.storeID
+	return p.meta.StoreId
 }
 
 // GetPeerID provides peer id
 func (p *PeerInfo) GetPeerID() uint64 {
-	return p.peerID
+	return p.meta.GetId()
 }
 
 // FromMetaPeer provides PeerInfo from metapb.Peer
 func FromMetaPeer(peer *metapb.Peer) *PeerInfo {
 	return &PeerInfo{
-		peerID:  peer.GetId(),
-		storeID: peer.GetStoreId(),
+		meta: peer,
 	}
 }
 
