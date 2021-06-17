30d4cc562b430e1accaafdcd4efa54533aabb70d
Ryan Leung <rleungx@gmail.com>
2021-06-17 12:53:50 +0800

    rollback the scheduler when failing to persist
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/cluster/coordinator.go
+++ server/cluster/coordinator.go
@@ -605,7 +605,6 @@ func (c *coordinator) removeScheduler(name string) error {
 
 	s.Stop()
 	schedulerStatusGauge.WithLabelValues(name, "allow").Set(0)
-	delete(c.schedulers, name)
 
 	var err error
 	opt := c.cluster.opt
@@ -624,6 +623,7 @@ func (c *coordinator) removeScheduler(name string) error {
 		log.Error("can not remove the scheduler config", errs.ZapError(err))
 		return err
 	}
+	delete(c.schedulers, name)
 
 	return nil
 }
