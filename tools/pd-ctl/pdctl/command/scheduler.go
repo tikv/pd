fcd72d2c6e14de04fc6ee1857b9ab6cb8440c385
yisaer <disxiaofei@163.com>
2021-07-22 13:35:52 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- tools/pd-ctl/pdctl/command/scheduler.go
+++ tools/pd-ctl/pdctl/command/scheduler.go
@@ -530,8 +530,7 @@ func postSchedulerConfigCommandFunc(cmd *cobra.Command, schedulerName string, ar
 	}
 	if schedulerName == "balance-hot-region-scheduler" && (key == "read-priorities" || key == "write-priorities") {
 		priorities := make([]string, 0)
-		if value == "none" {
-		} else {
+		if value != "none" {
 			for _, priority := range strings.Split(value, ",") {
 				if priority != schedulers.BytePriority && priority != schedulers.KeyPriority {
 					cmd.Println(fmt.Sprintf("priority should be one of %s,%s,%s",
