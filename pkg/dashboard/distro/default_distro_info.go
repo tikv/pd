d393401d75e289f43368f625cdfe4cb1b37cf2b5
Suhaha <jklopsdfw@gmail.com>
2021-07-24 18:32:04 +0800

    Update to TiDB Dashboard v2021.07.24.1
    Signed-off-by: Suhaha <jklopsdfw@gmail.com>

--- pkg/dashboard/distro/default_distro_info.go
+++ pkg/dashboard/distro/default_distro_info.go
@@ -1,9 +1,9 @@
-// +build !distro
+// +build !dashboard_distro
 
 package distro
 
 // Resource declared the distro brand information
-var Resource = map[string]interface{}{
+var Resource = map[string]string{
 	"tidb":    "TiDB",
 	"tikv":    "TiKV",
 	"tiflash": "TiFlash",
