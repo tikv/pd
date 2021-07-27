5806fb911550d25dea12bfcf598809bf617da9aa
disksing <i@disksing.com>
2021-07-27 16:00:16 +0800

    This is an automated cherry-pick of #3892
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- server/server.go
+++ server/server.go
@@ -22,7 +22,6 @@ import (
 	"net/http"
 	"path"
 	"path/filepath"
-	"reflect"
 	"strconv"
 	"strings"
 	"sync"
@@ -859,15 +858,19 @@ func (s *Server) SetReplicationConfig(cfg config.ReplicationConfig) error {
 				rules[0].GroupID == "pd" && rules[0].ID == "default") {
 				return errors.New("cannot update MaxReplicas or LocationLabels when placement rules feature is enabled and not only default rule exists, please update rule instead")
 			}
+<<<<<<< HEAD
 			rule = rules[0]
 			if !(rule.Count == int(old.MaxReplicas) && reflect.DeepEqual(rule.LocationLabels, []string(old.LocationLabels))) {
+=======
+			if !(defaultRule.Count == int(old.MaxReplicas) && typeutil.StringsEqual(defaultRule.LocationLabels, []string(old.LocationLabels))) {
+>>>>>>> 9e157278c (placement: return rules after clone (#3892))
 				return errors.New("cannot to update replication config, the default rules do not consistent with replication config, please update rule instead")
 			}
 
 			return nil
 		}
 
-		if !(cfg.MaxReplicas == old.MaxReplicas && reflect.DeepEqual(cfg.LocationLabels, old.LocationLabels)) {
+		if !(cfg.MaxReplicas == old.MaxReplicas && typeutil.StringsEqual(cfg.LocationLabels, old.LocationLabels)) {
 			if err := CheckInDefaultRule(); err != nil {
 				return err
 			}
