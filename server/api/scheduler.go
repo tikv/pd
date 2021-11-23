d1eb16f16a3a2058be723cdb8b37ac70b211a71d
bufferflies <1045931706@qq.com>
2021-11-23 15:19:08 +0800

    modify return msg
    Signed-off-by: bufferflies <1045931706@qq.com>

--- server/api/scheduler.go
+++ server/api/scheduler.go
@@ -201,12 +201,12 @@ func (h *schedulerHandler) Post(w http.ResponseWriter, r *http.Request) {
 	case schedulers.GrantHotRegionName:
 		leaderID, ok := input["store-leader-id"].(string)
 		if !ok {
-			h.r.JSON(w, http.StatusBadRequest, "missing store-leader-id")
+			h.r.JSON(w, http.StatusBadRequest, "missing leader id")
 			return
 		}
 		peerIDs, ok := input["store-id"].(string)
 		if !ok {
-			h.r.JSON(w, http.StatusBadRequest, "missing store-leader-id")
+			h.r.JSON(w, http.StatusBadRequest, "missing store id")
 			return
 		}
 		if err := h.AddGrantHotRegionScheduler(leaderID, peerIDs); err != nil {
