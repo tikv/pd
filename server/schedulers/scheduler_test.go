7ec26e9c23e7055af58feaeb994639fbace07220
HunDunDM <hundundm@gmail.com>
2021-11-25 14:14:47 +0800

    *: fix incorrect test in #4377 (#4388)
    Signed-off-by: HunDunDM <hundundm@gmail.com>

--- server/schedulers/scheduler_test.go
+++ server/schedulers/scheduler_test.go
@@ -641,6 +641,7 @@ func (s *testEvictSlowStoreSuite) TestEvictSlowStore(c *C) {
 	// Add evict leader scheduler to store 1
 	op := es.Schedule(tc)
 	testutil.CheckTransferLeader(c, op[0], operator.OpLeader, 1, 2)
+	c.Assert(op[0].Desc(), Equals, EvictSlowStoreType)
 	// Cannot balance leaders to store 1
 	op = bs.Schedule(tc)
 	c.Check(op, IsNil)
@@ -652,5 +653,4 @@ func (s *testEvictSlowStoreSuite) TestEvictSlowStore(c *C) {
 	c.Check(es.Schedule(tc), IsNil)
 	op = bs.Schedule(tc)
 	testutil.CheckTransferLeader(c, op[0], operator.OpLeader, 2, 1)
-	c.Assert(op[0].Desc(), Equals, EvictSlowStoreType)
 }
