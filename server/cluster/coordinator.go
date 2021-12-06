f8ce91e3cd22fc190027b0536affea04e66983be
Ryan Leung <rleungx@gmail.com>
2021-12-06 16:00:40 +0800

    resolve conflicts
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/cluster/coordinator.go
+++ server/cluster/coordinator.go
@@ -471,7 +471,7 @@ func (c *coordinator) stop() {
 
 func (c *coordinator) getHotRegionsByType(typ statistics.RWType) *statistics.StoreHotPeersInfos {
 	isTraceFlow := c.cluster.GetOpts().IsTraceRegionFlow()
-	storeLoads := c.cluster.GetStoresLoads()
+	storeLoads := c.cluster.GetStoresLoadsLocked()
 	stores := c.cluster.GetStores()
 	switch typ {
 	case statistics.Write:
