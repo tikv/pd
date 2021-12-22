765c431eeb5e0247b78cfd2847e97d48f33fd278
Cabinfever_B <cabinfeveroier@gmail.com>
2021-12-22 18:18:43 +0800

    Revert "close #4483"
    This reverts commit dd14b0b699d2535280a5aecbfb0e688fe4d70a83.
    
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>

--- pkg/apiutil/serverapi/middleware.go
+++ pkg/apiutil/serverapi/middleware.go
@@ -92,6 +92,7 @@ func NewSelfProtector(s *server.Server) negroni.Handler {
 func (protector *selfProtector) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	handler := protector.s.GetSelfProtectionHandler()
 	if handler == nil || handler.HandleHTTPSelfProtection(r) {
+
 		failpoint.Inject("addSelfProtectionHTTPHeader", func() {
 			w.Header().Add("self-protection", "ok")
 		})
