cb3d40f0bf38e4d5c8ae1502ffde4ec76728dc2e
Cabinfever_B <cabinfeveroier@gmail.com>
2022-01-20 11:53:43 +0800

    add comment
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>

--- pkg/audit/audit.go
+++ pkg/audit/audit.go
@@ -23,11 +23,6 @@ type BackendLabels struct {
 	Labels []string
 }
 
-// BackendMatcher is used in interface `Backend`.
-type BackendMatcher interface {
-	Match(*BackendLabels) bool
-}
-
 // LabelMatcher implements AuditBackendMatcher
 type LabelMatcher struct {
 	backendLabel string
@@ -48,5 +43,5 @@ type Backend interface {
 	// ProcessHTTPRequest is used to perform HTTP audit process
 	ProcessHTTPRequest(event *requestutil.RequestInfo) bool
 	// AuditBackendMatcher is used to determine if the backend matches
-	BackendMatcher
+	Match(*BackendLabels) bool
 }
