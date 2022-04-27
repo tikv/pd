be2ea2c2f3a749e98fc5326a641e441b6ec180fd
Cabinfever_B <cabinfeveroier@gmail.com>
2022-04-27 15:57:56 +0800

    simply code
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>

--- server/api/config.go
+++ server/api/config.go
@@ -98,21 +98,15 @@ func (h *confHandler) SetConfig(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var ttls int
-	var ttlFlag bool
 	if ttlSec := r.URL.Query().Get("ttlSecond"); ttlSec != "" {
 		var err error
-		ttls, err = strconv.Atoi(ttlSec)
+		ttls, err := strconv.Atoi(ttlSec)
 		if err != nil {
 			h.rd.JSON(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		ttlFlag = true
-	}
-
-	// if ttlSecond defined, we will apply if to temp configuration.
-	if ttlFlag {
-		err := h.svr.SaveTTLConfig(conf, time.Duration(ttls)*time.Second)
+		// if ttlSecond defined, we will apply if to temp configuration.
+		err = h.svr.SaveTTLConfig(conf, time.Duration(ttls)*time.Second)
 		if err != nil {
 			h.rd.JSON(w, http.StatusBadRequest, err.Error())
 			return
