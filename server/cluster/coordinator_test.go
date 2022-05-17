dcabcacaaf8efff8e9f6876c8615257b353589ad
Ryan Leung <rleungx@gmail.com>
2022-05-17 17:24:38 +0800

    This is an automated cherry-pick of #4875
    close tikv/pd#4769
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- server/cluster/coordinator_test.go
+++ server/cluster/coordinator_test.go
@@ -192,7 +192,11 @@ func (s *testCoordinatorSuite) TestBasic(c *C) {
 func (s *testCoordinatorSuite) TestDispatch(c *C) {
 	tc, co, cleanup := prepare(nil, func(tc *testCluster) { tc.prepareChecker.isPrepared = true }, nil, c)
 	defer cleanup()
+<<<<<<< HEAD
 
+=======
+	co.prepareChecker.prepared = true
+>>>>>>> 429b49283 (*: fix scheduling can not immediately start after transfer leader (#4875))
 	// Transfer peer from store 4 to store 1.
 	c.Assert(tc.addRegionStore(4, 40), IsNil)
 	c.Assert(tc.addRegionStore(3, 30), IsNil)
@@ -285,7 +289,7 @@ func prepare(setCfg func(*config.ScheduleConfig), setTc func(*testCluster), run 
 		setCfg(cfg)
 	}
 	tc := newTestCluster(ctx, opt)
-	hbStreams := hbstream.NewTestHeartbeatStreams(ctx, tc.getClusterID(), tc, true /* need to run */)
+	hbStreams := hbstream.NewTestHeartbeatStreams(ctx, tc.meta.GetId(), tc, true /* need to run */)
 	if setTc != nil {
 		setTc(tc)
 	}
