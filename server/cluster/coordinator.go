82ecde419b2287b954394bd129ca1b566a87b3ba
Ryan Leung <rleungx@gmail.com>
2022-05-17 17:24:38 +0800

    This is an automated cherry-pick of #4875
    close tikv/pd#4769
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- server/cluster/coordinator.go
+++ server/cluster/coordinator.go
@@ -212,6 +212,9 @@ func (c *coordinator) drivePushOperator() {
 
 func (c *coordinator) run() {
 	ticker := time.NewTicker(runSchedulerCheckInterval)
+	failpoint.Inject("changeCoordinatorTicker", func() {
+		ticker = time.NewTicker(100 * time.Millisecond)
+	})
 	defer ticker.Stop()
 	log.Info("coordinator starts to collect cluster information")
 	for {
