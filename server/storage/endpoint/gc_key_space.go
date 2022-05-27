176061ecdf86c17e89a431538f4437a8224e575d
David <8039876+AmoebaProtozoa@users.noreply.github.com>
2022-05-27 14:26:40 +0800

    lint
    Signed-off-by: David <8039876+AmoebaProtozoa@users.noreply.github.com>

--- server/storage/endpoint/gc_key_space.go
+++ server/storage/endpoint/gc_key_space.go
@@ -19,14 +19,9 @@ import (
 	"math"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/pingcap/errors"
-	"github.com/pingcap/failpoint"
-	"github.com/pingcap/log"
-	"github.com/tikv/pd/pkg/errs"
 	"go.etcd.io/etcd/clientv3"
-	"go.uber.org/zap"
 )
 
 // KeySpaceGCSafePoint is gcWorker's safepoint for specific key-space
