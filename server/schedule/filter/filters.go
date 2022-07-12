8b00fdb8293daadaf22b4a1d6bd613d7609d896d
Ryan Leung <rleungx@gmail.com>
2022-07-12 18:02:15 +0800

    address the comment
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/schedule/filter/filters.go
+++ server/schedule/filter/filters.go
@@ -121,7 +121,7 @@ func Source(opt *config.PersistOptions, store *core.StoreInfo, filters []Filter)
 	storeAddress := store.GetAddress()
 	storeID := strconv.FormatUint(store.GetID(), 10)
 	for _, filter := range filters {
-		if filter.Source(opt, store) != statusOK {
+		if !filter.Source(opt, store).IsOK() {
 			sourceID := storeID
 			targetID := ""
 			filterCounter.WithLabelValues("filter-source", storeAddress,
@@ -137,7 +137,7 @@ func Target(opt *config.PersistOptions, store *core.StoreInfo, filters []Filter)
 	storeAddress := store.GetAddress()
 	storeID := strconv.FormatUint(store.GetID(), 10)
 	for _, filter := range filters {
-		if filter.Target(opt, store) != statusOK {
+		if !filter.Target(opt, store).IsOK() {
 			cfilter, ok := filter.(comparingFilter)
 			targetID := storeID
 			sourceID := ""
@@ -478,7 +478,7 @@ func (f *StoreStateFilter) anyConditionMatch(typ int, opt *config.PersistOptions
 		funcs = []conditionFunc{f.isRemoved, f.isRemoving, f.isDown, f.isDisconnected, f.isBusy}
 	}
 	for _, cf := range funcs {
-		if status := cf(opt, store); status != statusOK {
+		if status := cf(opt, store); !status.IsOK() {
 			return status
 		}
 	}
@@ -489,12 +489,12 @@ func (f *StoreStateFilter) anyConditionMatch(typ int, opt *config.PersistOptions
 // source.
 func (f *StoreStateFilter) Source(opts *config.PersistOptions, store *core.StoreInfo) (status plan.Status) {
 	if f.TransferLeader {
-		if status = f.anyConditionMatch(leaderSource, opts, store); status != statusOK {
+		if status = f.anyConditionMatch(leaderSource, opts, store); !status.IsOK() {
 			return
 		}
 	}
 	if f.MoveRegion {
-		if status = f.anyConditionMatch(regionSource, opts, store); status != statusOK {
+		if status = f.anyConditionMatch(regionSource, opts, store); !status.IsOK() {
 			return
 		}
 	}
@@ -505,17 +505,17 @@ func (f *StoreStateFilter) Source(opts *config.PersistOptions, store *core.Store
 // target.
 func (f *StoreStateFilter) Target(opts *config.PersistOptions, store *core.StoreInfo) (status plan.Status) {
 	if f.TransferLeader {
-		if status = f.anyConditionMatch(leaderTarget, opts, store); status != statusOK {
+		if status = f.anyConditionMatch(leaderTarget, opts, store); !status.IsOK() {
 			return
 		}
 	}
 	if f.MoveRegion && f.ScatterRegion {
-		if status = f.anyConditionMatch(scatterRegionTarget, opts, store); status != statusOK {
+		if status = f.anyConditionMatch(scatterRegionTarget, opts, store); !status.IsOK() {
 			return
 		}
 	}
 	if f.MoveRegion && !f.ScatterRegion {
-		if status = f.anyConditionMatch(regionTarget, opts, store); status != statusOK {
+		if status = f.anyConditionMatch(regionTarget, opts, store); !status.IsOK() {
 			return
 		}
 	}
