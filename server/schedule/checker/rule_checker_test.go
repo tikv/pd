449815164ce96c29a4400c3059531451cb3b3c18
Wenbo Zhang <ethercflow@gmail.com>
2022-07-12 16:18:18 +0800

    Add testcase: TestFixRuleWitness
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- server/schedule/checker/rule_checker_test.go
+++ server/schedule/checker/rule_checker_test.go
@@ -302,6 +302,30 @@ func (suite *ruleCheckerTestSuite) TestFixLeaderRoleWithUnhealthyRegion() {
 	suite.Nil(op)
 }
 
+func (suite *ruleCheckerTestSuite) TestFixRuleWitness() {
+	suite.cluster.AddLabelsStore(1, 1, map[string]string{"A": "follower"})
+	suite.cluster.AddLabelsStore(2, 1, map[string]string{"B": "leader"})
+	suite.cluster.AddLabelsStore(3, 1, map[string]string{"C": "voter"})
+	suite.cluster.AddLeaderRegion(1, 1, 2)
+
+	suite.ruleManager.SetRule(&placement.Rule{
+		GroupID:   "pd",
+		ID:        "r1",
+		Index:     100,
+		Override:  true,
+		Role:      placement.Voter,
+		Count:     1,
+		IsWitness: true,
+		LabelConstraints: []placement.LabelConstraint{
+			{Key: "C", Op: "in", Values: []string{"voter"}},
+		},
+	})
+	op := suite.rc.Check(suite.cluster.GetRegion(1))
+	suite.NotNil(op)
+	suite.Equal("add-rule-peer", op.Desc())
+	suite.Equal(uint64(3), op.Step(0).(operator.AddLearner).ToStore)
+}
+
 func (suite *ruleCheckerTestSuite) TestBetterReplacement() {
 	suite.cluster.AddLabelsStore(1, 1, map[string]string{"host": "host1"})
 	suite.cluster.AddLabelsStore(2, 1, map[string]string{"host": "host1"})
