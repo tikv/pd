fab78867a33ecda4eeab9876c660b2d4a548f4d0
David <8039876+AmoebaProtozoa@users.noreply.github.com>
2022-08-08 18:25:31 +0800

    address comments
    Signed-off-by: David <8039876+AmoebaProtozoa@users.noreply.github.com>

--- server/keyspace/util.go
+++ server/keyspace/util.go
@@ -15,11 +15,9 @@
 package keyspace
 
 import (
-	"fmt"
 	"regexp"
 
 	"github.com/pingcap/errors"
-	"github.com/tikv/pd/pkg/syncutil"
 )
 
 const (
@@ -38,8 +36,6 @@ var (
 	errKeyspaceArchived = errors.New("keyspace already archived")
 	errArchiveEnabled   = errors.New("cannot archive ENABLED keyspace")
 	errModifyDefault    = errors.New("cannot modify default keyspace's state")
-	errIllegalID        = errors.New("illegal keyspace ID")
-	errIllegalName      = errors.New("illegal keyspace name")
 	errIllegalOperation = errors.New("unknown operation")
 )
 
@@ -48,10 +44,10 @@ var (
 // or if it collides with reserved id.
 func validateID(spaceID uint32) error {
 	if spaceID > spaceIDMax {
-		return errIllegalID
+		return errors.Errorf("illegal keyspace id %d, larger than spaceID Max %d", spaceID, spaceIDMax)
 	}
 	if spaceID == DefaultKeyspaceID {
-		return errIllegalID
+		return errors.Errorf("illegal keyspace id %d, collides with default keyspace id", spaceID)
 	}
 	return nil
 }
@@ -65,60 +61,10 @@ func validateName(name string) error {
 		return err
 	}
 	if !isValid {
-		return errIllegalName
+		return errors.Errorf("illegal keyspace name %s, should contain only alphanumerical and underline", name)
 	}
 	if name == DefaultKeyspaceName {
-		return errIllegalName
+		return errors.Errorf("illegal keyspace name %s, collides with default keyspace name", name)
 	}
 	return nil
-}
-
-// lockGroup is a map of mutex that locks each keyspace separately.
-// It's used to guarantee that operations on different keyspace won't block each other.
-type lockGroup struct {
-	groupLock syncutil.Mutex             // protects group.
-	entries   map[uint32]*syncutil.Mutex // map of locks with keyspaceID as key.
-	// hashFn hashes spaceID to map key, it's main purpose is to limit the total
-	// number of mutexes in the group, as using a mutex for every keyspace is too memory heavy.
-	hashFn func(spaceID uint32) uint32
-}
-
-// newLockGroup create and return a empty lockGroup.
-func newLockGroup() *lockGroup {
-	return &lockGroup{
-		entries: make(map[uint32]*syncutil.Mutex),
-		// A simple mask is applied to spaceID to use its last byte as map key,
-		// limiting the maximum map length to 256.
-		// Since keyspaceID is sequentially allocated, this can also reduce the chance
-		// of collision when comparing with random hashes.
-		hashFn: func(spaceID uint32) uint32 {
-			return spaceID & 0xFF
-		},
-	}
-}
-
-func (g *lockGroup) lock(spaceID uint32) {
-	g.groupLock.Lock()
-	hashedID := spaceID & 0xFF
-	e, ok := g.entries[hashedID]
-	// If target keyspace's lock has not been initialized.
-	if !ok {
-		e = &syncutil.Mutex{}
-		g.entries[hashedID] = e
-	}
-	g.groupLock.Unlock()
-	e.Lock()
-}
-
-func (g *lockGroup) unlock(spaceID uint32) {
-	g.groupLock.Lock()
-	hashedID := spaceID & 0xFF
-	e, ok := g.entries[hashedID]
-	if !ok {
-		// Entry must exist, otherwise there should be a run-time error and panic.
-		g.groupLock.Unlock()
-		panic(fmt.Errorf("unlock requested for key %v, but no entry found", spaceID))
-	}
-	g.groupLock.Unlock()
-	e.Unlock()
 }
