fd9fc204265be25d0d74bc00614f9427daeca840
Connor1996 <zbk602423539@gmail.com>
2022-08-08 13:37:53 +0800

    revert unrelated change
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- server/schedule/operator_controller.go
+++ server/schedule/operator_controller.go
@@ -746,7 +746,7 @@ func (oc *OperatorController) GetFastOpInfluence(cluster Cluster, influence oper
 func AddOpInfluence(op *operator.Operator, influence operator.OpInfluence, cluster Cluster) {
 	region := cluster.GetRegion(op.RegionID())
 	if region != nil {
-		op.UnfinishedInfluence(influence, region)
+		op.TotalInfluence(influence, region)
 	}
 }
 
