fd5f48682fcf0148a2b9ed9b358cb2d1084efff5
Cabinfever_B <cabinfeveroier@gmail.com>
2022-08-12 14:37:43 +0800

    add analyzer
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>

---
+++ server/schedulers/schedule_plan_test.go
@@ -0,0 +1,189 @@
+// Copyright 2022 TiKV Project Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schedulers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pingcap/kvproto/pkg/metapb"
+	"github.com/stretchr/testify/suite"
+	"github.com/tikv/pd/server/core"
+	"github.com/tikv/pd/server/schedule/plan"
+)
+
+type balanceSchedulerPlanAnalyzeTestSuite struct {
+	suite.Suite
+
+	stores  []*core.StoreInfo
+	regions []*core.RegionInfo
+	ctx     context.Context
+	cancel  context.CancelFunc
+}
+
+func TestBalanceSchedulerPlanAnalyzerTestSuite(t *testing.T) {
+	suite.Run(t, new(balanceSchedulerPlanAnalyzeTestSuite))
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) SetupSuite() {
+	suite.ctx, suite.cancel = context.WithCancel(context.Background())
+	suite.stores = []*core.StoreInfo{
+		core.NewStoreInfo(
+			&metapb.Store{
+				Id: 1,
+			},
+		),
+		core.NewStoreInfo(
+			&metapb.Store{
+				Id: 2,
+			},
+		),
+		core.NewStoreInfo(
+			&metapb.Store{
+				Id: 3,
+			},
+		),
+		core.NewStoreInfo(
+			&metapb.Store{
+				Id: 4,
+			},
+		),
+		core.NewStoreInfo(
+			&metapb.Store{
+				Id: 5,
+			},
+		),
+	}
+	suite.regions = []*core.RegionInfo{
+		core.NewRegionInfo(
+			&metapb.Region{
+				Id: 1,
+			},
+			&metapb.Peer{
+				Id:      1,
+				StoreId: 1,
+			},
+		),
+		core.NewRegionInfo(
+			&metapb.Region{
+				Id: 2,
+			},
+			&metapb.Peer{
+				Id:      2,
+				StoreId: 2,
+			},
+		),
+		core.NewRegionInfo(
+			&metapb.Region{
+				Id: 3,
+			},
+			&metapb.Peer{
+				Id:      3,
+				StoreId: 3,
+			},
+		),
+	}
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) TearDownSuite() {
+	suite.cancel()
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) TestAnalyzerResult1() {
+	plans := make([]*balanceSchedulerPlan, 0)
+	step2 := balanceStep(2)
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step2, target: suite.stores[0], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step2, target: suite.stores[1], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step2, target: suite.stores[2], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step2, target: suite.stores[3], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step2, target: suite.stores[4], status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step2, target: suite.stores[0], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step2, target: suite.stores[1], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step2, target: suite.stores[2], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step2, target: suite.stores[3], status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step2, target: suite.stores[4], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step2, target: suite.stores[0], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step2, target: suite.stores[1], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step2, target: suite.stores[2], status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step2, target: suite.stores[3], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step2, target: suite.stores[4], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step2, target: suite.stores[0], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step2, target: suite.stores[1], status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step2, target: suite.stores[2], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step2, target: suite.stores[3], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step2, target: suite.stores[4], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step2, target: suite.stores[0], status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step2, target: suite.stores[1], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step2, target: suite.stores[2], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step2, target: suite.stores[3], status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step2, target: suite.stores[4], status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	analyzer := &BalanceSchedulerPlanAnalyzer{}
+	str, err := analyzer.Summary(plans)
+	suite.NoError(err)
+	suite.Equal("4 stores are filtered by Store Score Almost Same; 1 stores are filtered by Store Not Match Rule; ", str)
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) TestAnalyzerResult2() {
+	plans := make([]*balanceSchedulerPlan, 0)
+	step0 := balanceStep(0)
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	analyzer := &BalanceSchedulerPlanAnalyzer{}
+	str, err := analyzer.Summary(plans)
+	suite.NoError(err)
+	suite.Equal("5 stores are filtered by Store Down; ", str)
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) TestAnalyzerResult3() {
+	plans := make([]*balanceSchedulerPlan, 0)
+	step0 := balanceStep(0)
+	step1 := balanceStep(1)
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], region: suite.regions[0], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], region: suite.regions[0], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], region: suite.regions[1], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], region: suite.regions[1], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	analyzer := &BalanceSchedulerPlanAnalyzer{}
+	str, err := analyzer.Summary(plans)
+	suite.NoError(err)
+	suite.Equal("4 stores are filtered by Region Not Match Rule; ", str)
+}
+
+func (suite *balanceSchedulerPlanAnalyzeTestSuite) TestAnalyzerResult4() {
+	plans := make([]*balanceSchedulerPlan, 0)
+	step0 := balanceStep(0)
+	step1 := balanceStep(1)
+	step2 := balanceStep(2)
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[4], step: &step0, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[3], region: suite.regions[0], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[2], region: suite.regions[0], step: &step1, status: plan.NewStatus(plan.StatusRegionNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], target: suite.stores[0], step: &step2, status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], target: suite.stores[1], step: &step2, status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], target: suite.stores[2], step: &step2, status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], target: suite.stores[3], step: &step2, status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[1], target: suite.stores[4], step: &step2, status: plan.NewStatus(plan.StatusStoreDown)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], target: suite.stores[0], step: &step2, status: plan.NewStatus(plan.StatusStoreAlreadyHasPeer)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], target: suite.stores[1], step: &step2, status: plan.NewStatus(plan.StatusScoreAlmostSame)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], target: suite.stores[2], step: &step2, status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], target: suite.stores[3], step: &step2, status: plan.NewStatus(plan.StatusStoreNotMatchRule)})
+	plans = append(plans, &balanceSchedulerPlan{source: suite.stores[0], target: suite.stores[4], step: &step2, status: plan.NewStatus(plan.StatusStoreDown)})
+	analyzer := &BalanceSchedulerPlanAnalyzer{}
+	str, err := analyzer.Summary(plans)
+	suite.NoError(err)
+	suite.Equal("2 stores are filtered by Store Not Match Rule; 1 stores are filtered by Store Down; 2 stores are filtered by Store Already Has Peer; ", str)
+}
