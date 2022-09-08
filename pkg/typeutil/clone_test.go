4dab0d8a928fde28bfbde95c17de0080642cbbf5
bufferflies <1045931706@qq.com>
2022-09-08 15:48:25 +0800

    add benchmark
    Signed-off-by: bufferflies <1045931706@qq.com>

--- pkg/typeutil/clone_test.go
+++ pkg/typeutil/clone_test.go
@@ -3,6 +3,7 @@ package typeutil
 import (
 	"testing"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,4 +19,24 @@ func Test_DeepClone(t *testing.T) {
 	}()
 	dst2 := DeepClone(src2)
 	re.Nil(dst2)
+}
+
+func BenchmarkDeepClone(b *testing.B) {
+	re := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		src := &metapb.Region{Id: 1}
+		dst := DeepClone(src)
+		dst.Id = 2
+		re.Equal(src.Id, uint64(1))
+	}
+}
+
+func BenchmarkProtoClone(b *testing.B) {
+	re := assert.New(b)
+	for i := 0; i < b.N; i++ {
+		src := &metapb.Region{Id: 1}
+		dst := proto.Clone(src).(*metapb.Region)
+		dst.Id = 2
+		re.Equal(src.Id, uint64(1))
+	}
 }
