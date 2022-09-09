f51c150272c973e1a80f3f297866f8d1082ee6a6
D3Hunter <jujj603@gmail.com>
2022-09-09 15:41:27 +0800

    add ut
    Signed-off-by: D3Hunter <jujj603@gmail.com>

--- tests/server/cluster/cluster_work_test.go
+++ tests/server/cluster/cluster_work_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/kvproto/pkg/pdpb"
 	"github.com/stretchr/testify/require"
+	"github.com/tikv/pd/pkg/errs"
 	"github.com/tikv/pd/pkg/testutil"
 	"github.com/tikv/pd/server/core"
 	"github.com/tikv/pd/tests"
@@ -100,9 +101,6 @@ func TestAskSplit(t *testing.T) {
 		Region: regions[0].GetMeta(),
 	}
 
-	_, err = rc.HandleAskSplit(req)
-	re.NoError(err)
-
 	req1 := &pdpb.AskBatchSplitRequest{
 		Header: &pdpb.RequestHeader{
 			ClusterId: clusterID,
@@ -110,6 +108,18 @@ func TestAskSplit(t *testing.T) {
 		Region:     regions[0].GetMeta(),
 		SplitCount: 10,
 	}
+
+	re.NoError(leaderServer.GetServer().SaveTTLConfig(map[string]interface{}{"schedule.enable-tikv-split-region": 0}, time.Minute))
+	_, err = rc.HandleAskSplit(req)
+	re.ErrorIs(err, errs.ErrSchedulerTiKVSplitDisabled)
+	_, err = rc.HandleAskBatchSplit(req1)
+	re.ErrorIs(err, errs.ErrSchedulerTiKVSplitDisabled)
+	re.NoError(leaderServer.GetServer().SaveTTLConfig(map[string]interface{}{"schedule.enable-tikv-split-region": 0}, 0))
+	// wait ttl config takes effect
+	time.Sleep(time.Second)
+
+	_, err = rc.HandleAskSplit(req)
+	re.NoError(err)
 
 	_, err = rc.HandleAskBatchSplit(req1)
 	re.NoError(err)
