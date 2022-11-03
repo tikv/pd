ea5e300507587dd0492d8545bf1bec29385224f1
Wenbo Zhang <ethercflow@gmail.com>
2022-11-03 17:16:23 +0800

    fix coordinator tests
    ref tikv/pd#5638
    
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- server/cluster/coordinator_test.go
+++ server/cluster/coordinator_test.go
@@ -672,6 +672,7 @@ func TestAddScheduler(t *testing.T) {
 	re.NoError(co.removeScheduler(schedulers.BalanceRegionName))
 	re.NoError(co.removeScheduler(schedulers.HotRegionName))
 	re.NoError(co.removeScheduler(schedulers.SplitBucketName))
+	re.NoError(co.removeScheduler(schedulers.TransferWitnessLeaderName))
 	re.Empty(co.schedulers)
 
 	stream := mockhbstream.NewHeartbeatStream()
@@ -738,7 +739,7 @@ func TestPersistScheduler(t *testing.T) {
 	re.NoError(tc.addLeaderStore(1, 1))
 	re.NoError(tc.addLeaderStore(2, 1))
 
-	re.Len(co.schedulers, 4)
+	re.Len(co.schedulers, 5)
 	oc := co.opController
 	storage := tc.RaftCluster.storage
 
@@ -748,15 +749,15 @@ func TestPersistScheduler(t *testing.T) {
 	evict, err := schedule.CreateScheduler(schedulers.EvictLeaderType, oc, storage, schedule.ConfigSliceDecoder(schedulers.EvictLeaderType, []string{"2"}))
 	re.NoError(err)
 	re.NoError(co.addScheduler(evict, "2"))
-	re.Len(co.schedulers, 6)
+	re.Len(co.schedulers, 7)
 	sches, _, err := storage.LoadAllScheduleConfig()
 	re.NoError(err)
-	re.Len(sches, 6)
+	re.Len(sches, 7)
 	re.NoError(co.removeScheduler(schedulers.BalanceLeaderName))
 	re.NoError(co.removeScheduler(schedulers.BalanceRegionName))
 	re.NoError(co.removeScheduler(schedulers.HotRegionName))
 	re.NoError(co.removeScheduler(schedulers.SplitBucketName))
-	re.Len(co.schedulers, 2)
+	re.Len(co.schedulers, 3)
 	re.NoError(co.cluster.opt.Persist(storage))
 	co.stop()
 	co.wg.Wait()
@@ -771,21 +772,21 @@ func TestPersistScheduler(t *testing.T) {
 	defer func() {
 		config.DefaultSchedulers = config.DefaultSchedulers[:len(config.DefaultSchedulers)-1]
 	}()
-	re.Len(newOpt.GetSchedulers(), 4)
+	re.Len(newOpt.GetSchedulers(), 5)
 	re.NoError(newOpt.Reload(storage))
 	// only remains 3 items with independent config.
 	sches, _, err = storage.LoadAllScheduleConfig()
 	re.NoError(err)
-	re.Len(sches, 3)
+	re.Len(sches, 4)
 
 	// option have 6 items because the default scheduler do not remove.
-	re.Len(newOpt.GetSchedulers(), 7)
+	re.Len(newOpt.GetSchedulers(), 8)
 	re.NoError(newOpt.Persist(storage))
 	tc.RaftCluster.opt = newOpt
 
 	co = newCoordinator(ctx, tc.RaftCluster, hbStreams)
 	co.run()
-	re.Len(co.schedulers, 3)
+	re.Len(co.schedulers, 4)
 	co.stop()
 	co.wg.Wait()
 	// suppose restart PD again
@@ -795,22 +796,22 @@ func TestPersistScheduler(t *testing.T) {
 	tc.RaftCluster.opt = newOpt
 	co = newCoordinator(ctx, tc.RaftCluster, hbStreams)
 	co.run()
-	re.Len(co.schedulers, 3)
+	re.Len(co.schedulers, 4)
 	bls, err := schedule.CreateScheduler(schedulers.BalanceLeaderType, oc, storage, schedule.ConfigSliceDecoder(schedulers.BalanceLeaderType, []string{"", ""}))
 	re.NoError(err)
 	re.NoError(co.addScheduler(bls))
 	brs, err := schedule.CreateScheduler(schedulers.BalanceRegionType, oc, storage, schedule.ConfigSliceDecoder(schedulers.BalanceRegionType, []string{"", ""}))
 	re.NoError(err)
 	re.NoError(co.addScheduler(brs))
-	re.Len(co.schedulers, 5)
+	re.Len(co.schedulers, 6)
 
 	// the scheduler option should contain 6 items
 	// the `hot scheduler` are disabled
-	re.Len(co.cluster.opt.GetSchedulers(), 7)
+	re.Len(co.cluster.opt.GetSchedulers(), 8)
 	re.NoError(co.removeScheduler(schedulers.GrantLeaderName))
 	// the scheduler that is not enable by default will be completely deleted
-	re.Len(co.cluster.opt.GetSchedulers(), 6)
-	re.Len(co.schedulers, 4)
+	re.Len(co.cluster.opt.GetSchedulers(), 7)
+	re.Len(co.schedulers, 5)
 	re.NoError(co.cluster.opt.Persist(co.cluster.storage))
 	co.stop()
 	co.wg.Wait()
@@ -821,9 +822,9 @@ func TestPersistScheduler(t *testing.T) {
 	co = newCoordinator(ctx, tc.RaftCluster, hbStreams)
 
 	co.run()
-	re.Len(co.schedulers, 4)
+	re.Len(co.schedulers, 5)
 	re.NoError(co.removeScheduler(schedulers.EvictLeaderName))
-	re.Len(co.schedulers, 3)
+	re.Len(co.schedulers, 4)
 }
 
 func TestRemoveScheduler(t *testing.T) {
@@ -841,17 +842,17 @@ func TestRemoveScheduler(t *testing.T) {
 	re.NoError(tc.addLeaderStore(1, 1))
 	re.NoError(tc.addLeaderStore(2, 1))
 
-	re.Len(co.schedulers, 4)
+	re.Len(co.schedulers, 5)
 	oc := co.opController
 	storage := tc.RaftCluster.storage
 
 	gls1, err := schedule.CreateScheduler(schedulers.GrantLeaderType, oc, storage, schedule.ConfigSliceDecoder(schedulers.GrantLeaderType, []string{"1"}))
 	re.NoError(err)
 	re.NoError(co.addScheduler(gls1, "1"))
-	re.Len(co.schedulers, 5)
+	re.Len(co.schedulers, 6)
 	sches, _, err := storage.LoadAllScheduleConfig()
 	re.NoError(err)
-	re.Len(sches, 5)
+	re.Len(sches, 6)
 
 	// remove all schedulers
 	re.NoError(co.removeScheduler(schedulers.BalanceLeaderName))
