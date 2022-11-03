8f95a5eecb2c2f3403311941fdba97f44c492cd4
Ryan Leung <rleungx@gmail.com>
2022-11-03 11:29:35 +0800

    use metrics to record statistics
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tools/pd-simulator/simulator/metrics.go
+++ tools/pd-simulator/simulator/metrics.go
@@ -11,8 +11,35 @@ var (
 			Help:      "Bucketed histogram of processing time (s) of handled snap requests.",
 			Buckets:   prometheus.ExponentialBuckets(0.0005, 2, 20),
 		}, []string{"store", "type"})
+
+	schedulingCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "pd",
+			Subsystem: "schedule",
+			Name:      "scheduling_count",
+			Help:      "Counter of region scheduling",
+		}, []string{"type"})
+
+	snapRecvCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "pd",
+			Subsystem: "schedule",
+			Name:      "snap_recv_count",
+			Help:      "Counter of receiving region snapshot",
+		}, []string{"store"})
+
+	snapSendCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "pd",
+			Subsystem: "schedule",
+			Name:      "snap_send_count",
+			Help:      "Counter of sending region snapshot",
+		}, []string{"store"})
 )
 
 func init() {
 	prometheus.MustRegister(snapDuration)
+	prometheus.MustRegister(schedulingCounter)
+	prometheus.MustRegister(snapRecvCounter)
+	prometheus.MustRegister(snapSendCounter)
 }
