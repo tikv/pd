6078724922e5abb370956888e4a87db1a74f5421
Sen Han <00hnes@gmail.com>
2023-02-15 16:25:17 +0800

    ignore error if kernel version is too old
    Signed-off-by: Sen Han <00hnes@gmail.com>

--- pkg/cgroup/cgroup_cpu_test.go
+++ pkg/cgroup/cgroup_cpu_test.go
@@ -17,14 +17,54 @@
 package cgroup
 
 import (
+	"fmt"
+	"regexp"
 	"runtime"
+	"strconv"
 	"sync"
+	"syscall"
 	"testing"
 
 	"github.com/pingcap/log"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
+
+func checkKernelVersionNewerThan(t *testing.T, major, minor int) bool {
+	u := syscall.Utsname{}
+	err := syscall.Uname(&u)
+	require.NoError(t, err)
+	releaseBs := make([]byte, 0, len(u.Release))
+	for _, v := range u.Release {
+		if v == 0 {
+			break
+		}
+		releaseBs = append(releaseBs, byte(v))
+	}
+	releaseStr := string(releaseBs)
+	log.Info("kernel release string", zap.String("release-str", releaseStr))
+	versionInfoRE := regexp.MustCompile(`[0-9]+\.[0-9]+\.[0-9]+`)
+	kernelVerion := versionInfoRE.FindAllString(releaseStr, 1)
+	require.Equal(t, 1, len(kernelVerion), fmt.Sprintf("release str is %s", releaseStr))
+	kernelVersionPartRE := regexp.MustCompile(`[0-9]+`)
+	kernelVersionParts := kernelVersionPartRE.FindAllString(kernelVerion[0], -1)
+	require.Equal(t, 3, len(kernelVersionParts), fmt.Sprintf("kernel verion str is %s", kernelVerion[0]))
+	log.Info("parsed kernel version parts", zap.String("major", kernelVersionParts[0]), zap.String("minor", kernelVersionParts[1]), zap.String("patch", kernelVersionParts[2]))
+	mustConvInt := func(s string) int {
+		i, err := strconv.Atoi(s)
+		require.NoError(t, err, s)
+		return i
+	}
+	versionNewerThanFlag := false
+	if mustConvInt(kernelVersionParts[0]) > major {
+		versionNewerThanFlag = true
+	} else {
+		if mustConvInt(kernelVersionParts[0]) == major && mustConvInt(kernelVersionParts[1]) > minor {
+			versionNewerThanFlag = true
+		}
+	}
+	return versionNewerThanFlag
+}
 
 func TestGetCgroupCPU(t *testing.T) {
 	exit := make(chan struct{})
@@ -44,12 +84,14 @@ func TestGetCgroupCPU(t *testing.T) {
 		}()
 	}
 	cpu, err := GetCgroupCPU()
-	log.Info("InContainer()", zap.Bool("InContainer", InContainer()))
 	if err == errNoCPUControllerDetected {
-		log.Info("errNoCPUControllerDetected", zap.Bool("errNoCPUControllerDetected", true))
-		require.False(t, InContainer(), "Please check linux version > v4.7.x. This is related to cgroup compatibility.")
+		// for more information, please refer https://github.com/pingcap/tidb/pull/41347
+		if checkKernelVersionNewerThan(t, 4, 7) {
+			require.NoError(t, err, "linux version > v4.7 and err still happens")
+		} else {
+			log.Info("the error is ignored because the kernel is too old")
+		}
 	} else {
-		log.Info("errNoCPUControllerDetected", zap.Bool("errNoCPUControllerDetected", false))
 		require.NoError(t, err)
 		require.NotZero(t, cpu.Period)
 		require.Less(t, int64(1), cpu.Period)
