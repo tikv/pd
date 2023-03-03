0c0f4f0a4ff3138db9f0f5e1c7af07a95a31dda3
lhy1024 <admin@liudos.us>
2023-03-03 13:37:10 +0800

    This is an automated cherry-pick of #6080
    ref tikv/pd#5798, close tikv/pd#6069
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/movingaverage/median_filter.go
+++ pkg/movingaverage/median_filter.go
@@ -20,10 +20,20 @@ import "github.com/elliotchance/pie/v2"
 // There are at most `size` data points for calculating.
 // References: https://en.wikipedia.org/wiki/Median_filter.
 type MedianFilter struct {
+<<<<<<< HEAD
 	records       []float64
 	size          uint64
 	count         uint64
 	instantaneous float64
+=======
+	// It is not thread safe to read and write records at the same time.
+	// If there are concurrent read and write, the read may get an old value.
+	// And we should avoid concurrent write.
+	records []float64
+	size    uint64
+	count   uint64
+	result  float64
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 }
 
 // NewMedianFilter returns a MedianFilter.
@@ -31,6 +41,10 @@ func NewMedianFilter(size int) *MedianFilter {
 	return &MedianFilter{
 		records: make([]float64, size),
 		size:    uint64(size),
+<<<<<<< HEAD
+=======
+		result:  0,
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 	}
 }
 
@@ -39,6 +53,7 @@ func (r *MedianFilter) Add(n float64) {
 	r.instantaneous = n
 	r.records[r.count%r.size] = n
 	r.count++
+<<<<<<< HEAD
 }
 
 // Get returns the median of the data set.
@@ -46,17 +61,32 @@ func (r *MedianFilter) Get() float64 {
 	if r.count == 0 {
 		return 0
 	}
+=======
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 	records := r.records
 	if r.count < r.size {
 		records = r.records[:r.count]
 	}
+<<<<<<< HEAD
 	return pie.Median(records)
+=======
+	r.result = pie.Median(records)
+}
+
+// Get returns the median of the data set.
+func (r *MedianFilter) Get() float64 {
+	return r.result
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 }
 
 // Reset cleans the data set.
 func (r *MedianFilter) Reset() {
 	r.instantaneous = 0
 	r.count = 0
+<<<<<<< HEAD
+=======
+	r.result = 0
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 }
 
 // Set = Reset + Add.
@@ -64,6 +94,10 @@ func (r *MedianFilter) Set(n float64) {
 	r.instantaneous = n
 	r.records[0] = n
 	r.count = 1
+<<<<<<< HEAD
+=======
+	r.result = n
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 }
 
 // GetInstantaneous returns the value just added.
@@ -76,9 +110,16 @@ func (r *MedianFilter) Clone() *MedianFilter {
 	records := make([]float64, len(r.records))
 	copy(records, r.records)
 	return &MedianFilter{
+<<<<<<< HEAD
 		records:       records,
 		size:          r.size,
 		count:         r.count,
 		instantaneous: r.instantaneous,
+=======
+		records: records,
+		size:    r.size,
+		count:   r.count,
+		result:  r.result,
+>>>>>>> d85a0e4e3 (moving_filter: fix data race with cache result (#6080))
 	}
 }
