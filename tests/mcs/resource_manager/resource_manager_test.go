384e4f9dd801b3e3a3628435fb5b68b84c98c86a
nolouch <nolouch@gmail.com>
2023-03-22 15:25:05 +0800

    resource_control: default create the resource groups
    Signed-off-by: nolouch <nolouch@gmail.com>

--- tests/mcs/resource_manager/resource_manager_test.go
+++ tests/mcs/resource_manager/resource_manager_test.go
@@ -128,6 +128,9 @@ func (suite *resourceManagerClientTestSuite) cleanupResourceGroups() {
 	groups, err := cli.ListResourceGroups(suite.ctx)
 	suite.NoError(err)
 	for _, group := range groups {
+		if group.Name == "default" {
+			continue
+		}
 		deleteResp, err := cli.DeleteResourceGroup(suite.ctx, group.GetName())
 		suite.NoError(err)
 		suite.Contains(deleteResp, "Success!")
@@ -166,7 +169,7 @@ func (suite *resourceManagerClientTestSuite) TestWatchResourceGroup() {
 	}
 	lresp, err := cli.ListResourceGroups(suite.ctx)
 	re.NoError(err)
-	re.Equal(len(lresp), 3)
+	re.Equal(len(lresp), 4)
 	// Start watcher
 	watchChan, err := suite.client.WatchResourceGroup(suite.ctx, int64(0))
 	suite.NoError(err)
@@ -481,7 +484,7 @@ func (suite *resourceManagerClientTestSuite) TestBasicResourceGroupCURD() {
 		}
 	}
 
-	finalNum := 0
+	finalNum := 1
 	// Test Resource Group CURD via gRPC
 	for i, tcase := range testCasesSet1 {
 		group := &rmpb.ResourceGroup{
@@ -533,12 +536,12 @@ func (suite *resourceManagerClientTestSuite) TestBasicResourceGroupCURD() {
 			// List Resource Group
 			lresp, err = cli.ListResourceGroups(suite.ctx)
 			re.NoError(err)
-			re.Equal(0, len(lresp))
+			re.Equal(1, len(lresp))
 		}
 	}
 
 	// Test Resource Group CURD via HTTP
-	finalNum = 0
+	finalNum = 1
 	getAddr := func(i int) string {
 		server := suite.cluster.GetServer(suite.cluster.GetLeader())
 		if i%2 == 1 {
@@ -606,6 +609,9 @@ func (suite *resourceManagerClientTestSuite) TestBasicResourceGroupCURD() {
 
 			// Delete all resource groups
 			for _, g := range groups {
+				if g.Name == "default" {
+					continue
+				}
 				req, err := http.NewRequest(http.MethodDelete, getAddr(i+1)+"/resource-manager/api/v1/config/group/"+g.Name, nil)
 				re.NoError(err)
 				resp, err := http.DefaultClient.Do(req)
@@ -626,7 +632,7 @@ func (suite *resourceManagerClientTestSuite) TestBasicResourceGroupCURD() {
 			re.NoError(err)
 			groups1 := make([]server.ResourceGroup, 0)
 			json.Unmarshal(respString1, &groups1)
-			re.Equal(0, len(groups1))
+			re.Equal(1, len(groups1))
 		}
 	}
 }
