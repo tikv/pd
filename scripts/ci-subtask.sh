e4320bdbc3dc0a6e23486a37858efd61d18f3e62
JmPotato <ghzpotato@gmail.com>
2023-03-24 12:05:41 +0800

    Refine the CI subtask
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- scripts/ci-subtask.sh
+++ scripts/ci-subtask.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# ./ci-subtask.sh <TOTAL_TASK_N> <TASK_INDEX> <IS_INTEGRATION_TEST>
+# ./ci-subtask.sh <TOTAL_TASK_N> <TASK_INDEX> <INTEGRATION_TEST_ONLY>
 
-tasks=()
-if [[ $3 ]]; then
-    makefile_dirs=(`find . -iname "Makefile" -exec dirname {} \; | sort -u`)
-    submod_dirs=(`find . -iname "go.mod" -exec dirname {} \; | sort -u`)
-    tasks=$(comm -12 <(printf "%s\n" "${makefile_dirs[@]}") <(printf "%s\n" "${submod_dirs[@]}") | grep "./tests/integrations/*")
-else
-    packages=(`go list ./...`)
-    dirs=(`find . -iname "*_test.go" -exec dirname {} \; | sort -u | sed -e "s/^\./github.com\/tikv\/pd/"`)
-    tasks=($(comm -12 <(printf "%s\n" "${packages[@]}") <(printf "%s\n" "${dirs[@]}")))
-fi
+# Get package test list.
+packages=(`go list ./...`)
+dirs=(`find . -iname "*_test.go" -exec dirname {} \; | sort -u | sed -e "s/^\./github.com\/tikv\/pd/"`)
+tasks=($(comm -12 <(printf "%s\n" "${packages[@]}") <(printf "%s\n" "${dirs[@]}")))
+# Get integration test list.
+makefile_dirs=(`find . -iname "Makefile" -exec dirname {} \; | sort -u`)
+submod_dirs=(`find . -iname "go.mod" -exec dirname {} \; | sort -u`)
+integration_tasks=$(comm -12 <(printf "%s\n" "${makefile_dirs[@]}") <(printf "%s\n" "${submod_dirs[@]}") | grep "./tests/integrations/*")
+
+all_tasks=("${tasks[@]}" "${integration_tasks[@]}")
 
 weight () {
+    [[ $1 =~ "tests/integrations" ]] && return 50
     [[ $1 == "github.com/tikv/pd/server/api" ]] && return 30
     [[ $1 == "github.com/tikv/pd/pkg/schedule" ]] && return 30
     [[ $1 =~ "pd/tests" ]] && return 5
@@ -23,14 +24,21 @@ weight () {
 scores=(`seq "$1" | xargs -I{} echo 0`)
 
 res=()
-for t in ${tasks[@]}; do
+for t in ${all_tasks[@]}; do
     min_i=0
     for i in ${!scores[@]}; do
         [[ ${scores[i]} -lt ${scores[$min_i]} ]] && min_i=$i
     done
     weight $t
     scores[$min_i]=$((${scores[$min_i]} + $?))
-    [[ $(($min_i+1)) -eq $2 ]] && res+=($t)
+    if [[ $(($min_i+1)) -eq $2 ]]; then
+        # Integration test only.
+        if [[ $3 && "$t" =~ "tests/integrations" ]]; then
+            res+=($t)
+        elif [[ ! $3 && "$t" =~ "github.com/tikv/pd" ]]; then
+            res+=($t)
+        fi
+    fi
 done
 
 printf "%s " "${res[@]}"
