6f3f264476c5dcad08904a6dfe244f566ef4b330
Bin Shi <binshi.bing@gmail.com>
2023-04-06 16:55:11 -0700

    Add more failure tests when tso service loading initial keyspace groups assignment
    Signed-off-by: Bin Shi <binshi.bing@gmail.com>

--- pkg/tso/keyspace_group_manager_test.go
+++ pkg/tso/keyspace_group_manager_test.go
@@ -29,8 +29,10 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/pingcap/failpoint"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"github.com/tikv/pd/pkg/errs"
 	"github.com/tikv/pd/pkg/mcs/discovery"
 	mcsutils "github.com/tikv/pd/pkg/mcs/utils"
 	"github.com/tikv/pd/pkg/storage/endpoint"
@@ -166,6 +168,73 @@ func (suite *keyspaceGroupManagerTestSuite) TestLoadWithDifferentBatchSize() {
 		runTestLoadKeyspaceGroupsAssignment(suite.ctx, re, suite.etcdClient, suite.cfg,
 			param.count+1, param.batchSize, param.probabilityAssignToMe)
 	}
+}
+
+// TestLoadKeyspaceGroupsTimeout tests there is timeout when loading the initial keyspace group assignment
+// from etcd. The initialization of the keyspace group manager should fail.
+func (suite *keyspaceGroupManagerTestSuite) TestLoadKeyspaceGroupsTimeout() {
+	re := suite.Require()
+
+	mgr := newUniqueKeyspaceGroupManager(suite.ctx, suite.etcdClient, suite.cfg, 0, 1)
+	re.NotNil(mgr)
+	defer mgr.Close()
+
+	addKeyspaceGroupAssignment(
+		suite.ctx, suite.etcdClient, true,
+		mgr.legacySvcRootPath, mgr.tsoServiceID.ServiceAddr, uint32(0))
+
+	// Set the timeout to 1 second and inject the delayLoadKeyspaceGroups to return 3 seconds to let
+	// the loading sleep 3 seconds.
+	mgr.loadKeyspaceGroupsTimeout = time.Second
+	re.NoError(failpoint.Enable("github.com/tikv/pd/pkg/tso/delayLoadKeyspaceGroups", "return(3)"))
+	err := mgr.Initialize(true)
+	// If loading keyspace groups timeout, the initialization should fail with ErrLoadKeyspaceGroupsTerminated.
+	re.Equal(errs.ErrLoadKeyspaceGroupsTerminated, err)
+	re.NoError(failpoint.Disable("github.com/tikv/pd/pkg/tso/delayLoadKeyspaceGroups"))
+}
+
+// TestLoadKeyspaceGroupsSucceedWithTempFailures tests the initialization should succeed when there are temporary
+// failures during loading the initial keyspace group assignment from etcd.
+func (suite *keyspaceGroupManagerTestSuite) TestLoadKeyspaceGroupsSucceedWithTempFailures() {
+	re := suite.Require()
+
+	mgr := newUniqueKeyspaceGroupManager(suite.ctx, suite.etcdClient, suite.cfg, 0, 1)
+	re.NotNil(mgr)
+	defer mgr.Close()
+
+	addKeyspaceGroupAssignment(
+		suite.ctx, suite.etcdClient, true,
+		mgr.legacySvcRootPath, mgr.tsoServiceID.ServiceAddr, uint32(0))
+
+	// Set the max retry times to 3 and inject the loadKeyspaceGroupsTemporaryFail to return 2 to let
+	// loading from etcd fail 2 times but the whole initialization still succeeds.
+	mgr.loadFromEtcdMaxRetryTimes = 3
+	re.NoError(failpoint.Enable("github.com/tikv/pd/pkg/tso/loadKeyspaceGroupsTemporaryFail", "return(2)"))
+	err := mgr.Initialize(true)
+	re.NoError(err)
+	re.NoError(failpoint.Disable("github.com/tikv/pd/pkg/tso/loadKeyspaceGroupsTemporaryFail"))
+}
+
+// TestLoadKeyspaceGroupsFailed tests the initialization should fail when there are too many failures
+// during loading the initial keyspace group assignment from etcd.
+func (suite *keyspaceGroupManagerTestSuite) TestLoadKeyspaceGroupsFailed() {
+	re := suite.Require()
+
+	mgr := newUniqueKeyspaceGroupManager(suite.ctx, suite.etcdClient, suite.cfg, 0, 1)
+	re.NotNil(mgr)
+	defer mgr.Close()
+
+	addKeyspaceGroupAssignment(
+		suite.ctx, suite.etcdClient, true,
+		mgr.legacySvcRootPath, mgr.tsoServiceID.ServiceAddr, uint32(0))
+
+	// Set the max retry times to 3 and inject the loadKeyspaceGroupsTemporaryFail to return 3 to let
+	// loading from etcd fail 3 times which should cause the whole initialization to fail.
+	mgr.loadFromEtcdMaxRetryTimes = 3
+	re.NoError(failpoint.Enable("github.com/tikv/pd/pkg/tso/loadKeyspaceGroupsTemporaryFail", "return(3)"))
+	err := mgr.Initialize(true)
+	re.Error(err)
+	re.NoError(failpoint.Disable("github.com/tikv/pd/pkg/tso/loadKeyspaceGroupsTemporaryFail"))
 }
 
 // TestWatchAndDynamicallyApplyChanges tests the keyspace group manager watch and dynamically apply
