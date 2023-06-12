4ef08712bcc10ab671481b0e1859d9a925d2633c
lhy1024 <admin@liudos.us>
2023-06-12 18:02:21 +0800

    enable test
    Signed-off-by: lhy1024 <admin@liudos.us>

--- tests/pdctl/scheduler/scheduler_test.go
+++ tests/pdctl/scheduler/scheduler_test.go
@@ -31,7 +31,6 @@ import (
 )
 
 func TestScheduler(t *testing.T) {
-	t.Skip("skip this super unstable test which impacts everyone's productivity")
 	re := require.New(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
