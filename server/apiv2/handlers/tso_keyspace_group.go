94f24319ca11a61c41f03f4abcd143a44843e161
lhy1024 <admin@liudos.us>
2023-07-04 19:36:26 +0800

    pdctl: support show keyspace group primary
    Signed-off-by: lhy1024 <admin@liudos.us>

--- server/apiv2/handlers/tso_keyspace_group.go
+++ server/apiv2/handlers/tso_keyspace_group.go
@@ -47,6 +47,7 @@ func RegisterTSOKeyspaceGroup(r *gin.RouterGroup) {
 	router.DELETE("/:id/split", FinishSplitKeyspaceByID)
 	router.POST("/:id/merge", MergeKeyspaceGroups)
 	router.DELETE("/:id/merge", FinishMergeKeyspaceByID)
+	router.GET("/:id/primary", GetKeyspaceGroupPrimaryByID)
 }
 
 // CreateKeyspaceGroupParams defines the params for creating keyspace groups.
@@ -483,6 +484,33 @@ func SetPriorityForKeyspaceGroup(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, nil)
+}
+
+func GetKeyspaceGroupPrimaryByID(c *gin.Context) {
+	id, err := validateKeyspaceGroupID(c)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "invalid keyspace group id")
+		return
+	}
+	svr := c.MustGet(middlewares.ServerContextKey).(*server.Server)
+	manager := svr.GetKeyspaceGroupManager()
+	if manager == nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, groupManagerUninitializedErr)
+		return
+	}
+	// check if keyspace group exists
+	kg, err := manager.GetKeyspaceGroupByID(id)
+	if err != nil || kg == nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "keyspace group does not exist")
+		return
+	}
+	// get primary
+	primary, err := manager.GetKeyspaceGroupPrimaryByID(id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, primary)
 }
 
 func validateKeyspaceGroupID(c *gin.Context) (uint32, error) {
