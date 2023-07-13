5055bea4f293c28623f717cc1437a75e18dcd647
ystaticy <y_static_y@sina.com>
2023-07-13 10:08:01 +0800

    get all keyspaces
    Signed-off-by: ystaticy <y_static_y@sina.com>

--- go_mod.sh
+++
@@ -1,5 +0,0 @@
-go mod tidy;
-cd client;go mod tidy;cd ../;
-cd tests/integrations/client/;go mod tidy;cd ../../../;
-cd tests/integrations/tso/;go mod tidy;cd ../../../;
-cd tests/integrations/mcs/;go mod tidy;cd ../../../;
