86b82ca63cd81e10c82f525d7d6ddfd8acd93718
bufferflies <1045931706@qq.com>
2023-07-24 14:26:15 +0800

    remove unused logic
    Signed-off-by: bufferflies <1045931706@qq.com>

--- server/cluster/store_limiter.go
+++ server/cluster/store_limiter.go
@@ -15,12 +15,9 @@
 package cluster
 
 import (
-	"github.com/pingcap/kvproto/pkg/pdpb"
-	"github.com/pingcap/log"
 	"github.com/tikv/pd/pkg/core/storelimit"
 	"github.com/tikv/pd/pkg/utils/syncutil"
 	"github.com/tikv/pd/server/config"
-	"go.uber.org/zap"
 )
 
 // StoreLimiter adjust the store limit dynamically
@@ -44,32 +41,6 @@ func NewStoreLimiter(opt *config.PersistOptions) *StoreLimiter {
 		state:   NewState(),
 		scene:   defaultScene,
 		current: LoadStateNone,
-	}
-}
-
-// Collect the store statistics and update the cluster state
-func (s *StoreLimiter) Collect(stats *pdpb.StoreStats) {
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	log.Debug("collected statistics", zap.Reflect("stats", stats))
-	s.state.Collect((*StatEntry)(stats))
-
-	state := s.state.State()
-	ratePeerAdd := s.calculateRate(storelimit.AddPeer, state)
-	ratePeerRemove := s.calculateRate(storelimit.RemovePeer, state)
-
-	if ratePeerAdd > 0 || ratePeerRemove > 0 {
-		if ratePeerAdd > 0 {
-			s.opt.SetAllStoresLimit(storelimit.AddPeer, ratePeerAdd)
-			log.Info("change store region add limit for cluster", zap.Stringer("state", state), zap.Float64("rate", ratePeerAdd))
-		}
-		if ratePeerRemove > 0 {
-			s.opt.SetAllStoresLimit(storelimit.RemovePeer, ratePeerRemove)
-			log.Info("change store region remove limit for cluster", zap.Stringer("state", state), zap.Float64("rate", ratePeerRemove))
-		}
-		s.current = state
-		collectClusterStateCurrent(state)
 	}
 }
 
