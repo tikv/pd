bfc1fe9809840e9152008900673a5004fae6da7b
husharp <ihusharp@gmail.com>
2023-09-01 14:05:10 +0800

    address comment
    Signed-off-by: husharp <ihusharp@gmail.com>

--- client/tso_service_discovery.go
+++ client/tso_service_discovery.go
@@ -217,7 +217,7 @@ func (c *tsoServiceDiscovery) ScheduleCheckMemberChanged() {
 	}
 }
 
-// Immediately check if there is any membership change among the primary/secondaries in
+// CheckMemberChanged Immediately check if there is any membership change among the primary/secondaries in
 // a primary/secondary configured cluster.
 func (c *tsoServiceDiscovery) CheckMemberChanged() error {
 	return c.updateMember()
