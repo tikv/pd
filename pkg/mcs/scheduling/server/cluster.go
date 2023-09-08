c1549581e53ef1a9f5bee53a97ea6882c19e4cc8
Ryan Leung <rleungx@gmail.com>
2023-09-08 16:54:22 +0800

    use memory storage
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/mcs/scheduling/server/cluster.go
+++ pkg/mcs/scheduling/server/cluster.go
@@ -143,12 +143,12 @@ func (c *Cluster) GetStoreConfig() sc.StoreConfigProvider { return c.persistConf
 
 // AllocID allocates a new ID.
 func (c *Cluster) AllocID() (uint64, error) {
-	cli := c.apiServerLeader.Load().(pdpb.PDClient)
+	cli := c.apiServerLeader.Load()
 	if cli == nil {
 		c.checkMembershipCh <- struct{}{}
 		return 0, errors.New("API server leader is not found")
 	}
-	resp, err := cli.AllocID(c.ctx, &pdpb.AllocIDRequest{Header: &pdpb.RequestHeader{ClusterId: c.clusterID}})
+	resp, err := cli.(pdpb.PDClient).AllocID(c.ctx, &pdpb.AllocIDRequest{Header: &pdpb.RequestHeader{ClusterId: c.clusterID}})
 	if err != nil {
 		c.checkMembershipCh <- struct{}{}
 		return 0, err
