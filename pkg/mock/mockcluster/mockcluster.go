fb6139c6dd483a7c9b81032ab2c96b8d844b5506
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-09-28 12:01:48 +0800

    *: fix sync isolation level to default placement rule (#7122) (#7127)
    close tikv/pd#7121
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    
    Co-authored-by: Ryan Leung <rleungx@gmail.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/mock/mockcluster/mockcluster.go
+++ pkg/mock/mockcluster/mockcluster.go
@@ -189,7 +189,7 @@ func (mc *Cluster) AllocPeer(storeID uint64) (*metapb.Peer, error) {
 func (mc *Cluster) initRuleManager() {
 	if mc.RuleManager == nil {
 		mc.RuleManager = placement.NewRuleManager(storage.NewStorageWithMemoryBackend(), mc, mc.GetOpts())
-		mc.RuleManager.Initialize(int(mc.GetReplicationConfig().MaxReplicas), mc.GetReplicationConfig().LocationLabels)
+		mc.RuleManager.Initialize(int(mc.GetReplicationConfig().MaxReplicas), mc.GetReplicationConfig().LocationLabels, mc.GetReplicationConfig().IsolationLevel)
 	}
 }
 
