b0477c9f50cff091d2ac75081ffcec3e31d09e4f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-10-12 13:55:55 +0800

    resourcemanager: do not check existence when add resource group (#6717) (#6739)
    ref tikv/pd#5851, ref pingcap/tidb#45050
    
    Signed-off-by: glorv <glorvs@163.com>
    
    Co-authored-by: glorv <glorvs@163.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/mcs/resource_manager/server/manager.go
+++ pkg/mcs/resource_manager/server/manager.go
@@ -156,6 +156,8 @@ func (m *Manager) Init(ctx context.Context) {
 }
 
 // AddResourceGroup puts a resource group.
+// NOTE: AddResourceGroup should also be idempotent because tidb depends
+// on this retry mechanism.
 func (m *Manager) AddResourceGroup(grouppb *rmpb.ResourceGroup) error {
 	// Check the name.
 	if len(grouppb.Name) == 0 || len(grouppb.Name) > 32 {
@@ -164,12 +166,6 @@ func (m *Manager) AddResourceGroup(grouppb *rmpb.ResourceGroup) error {
 	// Check the Priority.
 	if grouppb.GetPriority() > 16 {
 		return errs.ErrInvalidGroup
-	}
-	m.RLock()
-	_, ok := m.groups[grouppb.Name]
-	m.RUnlock()
-	if ok {
-		return errs.ErrResourceGroupAlreadyExists.FastGenByArgs(grouppb.Name)
 	}
 	group := FromProtoResourceGroup(grouppb)
 	m.Lock()
