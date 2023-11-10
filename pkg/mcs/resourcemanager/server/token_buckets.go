dfdf2f73a460521e86e60dc358f6c4d3e2dc963f
guo-shaoge <shaoge1994@163.com>
2023-11-10 12:06:23 +0800

    update resetLoan()
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- pkg/mcs/resourcemanager/server/token_buckets.go
+++ pkg/mcs/resourcemanager/server/token_buckets.go
@@ -114,13 +114,10 @@ func (gts *GroupTokenBucketState) resetLoan() {
 	}
 
 	evenTokens := gts.Tokens * evenRatio
-	now := time.Now()
-	gts.lastCheckExpireSlot = now
 	for _, slot := range gts.tokenSlots {
 		slot.requireTokensSum = 0
 		slot.tokenCapacity = evenTokens
 		slot.lastTokenCapacity = evenTokens
-		slot.lastReqTime = now
 	}
 }
 
