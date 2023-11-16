ef6ba8551e525a700546d6bdb7ad6766115209cc
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-11-16 14:29:16 +0800

    resourcemanager: return resource-group priority in OnRequestWait (#7378) (#7383)
    close tikv/pd#7379, ref tikv/tikv#15994
    
    Signed-off-by: glorv <glorvs@163.com>
    Signed-off-by: JmPotato <ghzpotato@gmail.com>
    
    Co-authored-by: glorv <glorvs@163.com>
    Co-authored-by: JmPotato <ghzpotato@gmail.com>

--- client/resource_group/controller/controller.go
+++ client/resource_group/controller/controller.go
@@ -57,7 +57,7 @@ const (
 // ResourceGroupKVInterceptor is used as quota limit controller for resource group using kv store.
 type ResourceGroupKVInterceptor interface {
 	// OnRequestWait is used to check whether resource group has enough tokens. It maybe needs to wait some time.
-	OnRequestWait(ctx context.Context, resourceGroupName string, info RequestInfo) (*rmpb.Consumption, *rmpb.Consumption, error)
+	OnRequestWait(ctx context.Context, resourceGroupName string, info RequestInfo) (*rmpb.Consumption, *rmpb.Consumption, uint32, error)
 	// OnResponse is used to consume tokens after receiving response.
 	OnResponse(resourceGroupName string, req RequestInfo, resp ResponseInfo) (*rmpb.Consumption, error)
 	// IsBackgroundRequest If the resource group has background jobs, we should not record consumption and wait for it.
@@ -171,12 +171,13 @@ func loadServerConfig(ctx context.Context, provider ResourceGroupProvider) (*Con
 	if err != nil {
 		return nil, err
 	}
-	if len(resp.Kvs) == 0 {
+	kvs := resp.GetKvs()
+	if len(kvs) == 0 {
 		log.Warn("[resource group controller] server does not save config, load config failed")
 		return DefaultConfig(), nil
 	}
 	config := &Config{}
-	err = json.Unmarshal(resp.Kvs[0].GetValue(), config)
+	err = json.Unmarshal(kvs[0].GetValue(), config)
 	if err != nil {
 		return nil, err
 	}
@@ -525,10 +526,10 @@ func (c *ResourceGroupsController) sendTokenBucketRequests(ctx context.Context, 
 // OnRequestWait is used to check whether resource group has enough tokens. It maybe needs to wait some time.
 func (c *ResourceGroupsController) OnRequestWait(
 	ctx context.Context, resourceGroupName string, info RequestInfo,
-) (*rmpb.Consumption, *rmpb.Consumption, error) {
+) (*rmpb.Consumption, *rmpb.Consumption, uint32, error) {
 	gc, err := c.tryGetResourceGroup(ctx, resourceGroupName)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, 0, err
 	}
 	return gc.onRequestWait(ctx, info)
 }
@@ -1175,7 +1176,7 @@ func (gc *groupCostController) calcRequest(counter *tokenCounter) float64 {
 
 func (gc *groupCostController) onRequestWait(
 	ctx context.Context, info RequestInfo,
-) (*rmpb.Consumption, *rmpb.Consumption, error) {
+) (*rmpb.Consumption, *rmpb.Consumption, uint32, error) {
 	delta := &rmpb.Consumption{}
 	for _, calc := range gc.calculators {
 		calc.BeforeKVRequest(delta, info)
@@ -1225,7 +1226,7 @@ func (gc *groupCostController) onRequestWait(
 			failpoint.Inject("triggerUpdate", func() {
 				gc.lowRUNotifyChan <- struct{}{}
 			})
-			return nil, nil, err
+			return nil, nil, 0, err
 		}
 		gc.successfulRequestDuration.Observe(d.Seconds())
 	}
@@ -1244,7 +1245,7 @@ func (gc *groupCostController) onRequestWait(
 	*gc.mu.storeCounter[info.StoreID()] = *gc.mu.globalCounter
 	gc.mu.Unlock()
 
-	return delta, penalty, nil
+	return delta, penalty, gc.meta.Priority, nil
 }
 
 func (gc *groupCostController) onResponse(
