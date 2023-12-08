0c70123349d4655a79e3e2025494d29a947960e2
lhy1024 <admin@liudos.us>
2023-12-08 18:39:40 +0800

    make test stable
    Signed-off-by: lhy1024 <admin@liudos.us>

--- tests/server/api/region_test.go
+++ tests/server/api/region_test.go
@@ -24,11 +24,13 @@ import (
 
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/kvproto/pkg/metapb"
+	"github.com/pingcap/log"
 	"github.com/stretchr/testify/suite"
 	"github.com/tikv/pd/pkg/core"
 	"github.com/tikv/pd/pkg/schedule/placement"
 	tu "github.com/tikv/pd/pkg/utils/testutil"
 	"github.com/tikv/pd/tests"
+	"go.uber.org/zap"
 )
 
 type regionTestSuite struct {
@@ -335,9 +337,20 @@ func (suite *regionTestSuite) checkRegionsReplicated(cluster *tests.TestCluster)
 	err = tu.CheckPostJSON(testDialClient, urlPrefix+"/config/placement-rule", data, tu.StatusOK(re))
 	suite.NoError(err)
 
-	err = tu.ReadGetJSON(re, testDialClient, url, &status)
-	suite.NoError(err)
-	suite.Equal("REPLICATED", status)
+	tu.Eventually(re, func() bool {
+		respBundle := make([]placement.GroupBundle, 0)
+		err = tu.CheckGetJSON(testDialClient, urlPrefix+"/config/placement-rule", nil,
+			tu.StatusOK(re), tu.ExtractJSON(re, &respBundle))
+		suite.NoError(err)
+		log.Info("respBundle", zap.Any("respBundle", respBundle))
+		return len(respBundle) == 1 && len(respBundle[0].Rules) == 2
+	})
+
+	tu.Eventually(re, func() bool {
+		err = tu.ReadGetJSON(re, testDialClient, url, &status)
+		suite.NoError(err)
+		return status == "REPLICATED"
+	})
 
 	// test multiple bundles
 	bundle = append(bundle, placement.GroupBundle{
@@ -377,9 +390,11 @@ func (suite *regionTestSuite) checkRegionsReplicated(cluster *tests.TestCluster)
 	r1.GetMeta().Peers = append(r1.GetMeta().Peers, &metapb.Peer{Id: 5, StoreId: 1}, &metapb.Peer{Id: 6, StoreId: 1}, &metapb.Peer{Id: 7, StoreId: 1})
 	tests.MustPutRegionInfo(re, cluster, r1)
 
-	err = tu.ReadGetJSON(re, testDialClient, url, &status)
-	suite.NoError(err)
-	suite.Equal("REPLICATED", status)
+	tu.Eventually(re, func() bool {
+		err = tu.ReadGetJSON(re, testDialClient, url, &status)
+		suite.NoError(err)
+		return status == "REPLICATED"
+	})
 }
 
 func (suite *regionTestSuite) checkRegionCount(cluster *tests.TestCluster, count uint64) {
