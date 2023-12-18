92f8a7a687620cd44b38fc3ee630a88977644167
husharp <jinhao.hu@pingcap.com>
2023-12-18 18:16:43 +0800

    return err
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- pkg/member/member.go
+++ pkg/member/member.go
@@ -185,12 +185,16 @@ func (m *EmbeddedEtcdMember) CampaignLeader(ctx context.Context, leaseTimeout in
 	failpoint.Inject("skipCampaignLeaderCheck", func() {
 		failpoint.Return(m.leadership.Campaign(leaseTimeout, m.MemberValue()))
 	})
+
 	if m.leadership.GetCampaignTimesNum() >= campaignLeaderFrequencyTimes {
-		log.Warn("campaign times is too frequent, resign and campaign again",
-			zap.String("leader-name", m.Name()), zap.String("leader-key", m.GetLeaderPath()))
 		m.leadership.ResetCampaignTimes()
-		return m.ResignEtcdLeader(ctx, m.Name(), "")
+		if err := m.ResignEtcdLeader(ctx, m.Name(), ""); err != nil {
+			return err
+		}
+		return errors.Errorf("campaign times is too frequent, resign and campaign again, "+
+			"leader-name: %s, leader-key: %s", m.Name(), m.GetLeaderPath())
 	}
+
 	return m.leadership.Campaign(leaseTimeout, m.MemberValue())
 }
 
