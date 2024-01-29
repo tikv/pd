83ddd7779954c058502c241021437b5920c07544
husharp <jinhao.hu@pingcap.com>
2024-01-29 12:07:39 +0800

    fix mcs scheduling ci
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- scripts/ci-subtask.sh
+++ scripts/ci-subtask.sh
@@ -10,7 +10,7 @@ if [[ $2 -gt 10 ]]; then
     # Currently, we only have 3 integration tests, so we can hardcode the task index.
     for t in "${integrations_tasks[@]}"; do
         if [[ "$t" = "$integrations_dir/client" && $2 -eq 11 ]]; then
-            cd ./client && make ci-test-job && cd .. && cat ./client/covprofile >> covprofile
+            cd ./client && make ci-test-job && cd .. && cat ./covprofile >> covprofile || exit 1
             cd $integrations_dir && make ci-test-job test_name=client && cat ./client/covprofile >> "$ROOT_PATH/covprofile" || exit 1
         elif [[ "$t" = "$integrations_dir/tso" && $2 -eq 12 ]]; then
             cd $integrations_dir && make ci-test-job test_name=tso && cat ./tso/covprofile >> "$ROOT_PATH/covprofile" || exit 1
