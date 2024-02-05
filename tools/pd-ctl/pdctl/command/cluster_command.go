ade96f4c0f59b0afdd57b0e6e311988ccac8dce9
JmPotato <ghzpotato@gmail.com>
2024-02-05 14:09:57 +0800

    Use cmd.Context()
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- tools/pd-ctl/pdctl/command/cluster_command.go
+++ tools/pd-ctl/pdctl/command/cluster_command.go
@@ -14,11 +14,7 @@
 
 package command
 
-import (
-	"context"
-
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
 
 // NewClusterCommand return a cluster subcommand of rootCmd
 func NewClusterCommand() *cobra.Command {
@@ -42,7 +38,7 @@ func NewClusterStatusCommand() *cobra.Command {
 }
 
 func showClusterCommandFunc(cmd *cobra.Command, _ []string) {
-	info, err := PDCli.GetCluster(context.Background())
+	info, err := PDCli.GetCluster(cmd.Context())
 	if err != nil {
 		cmd.Printf("Failed to get the cluster information: %s\n", err)
 		return
@@ -51,7 +47,7 @@ func showClusterCommandFunc(cmd *cobra.Command, _ []string) {
 }
 
 func showClusterStatusCommandFunc(cmd *cobra.Command, _ []string) {
-	status, err := PDCli.GetClusterStatus(context.Background())
+	status, err := PDCli.GetClusterStatus(cmd.Context())
 	if err != nil {
 		cmd.Printf("Failed to get the cluster status: %s\n", err)
 		return
