dee209b24cde7aa3bdc5d5c995c81372b09c6861
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-04-01 14:13:46 +0800

    client: remove failed request metrics for background setting query (#7925) (#7927)
    close tikv/pd#7926
    
    Signed-off-by: Cabinfever_B <cabinfeveroier@gmail.com>
    
    Co-authored-by: Cabinfever_B <cabinfeveroier@gmail.com>
    Co-authored-by: Yongbo Jiang <cabinfeveroier@gmail.com>

--- client/resource_group/controller/controller.go
+++ client/resource_group/controller/controller.go
@@ -563,7 +563,6 @@ func (c *ResourceGroupsController) IsBackgroundRequest(ctx context.Context,
 	resourceGroupName, requestResource string) bool {
 	gc, err := c.tryGetResourceGroup(ctx, resourceGroupName)
 	if err != nil {
-		failedRequestCounter.WithLabelValues(resourceGroupName).Inc()
 		return false
 	}
 
@@ -576,7 +575,6 @@ func (c *ResourceGroupsController) checkBackgroundSettings(ctx context.Context, 
 		resourceGroupName := "default"
 		gc, err := c.tryGetResourceGroup(ctx, resourceGroupName)
 		if err != nil {
-			failedRequestCounter.WithLabelValues(resourceGroupName).Inc()
 			return false
 		}
 		bg = gc.getMeta().BackgroundSettings
