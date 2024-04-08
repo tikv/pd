146b99fb1ab1b0ce6d79ed834acf33e5c68dd5d9
husharp <jinhao.hu@pingcap.com>
2024-04-08 15:45:03 +0800

    refine covprofile
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- scripts/ci-subtask.sh
+++ scripts/ci-subtask.sh
@@ -7,7 +7,7 @@ ROOT_PATH_COV=$(pwd)/covprofile
 if [[ $2 -gt 9 ]]; then
     # run tools tests
     if [[ $2 -eq 10 ]]; then
-        cd ./tools && make ci-test-job && cat covprofile >> ../covprofile && cd .. || exit 1
+        cd ./tools && make ci-test-job && cat covprofile >> $ROOT_PATH_COV || exit 1
         exit
     fi
 
@@ -16,7 +16,7 @@ if [[ $2 -gt 9 ]]; then
     integrations_tasks=($(find "$integrations_dir" -mindepth 1 -maxdepth 1 -type d))
     for t in "${integrations_tasks[@]}"; do
         if [[ "$t" = "$integrations_dir/client" && $2 -eq 11 ]]; then
-            cd ./client && make ci-test-job && cat covprofile >> ../covprofile && cd .. || exit 1
+            cd ./client && make ci-test-job && cat covprofile >> $ROOT_PATH_COV && cd .. || exit 1
             cd $integrations_dir && make ci-test-job test_name=client && cat ./client/covprofile >> $ROOT_PATH_COV || exit 1
         elif [[ "$t" = "$integrations_dir/tso" && $2 -eq 12 ]]; then
             cd $integrations_dir && make ci-test-job test_name=tso && cat ./tso/covprofile >> $ROOT_PATH_COV || exit 1
@@ -61,5 +61,5 @@ else
         [[ $(($min_i + 1)) -eq $2 ]] && res+=($t)
     done
 
-    CGO_ENABLED=1 go test -timeout=15m -tags deadlock -race -covermode=atomic -coverprofile=covprofile -coverpkg=./... ${res[@]}
+    CGO_ENABLED=1 go test -timeout=15m -tags deadlock -race -covermode=atomic -coverprofile=$ROOT_PATH_COV -coverpkg=./... ${res[@]}
 fi
