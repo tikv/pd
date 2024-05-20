13cfb17d7d391f61dc35efb07813f71c015fee7c
nolouch <nolouch@gmail.com>
2024-05-20 14:10:07 +0800

    fix
    Signed-off-by: nolouch <nolouch@gmail.com>

--- client/resource_group/controller/limiter.go
+++ client/resource_group/controller/limiter.go
@@ -376,16 +376,6 @@ func (lim *Limiter) reserveN(now time.Time, n float64, maxFutureReserve time.Dur
 		lim.tokens = tokens
 		lim.maybeNotify()
 	} else {
-<<<<<<< HEAD
-		log.Warn("[resource group controller] cannot reserve enough tokens",
-			zap.Duration("need-wait-duration", waitDuration),
-			zap.Duration("max-wait-duration", maxFutureReserve),
-			zap.Float64("current-ltb-tokens", lim.tokens),
-			zap.Float64("current-ltb-rate", float64(lim.limit)),
-			zap.Float64("request-tokens", n),
-			zap.Int64("burst", lim.burst),
-			zap.Int("remaining-notify-times", lim.remainingNotifyTimes))
-=======
 		// print log if the limiter cannot reserve for a while.
 		if time.Since(lim.last) > reserveWarnLogInterval {
 			log.Warn("[resource group controller] cannot reserve enough tokens",
@@ -399,7 +389,6 @@ func (lim *Limiter) reserveN(now time.Time, n float64, maxFutureReserve time.Dur
 				zap.Int64("burst", lim.burst),
 				zap.Int("remaining-notify-times", lim.remainingNotifyTimes))
 		}
->>>>>>> a674e668f (controller: Reduce the frequency of printing logs (#8160))
 		lim.last = last
 		if lim.limit == 0 {
 			lim.notify()
