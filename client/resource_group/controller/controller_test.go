9d9b9448fc990f88679f53994392a7f24263b38f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-05-31 17:19:22 +0800

    controller: fix error retry and add more metrics (#8219) (#8232)
    close tikv/pd#8217
    
    controller: fix error retry and add more metrics
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: nolouch <nolouch@gmail.com>
    
    Co-authored-by: ShuNing <nolouch@gmail.com>
    Co-authored-by: nolouch <nolouch@gmail.com>

--- client/resource_group/controller/controller_test.go
+++ client/resource_group/controller/controller_test.go
@@ -26,6 +26,7 @@ import (
 
 	rmpb "github.com/pingcap/kvproto/pkg/resource_manager"
 	"github.com/stretchr/testify/require"
+	"github.com/tikv/pd/client/errs"
 )
 
 func createTestGroupCostController(re *require.Assertions) *groupCostController {
@@ -111,4 +112,18 @@ func TestRequestAndResponseConsumption(t *testing.T) {
 		re.Equal(expectedConsumption.RRU, consumption.RRU, caseNum)
 		re.Equal(expectedConsumption.TotalCpuTimeMs, consumption.TotalCpuTimeMs, caseNum)
 	}
+}
+
+func TestResourceGroupThrottledError(t *testing.T) {
+	re := require.New(t)
+	gc := createTestGroupCostController(re)
+	gc.initRunState()
+	req := &TestRequestInfo{
+		isWrite:    true,
+		writeBytes: 10000000,
+	}
+	// The group is throttled
+	_, _, err := gc.onRequestWait(context.TODO(), req)
+	re.Error(err)
+	re.True(errs.ErrClientResourceGroupThrottled.Equal(err))
 }
