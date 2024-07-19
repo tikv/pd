c1b8865e6a7d567a22fd7fceeed9ccbb62a2064c
Ryan Leung <rleungx@gmail.com>
2024-07-19 18:19:30 +0800

    This is an automated cherry-pick of #8415
    close tikv/pd#8419
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/keyspace/keyspace.go
+++ pkg/keyspace/keyspace.go
@@ -31,6 +31,11 @@ import (
 	"github.com/tikv/pd/pkg/slice"
 	"github.com/tikv/pd/pkg/storage/endpoint"
 	"github.com/tikv/pd/pkg/storage/kv"
+<<<<<<< HEAD
+=======
+	"github.com/tikv/pd/pkg/utils/etcdutil"
+	"github.com/tikv/pd/pkg/utils/logutil"
+>>>>>>> 2d8e03f33 (*: fix redact log (#8415))
 	"github.com/tikv/pd/pkg/utils/syncutil"
 	"go.uber.org/zap"
 )
@@ -331,7 +336,7 @@ func (manager *Manager) splitKeyspaceRegion(id uint32, waitRegionSplit bool) (er
 	if waitRegionSplit {
 		ranges := keyspaceRule.Data.([]*labeler.KeyRangeRule)
 		if len(ranges) < 2 {
-			log.Warn("[keyspace] failed to split keyspace region with insufficient range", zap.Any("label-rule", keyspaceRule))
+			log.Warn("[keyspace] failed to split keyspace region with insufficient range", logutil.ZapRedactString("label-rule", keyspaceRule.String()))
 			return ErrRegionSplitFailed
 		}
 		rawLeftBound, rawRightBound := ranges[0].StartKey, ranges[0].EndKey
@@ -380,7 +385,7 @@ func (manager *Manager) splitKeyspaceRegion(id uint32, waitRegionSplit bool) (er
 
 	log.Info("[keyspace] added region label for keyspace",
 		zap.Uint32("keyspace-id", id),
-		zap.Any("label-rule", keyspaceRule),
+		logutil.ZapRedactString("label-rule", keyspaceRule.String()),
 		zap.Duration("takes", time.Since(start)),
 	)
 	return
