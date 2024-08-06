b50c3c5854e999edffe2e8f2de4c226a2ad5e8c8
Ryan Leung <rleungx@gmail.com>
2024-08-06 13:52:18 +0800

    typo
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/mcs/scheduling/server/config/config.go
+++ pkg/mcs/scheduling/server/config/config.go
@@ -34,7 +34,7 @@ import (
 	"github.com/tikv/pd/pkg/cache"
 	"github.com/tikv/pd/pkg/core/constant"
 	"github.com/tikv/pd/pkg/core/storelimit"
-	mcsconstat "github.com/tikv/pd/pkg/mcs/utils/constant"
+	mcsconstant "github.com/tikv/pd/pkg/mcs/utils/constant"
 	sc "github.com/tikv/pd/pkg/schedule/config"
 	types "github.com/tikv/pd/pkg/schedule/type"
 	"github.com/tikv/pd/pkg/slice"
@@ -144,7 +144,7 @@ func (c *Config) adjust(meta *toml.MetaData) error {
 	configutil.AdjustString(&c.AdvertiseListenAddr, c.ListenAddr)
 
 	if !configMetaData.IsDefined("enable-grpc-gateway") {
-		c.EnableGRPCGateway = mcsconstat.DefaultEnableGRPCGateway
+		c.EnableGRPCGateway = mcsconstant.DefaultEnableGRPCGateway
 	}
 
 	c.adjustLog(configMetaData.Child("log"))
@@ -152,7 +152,7 @@ func (c *Config) adjust(meta *toml.MetaData) error {
 		return err
 	}
 
-	configutil.AdjustInt64(&c.LeaderLease, mcsconstat.DefaultLeaderLease)
+	configutil.AdjustInt64(&c.LeaderLease, mcsconstant.DefaultLeaderLease)
 
 	if err := c.Schedule.Adjust(configMetaData.Child("schedule"), false); err != nil {
 		return err
@@ -162,10 +162,10 @@ func (c *Config) adjust(meta *toml.MetaData) error {
 
 func (c *Config) adjustLog(meta *configutil.ConfigMetaData) {
 	if !meta.IsDefined("disable-error-verbose") {
-		c.Log.DisableErrorVerbose = mcsconstat.DefaultDisableErrorVerbose
+		c.Log.DisableErrorVerbose = mcsconstant.DefaultDisableErrorVerbose
 	}
-	configutil.AdjustString(&c.Log.Format, mcsconstat.DefaultLogFormat)
-	configutil.AdjustString(&c.Log.Level, mcsconstat.DefaultLogLevel)
+	configutil.AdjustString(&c.Log.Format, mcsconstant.DefaultLogFormat)
+	configutil.AdjustString(&c.Log.Level, mcsconstant.DefaultLogLevel)
 }
 
 // GetName returns the Name
