933400e16b2a5a0dc2286e279755c96b21152228
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-10-31 15:37:15 +0800

    checker, statistic: avoid leak in label statistic (#8703) (#8751)
    close tikv/pd#8700
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    Co-authored-by: lhy1024 <admin@liudos.us>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -1058,7 +1058,7 @@ func (c *RaftCluster) processRegionHeartbeat(region *core.RegionInfo) error {
 			if c.regionStats != nil {
 				c.regionStats.ClearDefunctRegion(item.GetID())
 			}
-			c.labelLevelStats.ClearDefunctRegion(item.GetID())
+			c.labelLevelStats.MarkDefunctRegion(item.GetID())
 			c.ruleManager.InvalidCache(item.GetID())
 		}
 		regionUpdateCacheEventCounter.Inc()
@@ -2167,6 +2167,7 @@ func (c *RaftCluster) updateRegionsLabelLevelStats(regions []*core.RegionInfo) {
 	for _, region := range regions {
 		c.labelLevelStats.Observe(region, c.getStoresWithoutLabelLocked(region, core.EngineKey, core.EngineTiFlash), c.opt.GetLocationLabels())
 	}
+	c.labelLevelStats.ClearDefunctRegions()
 }
 
 func (c *RaftCluster) getRegionStoresLocked(region *core.RegionInfo) []*core.StoreInfo {
