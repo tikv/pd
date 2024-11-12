ec50c2ff7a78f28a983dbf20a7929338078451fa
okJiang <819421878@qq.com>
2024-11-12 16:20:04 +0800

    fix ut
    Signed-off-by: okJiang <819421878@qq.com>

--- client/caller/utils_test.go
+++ client/caller/utils_test.go
@@ -9,17 +9,11 @@ import (
 func TestGetComponent(t *testing.T) {
 	re := require.New(t)
 
-	// Test that GetComponent returns the correct package path
-	expectedComponent := "github.com/tikv/pd/client/caller"
-	actualComponent := GetComponent(0)
-	re.Equal(expectedComponent, actualComponent)
+	re.Equal(Component("github.com/tikv/pd/client/caller"), GetComponent(0))
 }
 
 func TestGetCallerID(t *testing.T) {
 	re := require.New(t)
 
-	// Test that GetCallerID returns the correct process name
-	expectedCallerID := "caller.test"
-	actualCallerID := GetCallerID()
-	re.Equal(expectedCallerID, actualCallerID)
+	re.Equal(ID("caller.test"), GetCallerID())
 }
