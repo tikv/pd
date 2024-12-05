d04c54c5786c7f904eb8f421b947a0e1909be9c0
Ryan Leung <rleungx@gmail.com>
2024-12-05 17:21:01 +0800

    fix lint
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/mock/mockserver/mockserver.go
+++ pkg/mock/mockserver/mockserver.go
@@ -23,6 +23,7 @@ import (
 	"github.com/tikv/pd/pkg/utils/grpcutil"
 )
 
+// MockServer is used to mock Server for test use.
 type MockServer struct {
 	ctx            context.Context
 	member, leader *pdpb.Member
@@ -30,6 +31,7 @@ type MockServer struct {
 	bc             *core.BasicCluster
 }
 
+// NewMockServer creates a new MockServer.
 func NewMockServer(ctx context.Context, member, leader *pdpb.Member, storage storage.Storage, bc *core.BasicCluster) *MockServer {
 	return &MockServer{
 		ctx:     ctx,
@@ -40,38 +42,47 @@ func NewMockServer(ctx context.Context, member, leader *pdpb.Member, storage sto
 	}
 }
 
+// LoopContext returns the context of the server.
 func (s *MockServer) LoopContext() context.Context {
 	return s.ctx
 }
 
+// ClusterID returns the cluster ID of the server.
 func (*MockServer) ClusterID() uint64 {
 	return 1
 }
 
+// GetMemberInfo returns the member info of the server.
 func (s *MockServer) GetMemberInfo() *pdpb.Member {
 	return s.member
 }
 
+// GetLeader returns the leader of the server.
 func (s *MockServer) GetLeader() *pdpb.Member {
 	return s.leader
 }
 
+// GetStorage returns the storage of the server.
 func (s *MockServer) GetStorage() storage.Storage {
 	return s.storage
 }
 
+// Name returns the name of the server.
 func (*MockServer) Name() string {
 	return "mock-server"
 }
 
+// GetRegions returns the regions of the server.
 func (s *MockServer) GetRegions() []*core.RegionInfo {
 	return s.bc.GetRegions()
 }
 
+// GetTLSConfig returns the TLS config of the server.
 func (*MockServer) GetTLSConfig() *grpcutil.TLSConfig {
 	return &grpcutil.TLSConfig{}
 }
 
+// GetBasicCluster returns the basic cluster of the server.
 func (s *MockServer) GetBasicCluster() *core.BasicCluster {
 	return s.bc
 }
