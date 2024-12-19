68993a30790be603c4666736e92ed41748ab44bd
lhy1024 <admin@liudos.us>
2024-12-19 16:44:21 +0800

    change log level
    Signed-off-by: lhy1024 <admin@liudos.us>

--- pkg/schedule/schedulers/grant_hot_region.go
+++ pkg/schedule/schedulers/grant_hot_region.go
@@ -319,7 +319,7 @@ func (s *grantHotRegionScheduler) transfer(cluster sche.SchedulerCluster, region
 		op, err = operator.CreateMovePeerOperator(s.GetName()+"-move", cluster, srcRegion, operator.OpRegion|operator.OpLeader, srcStore.GetID(), dstStore)
 	}
 	if err != nil {
-		log.Error("fail to create grant hot leader operator", errs.ZapError(err))
+		log.Debug("fail to create grant hot leader operator", errs.ZapError(err))
 		return
 	}
 	op.SetPriorityLevel(constant.High)
