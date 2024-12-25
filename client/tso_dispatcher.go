82069935e3d45f57699395a319e22f8f561c913e
JmPotato <ghzpotato@gmail.com>
2024-12-25 16:03:03 +0800

    This is an automated cherry-pick of #8948
    close tikv/pd#8947
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- client/tso_dispatcher.go
+++ client/tso_dispatcher.go
@@ -473,15 +473,28 @@ func (td *tsoDispatcher) connectionCtxsUpdater() {
 		updateTicker   = &time.Ticker{}
 	)
 
+<<<<<<< HEAD:client/tso_dispatcher.go
 	log.Info("[tso] start tso connection contexts updater", zap.String("dc-location", dc))
 	setNewUpdateTicker := func(ticker *time.Ticker) {
+=======
+	log.Info("[tso] start tso connection contexts updater")
+	setNewUpdateTicker := func(interval time.Duration) {
+>>>>>>> 95bfbe69a (client/tso: init the ticker when TSO Follower Proxy is already enabled (#8948)):client/clients/tso/dispatcher.go
 		if updateTicker.C != nil {
 			updateTicker.Stop()
 		}
-		updateTicker = ticker
+		if interval == 0 {
+			updateTicker = &time.Ticker{}
+		} else {
+			updateTicker = time.NewTicker(interval)
+		}
+	}
+	// If the TSO Follower Proxy is enabled, set the update interval to the member update interval.
+	if option.GetEnableTSOFollowerProxy() {
+		setNewUpdateTicker(sd.MemberUpdateInterval)
 	}
 	// Set to nil before returning to ensure that the existing ticker can be GC.
-	defer setNewUpdateTicker(nil)
+	defer setNewUpdateTicker(0)
 
 	for {
 		provider.updateConnectionCtxs(ctx, dc, connectionCtxs)
@@ -501,13 +514,15 @@ func (td *tsoDispatcher) connectionCtxsUpdater() {
 			if enableTSOFollowerProxy && updateTicker.C == nil {
 				// Because the TSO Follower Proxy is enabled,
 				// the periodic check needs to be performed.
+<<<<<<< HEAD:client/tso_dispatcher.go
 				setNewUpdateTicker(time.NewTicker(memberUpdateInterval))
+=======
+				setNewUpdateTicker(sd.MemberUpdateInterval)
+>>>>>>> 95bfbe69a (client/tso: init the ticker when TSO Follower Proxy is already enabled (#8948)):client/clients/tso/dispatcher.go
 			} else if !enableTSOFollowerProxy && updateTicker.C != nil {
 				// Because the TSO Follower Proxy is disabled,
 				// the periodic check needs to be turned off.
-				setNewUpdateTicker(&time.Ticker{})
-			} else {
-				continue
+				setNewUpdateTicker(0)
 			}
 		case <-updateTicker.C:
 			// Triggered periodically when the TSO Follower Proxy is enabled.
