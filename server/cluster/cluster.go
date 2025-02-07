8325c12dfcbb802333c9da12d8113742583b1c5a
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-02-07 11:18:07 +0800

    statistics: add gc in hot peer cache (#8702) (#9023)
    close tikv/pd#8698
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>
    Co-authored-by: lhy1024 <admin@liudos.us>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -230,7 +230,7 @@ func (c *RaftCluster) InitCluster(
 	c.core, c.opt, c.storage, c.id = basicCluster, opt, storage, id
 	c.ctx, c.cancel = context.WithCancel(c.serverCtx)
 	c.labelLevelStats = statistics.NewLabelStatistics()
-	c.hotStat = statistics.NewHotStat(c.ctx)
+	c.hotStat = statistics.NewHotStat(c.ctx, basicCluster)
 	c.hotBuckets = buckets.NewBucketsCache(c.ctx)
 	c.progressManager = progress.NewManager()
 	c.changedRegions = make(chan *core.RegionInfo, defaultChangedRegionsLimit)
