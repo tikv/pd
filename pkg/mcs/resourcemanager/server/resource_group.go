cd74c8953c4637360e1491aab3d08e2b7cea62e8
lhy1024 <admin@liudos.us>
2025-02-11 04:05:27 +0800

    fix
    Signed-off-by: lhy1024 <admin@liudos.us>

--- pkg/mcs/resourcemanager/server/resource_group.go
+++ pkg/mcs/resourcemanager/server/resource_group.go
@@ -101,7 +101,7 @@ func (rg *ResourceGroup) Clone(withStats bool) *ResourceGroup {
 		newRG.RUConsumption = proto.Clone(rg.RUConsumption).(*rmpb.Consumption)
 		// only show the burst limit factor when the burst limit is -2, otherwise, it is meaningless
 		// when the burst limit is -2, the burst mode is moderated by the burst limit factor
-		if rg.getBurstLimit() == -2 {
+		if rg.RUSettings.RU.Settings.BurstLimit == -2 {
 			newRG.RUSettings.RU.BurstLimitFactor = defaultBurstLimitFactor
 		}
 	}
