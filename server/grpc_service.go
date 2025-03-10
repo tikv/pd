8626b21d1f712ef7958a8c99125468cb06bcedc5
okJiang <819421878@qq.com>
2025-03-10 17:57:23 +0800

    fix conflict
    Signed-off-by: okJiang <819421878@qq.com>

--- server/grpc_service.go
+++ server/grpc_service.go
@@ -705,9 +705,7 @@ func (s *GrpcServer) IsSnapshotRecovering(ctx context.Context, _ *pdpb.IsSnapsho
 	if s.IsClosed() {
 		return nil, errs.ErrNotStarted
 	}
-	if s.IsClosed() {
-		return nil, ErrNotStarted
-	}
+
 	// recovering mark is stored in etcd directly, there's no need to forward.
 	marked, err := s.Server.IsSnapshotRecovering(ctx)
 	if err != nil {
