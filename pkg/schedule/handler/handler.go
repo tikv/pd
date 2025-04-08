bf9a54e61d9c63c26eb6c0e34a32928991d7680a
Ryan Leung <rleungx@gmail.com>
2025-04-08 13:02:07 +0800

    return error if regions not found
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/schedule/handler/handler.go
+++ pkg/schedule/handler/handler.go
@@ -1284,6 +1284,9 @@ func (h *Handler) CheckRegionsReplicated(rawStartKey, rawEndKey string) (string,
 		return "", errs.ErrNotBootstrapped.GenWithStackByArgs()
 	}
 	regions := c.ScanRegions(startKey, endKey, -1)
+	if len(regions) == 0 {
+		return "", errs.ErrRegionNotFound.GenWithStackByArgs()
+	}
 	state := "REPLICATED"
 	for _, region := range regions {
 		if !filter.IsRegionReplicated(c, region) {
