5d37c374547580dc50abbde5a97ce614501ad81b
Ryan Leung <rleungx@gmail.com>
2025-04-14 14:35:23 +0800

    address the comment
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- client/pkg/circuitbreaker/circuit_breaker.go
+++ client/pkg/circuitbreaker/circuit_breaker.go
@@ -325,7 +325,7 @@ func (s *State) onResult(overloaded Overloading) {
 // Define context key type
 type cbCtxKey struct{}
 
-// CircuitBreakerKey used to store circuit breaker
+// CircuitBreakerKey is used to store circuit breaker
 var CircuitBreakerKey = cbCtxKey{}
 
 // FromContext retrieves the circuit breaker from the context
