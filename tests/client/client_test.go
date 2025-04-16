679ee8b9d4ed235dc54d3fe937d1d221bf8cf0cd
Ryan Leung <rleungx@gmail.com>
2025-04-16 17:22:38 +0800

    pkg: fix tso is generated even if the member is not leader (#9056) (#9207)
    * pkg: fix tso is generated even if the member is not leader (#9056)
    
    close tikv/pd#9051
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>
    
    * ci: transitioning action version from node 16 to node 20 (#8071)
    
    close tikv/pd#8070
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    
    ---------
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>
    Co-authored-by: Hu# <jinhao.hu@pingcap.com>

--- tests/client/client_test.go
+++ tests/client/client_test.go
@@ -1462,3 +1462,37 @@ func waitLeaderChange(re *require.Assertions, cluster *tests.TestCluster, old st
 	})
 	return leader
 }
+
+func TestTSONotLeader(t *testing.T) {
+	re := require.New(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	pdCluster, err := tests.NewTestCluster(ctx, 3)
+	re.NoError(err)
+	defer pdCluster.Destroy()
+	err = pdCluster.RunInitialServers()
+	re.NoError(err)
+	leaderName := pdCluster.WaitLeader()
+	re.NotEmpty(leaderName)
+	pdLeader := pdCluster.GetServer(leaderName)
+	backendEndpoints := pdLeader.GetAddr()
+	pdClient, err := pd.NewClientWithContext(context.Background(),
+		[]string{backendEndpoints}, pd.SecurityOption{}, pd.WithMaxErrorRetry(1))
+	re.NoError(err)
+	defer pdClient.Close()
+	re.NoError(failpoint.Enable("github.com/tikv/pd/server/rebaseErr", "return(true)"))
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(client pd.Client) {
+		defer wg.Done()
+		pdLeader.ResignLeader()
+		for i := 0; i < 10; i++ {
+			_, _, err := client.GetTS(ctx)
+			re.ErrorContains(err, "not leader")
+		}
+	}(pdClient)
+
+	wg.Wait()
+	re.NoError(failpoint.Disable("github.com/tikv/pd/server/rebaseErr"))
+}
