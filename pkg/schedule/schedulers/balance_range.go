58af5dbb4f32ca66f932cf3b405c4486417006f8
Ryan Leung <rleungx@gmail.com>
2025-04-21 11:56:57 +0800

    resolve conflicts
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- pkg/schedule/schedulers/balance_range.go
+++ pkg/schedule/schedulers/balance_range.go
@@ -588,7 +588,7 @@ func (s *balanceRangeScheduler) transferPeer(dstStores []*core.StoreInfo, faultS
 		filter.NewPlacementSafeguard(s.GetName(), conf, solver.GetBasicCluster(), solver.GetRuleManager(), solver.Region, solver.Source, solver.fit),
 	)
 
-	candidates := filter.NewCandidates(s.R, dstStores).FilterTarget(conf, nil, s.filterCounter, filters...)
+	candidates := filter.NewCandidates(dstStores).FilterTarget(conf, nil, s.filterCounter, filters...)
 	for i := range candidates.Stores {
 		solver.Target = candidates.Stores[len(candidates.Stores)-i-1]
 		solver.targetScore = s.score(solver.targetStoreID())
