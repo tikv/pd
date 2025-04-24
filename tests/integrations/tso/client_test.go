70e99033d29d0a123377a700d08c593d56582ee6
童剑 <1045931706@qq.com>
2025-04-24 10:44:15 +0800

    check the nil point
    Signed-off-by: 童剑 <1045931706@qq.com>

--- tests/integrations/tso/client_test.go
+++ tests/integrations/tso/client_test.go
@@ -511,10 +511,6 @@ func TestTSONotLeader(t *testing.T) {
 		pdLeader.ResignLeader()
 		for range 10 {
 			_, _, err := client.GetTS(ctx)
-			// stream maybe cancelld when the leader is resigned
-			if err.Error() == context.Canceled.Error() {
-				return
-			}
 			re.ErrorContains(err, "not leader")
 		}
 	}(pdClient)
