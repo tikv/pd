d5fd3ba5c1afec0d6e86325b3d9895e56ee9b158
JmPotato <ghzpotato@gmail.com>
2025-04-27 10:47:02 +0800

    This is an automated cherry-pick of #9267
    ref tikv/pd#9157
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/storage/endpoint/tso.go
+++ pkg/storage/endpoint/tso.go
@@ -68,8 +68,26 @@ func (se *StorageEndpoint) LoadTimestamp(prefix string) (time.Time, error) {
 	return maxTSWindow, nil
 }
 
+<<<<<<< HEAD
 // SaveTimestamp saves the timestamp to the storage.
 func (se *StorageEndpoint) SaveTimestamp(key string, ts time.Time) error {
+=======
+// SaveTimestamp saves the timestamp to the storage. The leadership is used to check if the current server is leader
+// before saving the timestamp to ensure a strong consistency for persistence of the TSO timestamp window.
+func (se *StorageEndpoint) SaveTimestamp(groupID uint32, ts time.Time, leadership *election.Leadership) error {
+	logFilds := []zap.Field{
+		zap.Uint32("group-id", groupID),
+		zap.Time("ts", ts),
+		zap.String("leader-key", leadership.GetLeaderKey()),
+		zap.String("expected-leader-value", leadership.GetLeaderValue()),
+	}
+	log.Debug("saving timestamp to the storage", logFilds...)
+	// The PD leadership or TSO primary will always be granted first before the TSO timestamp window is saved.
+	// So we here check whether the leader value is filled to see if the requirement is met.
+	if len(leadership.GetLeaderValue()) == 0 {
+		return errors.Errorf("%s due to leadership has not been granted yet", errs.NotLeaderErr)
+	}
+>>>>>>> 873ea1ec8 (fix(storage): change timestamp save log level from Info to Debug (#9267))
 	return se.RunInTxn(context.Background(), func(txn kv.Txn) error {
 		value, err := txn.Load(key)
 		if err != nil {
