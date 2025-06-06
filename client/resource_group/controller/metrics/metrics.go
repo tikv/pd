08e0941b5f4d1569ea20af48ac04c6a79f27118c
yibin87 <huyibin@pingcap.com>
2025-06-06 16:43:43 +0800

    little update
    Signed-off-by: yibin87 <huyibin@pingcap.com>

--- client/resource_group/controller/metrics/metrics.go
+++ client/resource_group/controller/metrics/metrics.go
@@ -29,18 +29,30 @@ const (
 )
 
 var (
-	ResourceGroupStatusGauge         *prometheus.GaugeVec
-	SuccessfulRequestDuration        *prometheus.HistogramVec
-	FailedLimitReserveDuration       *prometheus.HistogramVec
-	FailedRequestCounter             *prometheus.CounterVec
-	GroupRunningKVRequestCounter     *prometheus.GaugeVec
-	RequestRetryCounter              *prometheus.CounterVec
-	TokenRequestDuration             *prometheus.HistogramVec
+	//ResourceGroupStatusGauge comments placeholder
+	ResourceGroupStatusGauge *prometheus.GaugeVec
+	//SuccessfulRequestDuration comments placeholder
+	SuccessfulRequestDuration *prometheus.HistogramVec
+	//FailedLimitReserveDuration comments placeholder
+	FailedLimitReserveDuration *prometheus.HistogramVec
+	//FailedRequestCounter comments placeholder
+	FailedRequestCounter *prometheus.CounterVec
+	//GroupRunningKVRequestCounter comments placeholder
+	GroupRunningKVRequestCounter *prometheus.GaugeVec
+	//RequestRetryCounter comments placeholder
+	RequestRetryCounter *prometheus.CounterVec
+	//TokenRequestDuration comments placeholder
+	TokenRequestDuration *prometheus.HistogramVec
+	//ResourceGroupTokenRequestCounter comments placeholder
 	ResourceGroupTokenRequestCounter *prometheus.CounterVec
-	LowTokenRequestNotifyCounter     *prometheus.CounterVec
-	TokenConsumedHistogram           *prometheus.HistogramVec
+	//LowTokenRequestNotifyCounter comments placeholder
+	LowTokenRequestNotifyCounter *prometheus.CounterVec
+	//TokenConsumedHistogram comments placeholder
+	TokenConsumedHistogram *prometheus.HistogramVec
 	// WithLabelValues is a heavy operation, define variable to avoid call it every time.
-	FailedTokenRequestDuration     prometheus.Observer
+	//FailedTokenRequestDuration comments placeholder
+	FailedTokenRequestDuration prometheus.Observer
+	//SuccessfulTokenRequestDuration comments placeholder
 	SuccessfulTokenRequestDuration prometheus.Observer
 )
 
@@ -48,88 +60,98 @@ func init() {
 	initMetrics(nil)
 }
 
-func initMetrics(onstLabels prometheus.Labels) {
+func initMetrics(constLabels prometheus.Labels) {
 	ResourceGroupStatusGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: namespace,
-			Subsystem: "resource_group",
-			Name:      "status",
-			Help:      "Status of the resource group.",
+			Namespace:   namespace,
+			Subsystem:   "resource_group",
+			Name:        "status",
+			Help:        "Status of the resource group.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel})
 
 	SuccessfulRequestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Namespace: namespace,
-			Subsystem: requestSubsystem,
-			Name:      "success",
-			Buckets:   []float64{0.0005, .005, .01, .05, .1, .5, 1, 5, 10, 20, 25, 30, 60, 600, 1800, 3600}, // 0.0005 ~ 1h
-			Help:      "Bucketed histogram of wait duration of successful request.",
+			Namespace:   namespace,
+			Subsystem:   requestSubsystem,
+			Name:        "success",
+			Buckets:     []float64{0.0005, .005, .01, .05, .1, .5, 1, 5, 10, 20, 25, 30, 60, 600, 1800, 3600}, // 0.0005 ~ 1h
+			Help:        "Bucketed histogram of wait duration of successful request.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel})
 
 	FailedLimitReserveDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Namespace: namespace,
-			Subsystem: requestSubsystem,
-			Name:      "limit_reserve_time_failed",
-			Buckets:   []float64{0.0005, .01, .05, .1, .5, 1, 5, 10, 20, 25, 30, 60, 600, 1800, 3600, 86400}, // 0.0005 ~ 24h
-			Help:      "Bucketed histogram of wait duration of failed request.",
+			Namespace:   namespace,
+			Subsystem:   requestSubsystem,
+			Name:        "limit_reserve_time_failed",
+			Buckets:     []float64{0.0005, .01, .05, .1, .5, 1, 5, 10, 20, 25, 30, 60, 600, 1800, 3600, 86400}, // 0.0005 ~ 24h
+			Help:        "Bucketed histogram of wait duration of failed request.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel})
 
 	FailedRequestCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: requestSubsystem,
-			Name:      "fail",
-			Help:      "Counter of failed request.",
+			Namespace:   namespace,
+			Subsystem:   requestSubsystem,
+			Name:        "fail",
+			Help:        "Counter of failed request.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel, errType})
 
 	GroupRunningKVRequestCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: namespace,
-			Subsystem: requestSubsystem,
-			Name:      "running_kv_request",
-			Help:      "Counter of running kv request.",
+			Namespace:   namespace,
+			Subsystem:   requestSubsystem,
+			Name:        "running_kv_request",
+			Help:        "Counter of running kv request.",
+			ConstLabels: constLabels,
 		}, []string{newResourceGroupNameLabel})
 
 	RequestRetryCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: requestSubsystem,
-			Name:      "retry",
-			Help:      "Counter of retry time for request.",
+			Namespace:   namespace,
+			Subsystem:   requestSubsystem,
+			Name:        "retry",
+			Help:        "Counter of retry time for request.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel})
 
 	TokenRequestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Namespace: namespace,
-			Subsystem: tokenRequestSubsystem,
-			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 13), // 1ms ~ 8s
-			Name:      "duration",
-			Help:      "Bucketed histogram of latency(s) of token request.",
+			Namespace:   namespace,
+			Subsystem:   tokenRequestSubsystem,
+			Buckets:     prometheus.ExponentialBuckets(0.001, 2, 13), // 1ms ~ 8s
+			Name:        "duration",
+			Help:        "Bucketed histogram of latency(s) of token request.",
+			ConstLabels: constLabels,
 		}, []string{"type"})
 
 	ResourceGroupTokenRequestCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: tokenRequestSubsystem,
-			Name:      "resource_group",
-			Help:      "Counter of token request by every resource group.",
+			Namespace:   namespace,
+			Subsystem:   tokenRequestSubsystem,
+			Name:        "resource_group",
+			Help:        "Counter of token request by every resource group.",
+			ConstLabels: constLabels,
 		}, []string{resourceGroupNameLabel, newResourceGroupNameLabel})
 
 	LowTokenRequestNotifyCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Namespace: namespace,
-			Subsystem: tokenRequestSubsystem,
-			Name:      "low_token_notified",
-			Help:      "Counter of low token request.",
+			Namespace:   namespace,
+			Subsystem:   tokenRequestSubsystem,
+			Name:        "low_token_notified",
+			Help:        "Counter of low token request.",
+			ConstLabels: constLabels,
 		}, []string{newResourceGroupNameLabel})
 	TokenConsumedHistogram = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Namespace: namespace,
-			Subsystem: tokenRequestSubsystem,
-			Name:      "consume",
-			Buckets:   []float64{.5, 1, 2, 5, 10, 15, 20, 40, 64, 128, 256, 512, 1024, 2048}, // 0 ~ 2048
-			Help:      "Bucketed histogram of token consume.",
+			Namespace:   namespace,
+			Subsystem:   tokenRequestSubsystem,
+			Name:        "consume",
+			Buckets:     []float64{.5, 1, 2, 5, 10, 15, 20, 40, 64, 128, 256, 512, 1024, 2048}, // 0 ~ 2048
+			Help:        "Bucketed histogram of token consume.",
+			ConstLabels: constLabels,
 		}, []string{newResourceGroupNameLabel})
 
 	// WithLabelValues is a heavy operation, define variable to avoid call it every time.
