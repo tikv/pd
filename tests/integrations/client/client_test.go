80816bde9779e7f0b9edc97491180a95d01f44c9
gmhdbjd <gmhdbjd@gmail.com>
2025-06-11 14:37:35 +0800

    fix test
    Signed-off-by: gmhdbjd <gmhdbjd@gmail.com>

--- tests/integrations/client/client_test.go
+++ tests/integrations/client/client_test.go
@@ -1305,9 +1305,8 @@ func (suite *clientStatelessTestSuite) TestScatterRegion() {
 		if scatterResp.FinishedPercentage != uint64(100) {
 			re.Contains(scatterResp.FailedRegionsId, regionID)
 			return false
-		} else {
-			re.Empty(scatterResp.FailedRegionsId)
 		}
+		re.Empty(scatterResp.FailedRegionsId)
 		resp, err := suite.client.GetOperator(context.Background(), regionID)
 		if err != nil {
 			return false
