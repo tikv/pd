5c646e509c29040f9095bb288fa63053e202b562
JmPotato <github@ipotato.me>
2025-06-17 15:14:54 +0800

    feat(resourcemanager): persist the service limits for keyspaces
    Signed-off-by: JmPotato <github@ipotato.me>

--- pkg/utils/keypath/resource_group.go
+++ pkg/utils/keypath/resource_group.go
@@ -73,6 +73,16 @@ func KeyspaceResourceGroupStatePrefix() string {
 	return keyspaceResourceGroupStatesPathPrefixFormat
 }
 
+// KeyspaceServiceLimitPath returns the path to save the keyspace service limit.
+func KeyspaceServiceLimitPath(keyspaceID uint32) string {
+	return fmt.Sprintf(keyspaceServiceLimitPathFormat, keyspaceID)
+}
+
+// KeyspaceServiceLimitPrefix returns the prefix of the keyspace service limits.
+func KeyspaceServiceLimitPrefix() string {
+	return keyspaceServiceLimitPathPrefixFormat
+}
+
 // ParseKeyspaceResourceGroupPath parses the keyspace ID and resource group name from the keyspace resource group path.
 func ParseKeyspaceResourceGroupPath(path string) (uint32, string, error) {
 	parts := strings.Split(path, "/")
