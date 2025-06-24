6230a5f43942105a969a6e5bc17d9e93d6c772e5
lhy1024 <admin@liudos.us>
2025-06-24 12:03:59 +0800

    reduce unused error class
    Signed-off-by: lhy1024 <admin@liudos.us>

--- tools/pd-ctl/helper/tidb/dbterror/terror.go
+++ tools/pd-ctl/helper/tidb/dbterror/terror.go
@@ -10,28 +10,8 @@ type ErrClass struct{ terror.ErrClass }
 
 // Error classes.
 var (
-	ClassAutoid     = ErrClass{terror.ClassAutoid}
-	ClassDDL        = ErrClass{terror.ClassDDL}
-	ClassDomain     = ErrClass{terror.ClassDomain}
-	ClassExecutor   = ErrClass{terror.ClassExecutor}
-	ClassExpression = ErrClass{terror.ClassExpression}
-	ClassAdmin      = ErrClass{terror.ClassAdmin}
-	ClassKV         = ErrClass{terror.ClassKV}
-	ClassMeta       = ErrClass{terror.ClassMeta}
-	ClassOptimizer  = ErrClass{terror.ClassOptimizer}
-	ClassPrivilege  = ErrClass{terror.ClassPrivilege}
-	ClassSchema     = ErrClass{terror.ClassSchema}
-	ClassServer     = ErrClass{terror.ClassServer}
-	ClassStructure  = ErrClass{terror.ClassStructure}
-	ClassVariable   = ErrClass{terror.ClassVariable}
-	ClassXEval      = ErrClass{terror.ClassXEval}
-	ClassTable      = ErrClass{terror.ClassTable}
 	ClassTypes      = ErrClass{terror.ClassTypes}
 	ClassJSON       = ErrClass{terror.ClassJSON}
-	ClassTiKV       = ErrClass{terror.ClassTiKV}
-	ClassSession    = ErrClass{terror.ClassSession}
-	ClassPlugin     = ErrClass{terror.ClassPlugin}
-	ClassUtil       = ErrClass{terror.ClassUtil}
 )
 
 // NewStd calls New using the standard message for the error code
