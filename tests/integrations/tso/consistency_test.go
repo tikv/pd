dca65bcc48c6d8a37ca89a5cdc0e022c50b97837
Ryan Leung <rleungx@gmail.com>
2025-06-26 17:28:12 +0800

    remove unnecessary alias
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tests/integrations/tso/consistency_test.go
+++ tests/integrations/tso/consistency_test.go
@@ -31,7 +31,7 @@ import (
 	tsopkg "github.com/tikv/pd/pkg/tso"
 	"github.com/tikv/pd/pkg/utils/keypath"
 	"github.com/tikv/pd/pkg/utils/tempurl"
-	tu "github.com/tikv/pd/pkg/utils/testutil"
+	"github.com/tikv/pd/pkg/utils/testutil"
 	"github.com/tikv/pd/pkg/utils/tsoutil"
 	"github.com/tikv/pd/tests"
 )
@@ -87,7 +87,7 @@ func (suite *tsoConsistencyTestSuite) SetupSuite() {
 	suite.pdLeaderServer.BootstrapCluster()
 	backendEndpoints := suite.pdLeaderServer.GetAddr()
 	if suite.legacy {
-		suite.pdClient = tu.MustNewGrpcClient(re, backendEndpoints)
+		suite.pdClient = testutil.MustNewGrpcClient(re, backendEndpoints)
 	} else {
 		suite.tsoServer, suite.tsoServerCleanup = tests.StartSingleTSOTestServer(suite.ctx, re, backendEndpoints, tempurl.Alloc())
 		suite.tsoClientConn, suite.tsoClient = tso.MustNewGrpcClient(re, suite.tsoServer.GetAddr())
@@ -126,7 +126,7 @@ func (suite *tsoConsistencyTestSuite) request(ctx context.Context, count uint32)
 		Count:      count,
 	}
 	var resp *tsopb.TsoResponse
-	tu.Eventually(re, func() bool {
+	testutil.Eventually(re, func() bool {
 		tsoClient, err := suite.tsoClient.Tso(ctx)
 		re.NoError(err)
 		defer tsoClient.CloseSend()
