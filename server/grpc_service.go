ca4dd119dfe736402da176bedc67aa5dd6556b29
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-07-23 15:41:29 +0800

    tso: fix tso proxy error propagation and add test (#9219) (#9264)
    close tikv/pd#9188
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: JmPotato <github@ipotato.me>
    
    Co-authored-by: Artem Danilov <artem.danilov@airbnb.com>
    Co-authored-by: JmPotato <github@ipotato.me>

--- server/grpc_service.go
+++ server/grpc_service.go
@@ -372,26 +372,46 @@ func (s *GrpcServer) Tso(stream pdpb.PD_TsoServer) error {
 		return s.forwardTSO(stream)
 	}
 
-	var (
-		doneCh chan struct{}
-		errCh  chan error
-	)
+	var tsoRequestProxyCtx context.Context
 	ctx, cancel := context.WithCancel(stream.Context())
 	defer cancel()
 	for {
-		// Prevent unnecessary performance overhead of the channel.
-		if errCh != nil {
+		var (
+			request *pdpb.TsoRequest
+			err     error
+		)
+
+		if tsoRequestProxyCtx == nil {
+			request, err = stream.Recv()
+		} else {
+			// if we forward requests to TSO proxy we can't block on the next request in the stream
+			// as proxy might fail on the previous request, and we need to return the error to client
+
+			// Create a channel to receive the stream data or error asynchronously
+			streamCh := make(chan *pdpb.TsoRequest, 1)
+			streamErrCh := make(chan error, 1)
+			go func() {
+				req, err := stream.Recv()
+				if err != nil {
+					streamErrCh <- err
+				} else {
+					streamCh <- req
+				}
+			}()
+
+			// Wait for either stream data or error from tso proxy
 			select {
-			case err := <-errCh:
-				return errors.WithStack(err)
-			default:
+			case <-tsoRequestProxyCtx.Done():
+				err = context.Cause(tsoRequestProxyCtx)
+			case err = <-streamErrCh:
+			case req := <-streamCh:
+				request = req
 			}
 		}
-		request, err := stream.Recv()
+
 		if err == io.EOF {
 			return nil
-		}
-		if err != nil {
+		} else if err != nil {
 			return errors.WithStack(err)
 		}
 
@@ -407,14 +427,9 @@ func (s *GrpcServer) Tso(stream pdpb.PD_TsoServer) error {
 				return errors.WithStack(err)
 			}
 
-			if errCh == nil {
-				doneCh = make(chan struct{})
-				defer close(doneCh)
-				errCh = make(chan error)
-			}
-
 			tsoRequest := tsoutil.NewPDProtoRequest(forwardedHost, clientConn, request, stream)
-			s.tsoDispatcher.DispatchRequest(ctx, tsoRequest, s.pdProtoFactory, doneCh, errCh, s.tsoPrimaryWatcher)
+			// don't pass a stream context here as dispatcher serves multiple streams
+			tsoRequestProxyCtx = s.tsoDispatcher.DispatchRequest(s.ctx, tsoRequest, s.pdProtoFactory, s.tsoPrimaryWatcher)
 			continue
 		}
 
