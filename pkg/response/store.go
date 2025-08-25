14eb01623728adf6ac63c452f7f2ad21265cf871
okJiang <819421878@qq.com>
2025-08-25 17:26:07 +0800

    scheduler: implement network slow store scheduler (#9451)
    ref tikv/pd#9359
    
    Signed-off-by: okjiang <819421878@qq.com>

--- pkg/response/store.go
+++ pkg/response/store.go
@@ -67,6 +67,7 @@ type StoreStatus struct {
 	WitnessCount           int                `json:"witness_count,omitempty"`
 	PendingPeerCount       int                `json:"pending_peer_count,omitempty"`
 	SlowScore              uint64             `json:"slow_score,omitempty"`
+	NetworkSlowScore       map[uint64]uint64  `json:"network_slow_score,omitempty"`
 	SlowTrend              *SlowTrend         `json:"slow_trend,omitempty"`
 	SendingSnapCount       uint32             `json:"sending_snap_count,omitempty"`
 	ReceivingSnapCount     uint32             `json:"receiving_snap_count,omitempty"`
@@ -118,6 +119,7 @@ func BuildStoreInfo(opt *sc.ScheduleConfig, store *core.StoreInfo) *StoreInfo {
 			LearnerCount:           store.GetLearnerCount(),
 			WitnessCount:           store.GetWitnessCount(),
 			SlowScore:              store.GetSlowScore(),
+			NetworkSlowScore:       store.GetNetworkSlowScores(),
 			SlowTrend:              slowTrend,
 			SendingSnapCount:       store.GetSendingSnapCount(),
 			ReceivingSnapCount:     store.GetReceivingSnapCount(),
