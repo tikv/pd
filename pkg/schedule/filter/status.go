be5b6183e4acffceb4a0be327e7474c28355808c
HunDunDM <hundundm@gmail.com>
2025-09-22 16:15:22 +0800

    schedule: support disable region schedule
    Signed-off-by: HunDunDM <hundundm@gmail.com>

--- pkg/schedule/filter/status.go
+++ pkg/schedule/filter/status.go
@@ -50,4 +50,5 @@ var (
 	statusRegionNotReplicated               = plan.NewStatus(plan.StatusRegionNotReplicated)
 	statusRegionWitnessPeer                 = plan.NewStatus(plan.StatusRegionNotMatchRule)
 	statusRegionLeaderSendSnapshotThrottled = plan.NewStatus(plan.StatusRegionSendSnapshotThrottled)
+	statusRegionScheduleDeny                = plan.NewStatus(plan.StatusRegionScheduleDeny)
 )
