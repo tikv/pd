deda385d95ea313a3b4488bad73cb7e8ce3c78fb
lhy1024 <liuhanyang@pingcap.com>
2025-10-09 12:31:03 +0800

    This is an automated cherry-pick of #9796
    close tikv/pd#9797
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- pkg/mcs/tso/server/apis/v1/api.go
+++ pkg/mcs/tso/server/apis/v1/api.go
@@ -86,7 +86,6 @@ func NewService(srv *tsoserver.Service) *Service {
 	apiHandlerEngine.GET("status", utils.StatusHandler)
 	pprof.Register(apiHandlerEngine)
 	root := apiHandlerEngine.Group(APIPathPrefix)
-	root.Use(multiservicesapi.ServiceRedirector())
 	s := &Service{
 		srv:              srv,
 		apiHandlerEngine: apiHandlerEngine,
@@ -103,7 +102,12 @@ func NewService(srv *tsoserver.Service) *Service {
 // RegisterAdminRouter registers the router of the TSO admin handler.
 func (s *Service) RegisterAdminRouter() {
 	router := s.root.Group("admin")
+<<<<<<< HEAD
 	router.POST("/reset-ts", ResetTS)
+=======
+	// reset-ts needs to be forwarded to the primary.
+	router.POST("/reset-ts", multiservicesapi.ServiceRedirector(), resetTS)
+>>>>>>> f59aebd4a (mcs: fix `/config` and `/members` tso forward logic (#9796))
 	router.PUT("/log", changeLogLevel)
 }
 
@@ -125,6 +129,16 @@ func (s *Service) RegisterConfigRouter() {
 	router.GET("", getConfig)
 }
 
+<<<<<<< HEAD
+=======
+// RegisterPrimaryRouter registers the router of the primary handler.
+func (s *Service) RegisterPrimaryRouter() {
+	router := s.root.Group("primary")
+	// Transferring primary needs to be forwarded to the primary.
+	router.POST("transfer", multiservicesapi.ServiceRedirector(), transferPrimary)
+}
+
+>>>>>>> f59aebd4a (mcs: fix `/config` and `/members` tso forward logic (#9796))
 func changeLogLevel(c *gin.Context) {
 	svr := c.MustGet(multiservicesapi.ServiceContextKey).(*tsoserver.Service)
 	var level string
