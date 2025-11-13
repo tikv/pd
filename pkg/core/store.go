7c894e3cbdf94ad507d2f805f33fe35583834aa8
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-11-13 12:43:37 +0800

    scheduler: fix data race test in evict_stopping_store scheduler (#9828) (#9930)
    close tikv/pd#9812
    
    change the stopping state to atomic variable
    
    Signed-off-by: hujiatao0 <hhjjtt110@gmail.com>
    
    Co-authored-by: hujiatao0 <hhjjtt110@gmail.com>

--- pkg/core/store.go
+++ pkg/core/store.go
@@ -66,6 +66,7 @@ type StoreInfo struct {
 	pauseLeaderTransferOut atomic.Int64 // not allow to be used as source of transfer leader
 	slowStoreEvicted       atomic.Int64 // this store has been evicted as a slow store, should not transfer leader to it
 	slowTrendEvicted       atomic.Int64 // this store has been evicted as a slow store by trend, should not transfer leader to it
+	stoppingStoreEvicted   atomic.Int64 // this store has been evicted as a stopping store, should not transfer leader to it
 	leaderCount            int
 	regionCount            int
 	learnerCount           int
@@ -140,6 +141,7 @@ func (s *StoreInfo) Clone(opts ...StoreCreateOption) *StoreInfo {
 	store.pauseLeaderTransferOut.Store(s.pauseLeaderTransferOut.Load())
 	store.slowStoreEvicted.Store(s.slowStoreEvicted.Load())
 	store.slowTrendEvicted.Store(s.slowTrendEvicted.Load())
+	store.stoppingStoreEvicted.Store(s.stoppingStoreEvicted.Load())
 	for _, opt := range opts {
 		if opt != nil {
 			opt(store)
@@ -186,6 +188,7 @@ func (s *StoreInfo) ShallowClone(opts ...StoreCreateOption) *StoreInfo {
 	store.pauseLeaderTransferOut.Store(s.pauseLeaderTransferOut.Load())
 	store.slowStoreEvicted.Store(s.slowStoreEvicted.Load())
 	store.slowTrendEvicted.Store(s.slowTrendEvicted.Load())
+	store.stoppingStoreEvicted.Store(s.stoppingStoreEvicted.Load())
 	for _, opt := range opts {
 		opt(store)
 	}
@@ -211,7 +214,7 @@ func (s *StoreInfo) EvictedAsSlowStore() bool {
 
 // EvictedAsStoppingStore returns if the store should be evicted as a stopping store.
 func (s *StoreInfo) EvictedAsStoppingStore() bool {
-	return s.rawStats.IsStopping
+	return s.stoppingStoreEvicted.Load() > 0
 }
 
 // IsEvictedAsSlowTrend returns if the store should be evicted as a slow store by trend.
