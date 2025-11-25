d3c9b275246339474fc21b798a79e1ac133e26fb
okjiang <819421878@qq.com>
2025-11-25 18:08:42 +0800

    support TestServerPrimaryChange
    Signed-off-by: okjiang <819421878@qq.com>

--- tests/integrations/mcs/discovery/register_test.go
+++ tests/integrations/mcs/discovery/register_test.go
@@ -110,6 +110,7 @@ func (suite *serverRegisterTestSuite) checkServerRegister(serviceName string) {
 
 func (suite *serverRegisterTestSuite) TestServerPrimaryChange() {
 	suite.checkServerPrimaryChange(constant.TSOServiceName, 3)
+	suite.checkServerPrimaryChange(constant.ResourceManagerServiceName, 3)
 }
 
 func (suite *serverRegisterTestSuite) checkServerPrimaryChange(serviceName string, serverNum int) {
