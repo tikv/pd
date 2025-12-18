4f8e88c1af02c365c2f72c289726cce042d31714
lhy1024 <admin@liudos.us>
2025-12-18 17:12:17 +0800

    revert to `operator.OpAdmin|operator.OpMerge`
    Signed-off-by: lhy1024 <admin@liudos.us>

--- tests/server/api/operator_test.go
+++ tests/server/api/operator_test.go
@@ -571,7 +571,7 @@ func (suite *operatorTestSuite) checkGetOperatorsAsObject(cluster *tests.TestClu
 		ConfVer: 1,
 		Version: 1,
 	}, resp[0].RegionEpoch)
-	re.Equal(operator.OpAdmin, resp[0].Kind)
+	re.Equal(operator.OpAdmin|operator.OpMerge, resp[0].Kind)
 	re.Truef(resp[0].Status == operator.CREATED || resp[0].Status == operator.STARTED, "unexpected status %s", resp[0].Status)
 	re.Equal(uint64(20), resp[1].RegionID)
 	re.Equal("admin-merge-region", resp[1].Desc)
