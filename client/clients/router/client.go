c4fd9744894443b30661213ce5569ea7b9849a20
bufferflies <1045931706@qq.com>
2026-01-06 20:22:46 +0800

    resolve conflict
    Signed-off-by: bufferflies <1045931706@qq.com>

--- client/clients/router/client.go
+++ client/clients/router/client.go
@@ -31,6 +31,7 @@ import (
 
 	"github.com/pingcap/kvproto/pkg/metapb"
 	"github.com/pingcap/kvproto/pkg/pdpb"
+	"github.com/pingcap/kvproto/pkg/routerpb"
 	"github.com/pingcap/log"
 
 	"github.com/tikv/pd/client/errs"
@@ -178,6 +179,11 @@ type Cli struct {
 	conCtxMgr *cctx.Manager[pdpb.PD_QueryRegionClient]
 	// updateConnectionCh is used to trigger the connection update actively.
 	updateConnectionCh chan struct{}
+
+	// mcsDiscovery is the service discovery for the router mcs service.
+	mcsDiscovery sd.ServiceDiscovery
+	// mcsConCtxMgr is used to store the context of the router service stream connection.
+	mcsConCtxMgr *cctx.Manager[routerpb.Router_QueryRegionClient]
 	// bo is the backoffer for the router client.
 	bo *retry.Backoffer
 
@@ -190,6 +196,7 @@ type Cli struct {
 func NewClient(
 	ctx context.Context,
 	svcDiscovery sd.ServiceDiscovery,
+	mcsSvcDiscovery sd.ServiceDiscovery,
 	option *opt.Option,
 ) *Cli {
 	ctx, cancel := context.WithCancel(ctx)
@@ -200,6 +207,8 @@ func NewClient(
 		option:             option,
 		conCtxMgr:          cctx.NewManager[pdpb.PD_QueryRegionClient](),
 		updateConnectionCh: make(chan struct{}, 1),
+		mcsConCtxMgr:       cctx.NewManager[routerpb.Router_QueryRegionClient](),
+		mcsDiscovery:       mcsSvcDiscovery,
 		bo: retry.InitialBackoffer(
 			sd.UpdateMemberBackOffBaseTime,
 			sd.UpdateMemberMaxBackoffTime,
@@ -222,6 +231,7 @@ func NewClient(
 	c.leaderURL.Store(svcDiscovery.GetServingURL())
 	c.svcDiscovery.ExecAndAddLeaderSwitchedCallback(c.updateLeaderURL)
 	c.svcDiscovery.AddMembersChangedCallback(c.scheduleUpdateConnection)
+	c.mcsDiscovery.AddMembersChangedCallback(c.scheduleUpdateConnection)
 
 	c.wg.Add(2)
 	go c.connectionDaemon()
@@ -364,6 +374,23 @@ func (c *Cli) getAllClientConns() map[string]*grpc.ClientConn {
 	return conns
 }
 
+func (c *Cli) getAllMcsClientConns() map[string]*grpc.ClientConn {
+	conns := make(map[string]*grpc.ClientConn)
+	c.mcsDiscovery.GetClientConns().Range(func(key, value any) bool {
+		url, ok := key.(string)
+		if !ok || len(url) == 0 {
+			return true
+		}
+		conn, ok := value.(*grpc.ClientConn)
+		if !ok || conn == nil {
+			return true
+		}
+		conns[url] = conn
+		return true
+	})
+	return conns
+}
+
 // scheduleUpdateConnection is used to schedule an update to the connection(s).
 func (c *Cli) scheduleUpdateConnection() {
 	select {
@@ -385,6 +412,7 @@ func (c *Cli) connectionDaemon() {
 	log.Info("[router] connection daemon is started")
 	for {
 		c.updateConnection(updaterCtx)
+		c.updateMcsServiceConnection(updaterCtx)
 		select {
 		case <-updaterCtx.Done():
 			log.Info("[router] connection daemon is exiting")
@@ -399,6 +427,42 @@ func (c *Cli) connectionDaemon() {
 			// Triggered by the leader/follower change.
 		}
 	}
+}
+
+func (c *Cli) updateMcsServiceConnection(ctx context.Context) {
+	conns := c.getAllMcsClientConns()
+	if len(conns) == 0 {
+		log.Warn("[router] no router service node found")
+		return
+	}
+	// Add the missing router service stream connections.
+	for url, conn := range conns {
+		if c.mcsConCtxMgr.Exist(url) {
+			log.Debug("[router] the router service remains unchanged", zap.String("url", url))
+			continue
+		}
+		cctx, cancel := context.WithCancel(ctx)
+		stream, err := routerpb.NewRouterClient(conn).QueryRegion(cctx)
+		if err != nil {
+			log.Warn("[router] failed to create the router service stream connection", errs.ZapError(err))
+		}
+		// Store the stream connection context if it is successfully created.
+		if stream != nil {
+			c.mcsConCtxMgr.Store(cctx, cancel, url, stream)
+			log.Info("[router] successfully established the router service stream connection", zap.String("url", url))
+		} else {
+			log.Warn("[router] failed to create the router service stream connection")
+			cancel()
+		}
+	}
+	// Remove the stale follower router stream connections.
+	c.mcsConCtxMgr.GC(func(url string) bool {
+		if _, ok := conns[url]; !ok {
+			log.Info("[router] release the stale router service stream connection", zap.String("url", url))
+			return true
+		}
+		return false
+	})
 }
 
 // updateConnection is used to get the leader client connection and update the connection context if it does not exist before.
@@ -475,9 +539,7 @@ func (c *Cli) dispatcher() {
 	defer c.wg.Done()
 
 	var (
-		stream            pdpb.PD_QueryRegionClient
 		streamURL         string
-		streamCtx         context.Context
 		timeoutTimer      *time.Timer
 		resetTimeoutTimer = func() {
 			if timeoutTimer == nil {
@@ -519,6 +581,10 @@ batchLoop:
 
 		// Step 2: Choose a stream connection to send the router request.
 		resetTimeoutTimer()
+		var (
+			fn    processFn
+			retry bool
+		)
 	connectionCtxChoosingLoop:
 		for {
 			// Check if the dispatcher is canceled or the timeout timer is triggered.
@@ -532,53 +598,107 @@ batchLoop:
 				continue batchLoop
 			default:
 			}
-			// Check whether allow the follower to handle this batch of requests.
-			allowFollowerHandle := c.option.GetEnableFollowerHandle()
-			if allowFollowerHandle {
-				// We need to ensure all requests in a same batch allow to be handled by the follower.
-				// IMPROVE: separate into the follower and leader handle batches.
-				c.batchController.IterCollectedRequests(func(req *Request) bool {
-					if !req.options.AllowFollowerHandle {
-						allowFollowerHandle = false
-						return false
-					}
-					return true
-				})
+			fn, streamURL = c.sendToMcs(ctx)
+			if fn == nil {
+				fn, streamURL, retry = c.sendToPD(ctx)
 			}
-			// Check if the follower handle is enabled again before choosing the stream connection.
-			allowFollowerHandle = allowFollowerHandle && c.option.GetEnableFollowerHandle()
-			// Choose a stream connection to send the router request later.
-			var connectionCtx *cctx.ConnectionCtx[pdpb.PD_QueryRegionClient]
-			if allowFollowerHandle {
-				connectionCtx = c.conCtxMgr.RandomlyPick()
-			} else {
-				connectionCtx = c.conCtxMgr.GetConnectionCtx(c.getLeaderURL())
-			}
-			if connectionCtx == nil {
-				log.Info("[router] router stream connection is not ready")
-				c.updateConnection(ctx)
+			if retry {
 				continue connectionCtxChoosingLoop
 			}
-			streamCtx, streamURL, stream = connectionCtx.Ctx, connectionCtx.StreamURL, connectionCtx.Stream
-			// Check if the stream connection is canceled.
-			select {
-			case <-streamCtx.Done():
-				log.Info("[router] router stream connection is canceled", zap.String("stream-url", streamURL))
-				c.conCtxMgr.Release(streamURL)
-				continue connectionCtxChoosingLoop
-			default:
-			}
-			// The stream connection is ready, break the loop.
 			break connectionCtxChoosingLoop
 		}
 
 		// Step 3: Dispatch the router requests to the stream connection.
 		// TODO: timeout handling if the stream takes too long to process the requests.
-		err = c.processRequests(stream)
+		err = fn()
 		if err != nil && !c.handleProcessRequestError(ctx, streamURL, err) {
 			return
 		}
 	}
+}
+
+func (c *Cli) sendToPD(ctx context.Context) (processFn, string, bool) {
+	allowFollowerHandle := c.option.GetEnableFollowerHandle()
+	// Check whether allow the follower to handle this batch of requests.
+	if allowFollowerHandle {
+		// We need to ensure all requests in a same batch allow to be handled by the follower.
+		// IMPROVE: separate into the follower and leader handle batches.
+		c.batchController.IterCollectedRequests(func(req *Request) bool {
+			if !req.options.AllowFollowerHandle {
+				allowFollowerHandle = false
+				return false
+			}
+			return true
+		})
+	}
+
+	var connectionCtx *cctx.ConnectionCtx[pdpb.PD_QueryRegionClient]
+	if allowFollowerHandle {
+		connectionCtx = c.conCtxMgr.RandomlyPick()
+	} else {
+		connectionCtx = c.conCtxMgr.GetConnectionCtx(c.getLeaderURL())
+	}
+	if connectionCtx == nil {
+		log.Info("[router] router stream connection is not ready")
+		c.updateConnection(ctx)
+		return nil, "", true
+	}
+	// Check if the stream connection is canceled.
+	select {
+	case <-connectionCtx.Ctx.Done():
+		log.Info("[router] router stream connection is canceled", zap.String("stream-url", connectionCtx.StreamURL))
+		c.mcsConCtxMgr.Release(connectionCtx.StreamURL)
+		return nil, "", true
+	default:
+	}
+	return func() error {
+		return c.processRequests(connectionCtx.Stream)
+	}, connectionCtx.StreamURL, false
+}
+
+type processFn func() error
+
+// sendToMcs returns the stream function, stream url
+func (c *Cli) sendToMcs(ctx context.Context) (processFn, string) {
+	allowRouterServiceHandle := c.mcsConCtxMgr.Size() > 0
+	if allowRouterServiceHandle {
+		// We need to ensure all requests in a same batch allow to be handled by the router service.
+		c.batchController.IterCollectedRequests(func(req *Request) bool {
+			if !req.options.AllowRouterServiceHandle {
+				allowRouterServiceHandle = false
+				return false
+			}
+			return true
+		})
+	}
+	allowRouterServiceHandle = allowRouterServiceHandle && c.mcsConCtxMgr.Size() > 0
+	if !allowRouterServiceHandle {
+		return nil, ""
+	}
+	connectionCtx := c.mcsConCtxMgr.RandomlyPick()
+	if connectionCtx == nil {
+		log.Info("[router] router service stream connection is not ready")
+		c.updateMcsServiceConnection(ctx)
+		return nil, ""
+	}
+	streamCtx, streamURL, stream := connectionCtx.Ctx, connectionCtx.StreamURL, connectionCtx.Stream
+	select {
+	case <-streamCtx.Done():
+		log.Info("[router] router service stream connection is canceled", zap.String("stream-url", streamURL))
+		c.mcsConCtxMgr.Release(streamURL)
+		return nil, ""
+	default:
+	}
+	return func() error {
+		return c.processRouterRequests(stream)
+	}, streamURL
+}
+
+type recvFn func() (*pdpb.QueryRegionResponse, error)
+type sendFn func(*pdpb.QueryRegionRequest) error
+
+func (c *Cli) processRouterRequests(stream routerpb.Router_QueryRegionClient) error {
+	return c.processRequestsInner(stream.Send, stream.Recv)
 }
 
 func (c *Cli) processRequests(stream pdpb.PD_QueryRegionClient) error {
@@ -659,6 +779,84 @@ func (c *Cli) processRequests(stream pdpb.PD_QueryRegionClient) error {
 	return nil
 }
 
+func (c *Cli) processRequestsInner(send sendFn, recv recvFn) error {
+	var (
+		requests     = c.batchController.GetCollectedRequests()
+		traceRegions = make([]*trace.Region, 0, len(requests))
+		spans        = make([]opentracing.Span, 0, len(requests))
+	)
+	for _, req := range requests {
+		traceRegions = append(traceRegions, trace.StartRegion(req.requestCtx, "pdclient.regionReqSend"))
+		if span := opentracing.SpanFromContext(req.requestCtx); span != nil && span.Tracer() != nil {
+			spans = append(spans, span.Tracer().StartSpan("pdclient.processRegionRequests", opentracing.ChildOf(span.Context())))
+		}
+	}
+	defer func() {
+		for i := range spans {
+			spans[i].Finish()
+		}
+		for i := range traceRegions {
+			traceRegions[i].End()
+		}
+	}()
+
+	queryReq := &pdpb.QueryRegionRequest{
+		Header: &pdpb.RequestHeader{
+			ClusterId: c.svcDiscovery.GetClusterID(),
+		},
+		Keys:     make([][]byte, 0, len(requests)),
+		PrevKeys: make([][]byte, 0, len(requests)),
+		Ids:      make([]uint64, 0, len(requests)),
+	}
+	for _, req := range requests {
+		if !queryReq.NeedBuckets && req.options.NeedBuckets {
+			queryReq.NeedBuckets = true
+		}
+		if req.key != nil {
+			queryReq.Keys = append(queryReq.Keys, req.key)
+		} else if req.prevKey != nil {
+			queryReq.PrevKeys = append(queryReq.PrevKeys, req.prevKey)
+		} else if req.id != 0 {
+			queryReq.Ids = append(queryReq.Ids, req.id)
+		} else {
+			panic("invalid region query request received")
+		}
+	}
+	start := time.Now()
+	err := send(queryReq)
+	if err != nil {
+		return err
+	}
+	metrics.QueryRegionBatchSendLatency.Observe(
+		time.Since(
+			c.batchController.GetExtraBatchingStartTime(),
+		).Seconds(),
+	)
+	resp, err := recv()
+	if err != nil {
+		metrics.RequestFailedDurationQueryRegion.Observe(time.Since(start).Seconds())
+		return err
+	}
+	metrics.RequestDurationQueryRegion.Observe(time.Since(start).Seconds())
+	metrics.QueryRegionBatchSizeTotal.Observe(float64(len(requests)))
+	// Currently, header errors can occur due to an unready PD leader or follower,
+	// resulting in either a `NOT_BOOTSTRAPPED` or `REGION_NOT_FOUND` error.
+	if headerErr := resp.GetHeader().GetError(); headerErr != nil {
+		return errors.New(headerErr.String())
+	}
+	if keysLen := len(queryReq.Keys); keysLen > 0 {
+		metrics.QueryRegionBatchSizeByKeys.Observe(float64(keysLen))
+	}
+	if prevKeysLen := len(queryReq.PrevKeys); prevKeysLen > 0 {
+		metrics.QueryRegionBatchSizeByPrevKeys.Observe(float64(prevKeysLen))
+	}
+	if idsLen := len(queryReq.Ids); idsLen > 0 {
+		metrics.QueryRegionBatchSizeByIDs.Observe(float64(idsLen))
+	}
+	c.doneCollectedRequests(resp)
+	return nil
+}
+
 func (c *Cli) handleProcessRequestError(
 	ctx context.Context,
 	streamURL string,
@@ -677,6 +875,8 @@ func (c *Cli) handleProcessRequestError(
 
 	// Delete the stream connection context.
 	c.conCtxMgr.Release(streamURL)
+	c.mcsConCtxMgr.Release(streamURL)
+
 	if errs.IsLeaderChange(err) {
 		// If the leader changes, we better call `CheckMemberChanged` blockingly to
 		// ensure the next round of router requests can be sent to the new leader.
@@ -690,6 +890,7 @@ func (c *Cli) handleProcessRequestError(
 	} else {
 		// For other errors, we can just schedule a member change check asynchronously.
 		c.svcDiscovery.ScheduleCheckMemberChanged()
+		c.mcsDiscovery.ScheduleCheckMemberChanged()
 	}
 
 	return true
---
+++ client/servicediscovery/router_service_discovery.go
@@ -0,0 +1,327 @@
+// Copyright 2025 TiKV Project Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package servicediscovery
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"reflect"
+	"sort"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+
+	"github.com/pingcap/log"
+
+	"github.com/tikv/pd/client/clients/metastorage"
+	"github.com/tikv/pd/client/errs"
+	"github.com/tikv/pd/client/opt"
+	"github.com/tikv/pd/client/pkg/retry"
+	"github.com/tikv/pd/client/pkg/utils/grpcutil"
+	"github.com/tikv/pd/client/pkg/utils/tlsutil"
+)
+
+const (
+	// "/ms/{cluster_id}/router/registry/"
+	servicePathFormat = "/ms/%d/router/registry/"
+)
+
+var _ ServiceDiscovery = (*routerServiceDiscovery)(nil)
+
+// routerServiceDiscovery implements ServiceDiscovery for the router.
+type routerServiceDiscovery struct {
+	ServiceDiscovery
+
+	metaCli metastorage.Client
+	// defaultDiscoveryKey is the etcd path used for discovering the serving endpoints of
+	// the default keyspace group
+	defaultDiscoveryKey string
+
+	sortedUrls atomic.Value // Store as []string
+	balancer   *serviceBalancer
+	nodes      sync.Map // Store as map[string]serviceClient
+
+	// URL -> a gRPC connection
+	clientConns sync.Map // Store as map[string]*grpc.ClientConn
+
+	callbacks *serviceCallbacks
+
+	checkMembershipCh chan struct{}
+
+	parentCtx context.Context
+	ctx       context.Context
+	cancel    context.CancelFunc
+	wg        sync.WaitGroup
+
+	// Client option.
+	option *opt.Option
+	tlsCfg *tls.Config
+}
+
+// GetServiceURLs returns the URLs of the router service.
+func (r *routerServiceDiscovery) GetServiceURLs() []string {
+	if r.sortedUrls.Load() == nil {
+		return nil
+	}
+	return r.sortedUrls.Load().([]string)
+}
+
+// GetClientConns returns the gRPC client connections to the router service.
+func (r *routerServiceDiscovery) GetClientConns() *sync.Map {
+	return &r.clientConns
+}
+
+// GetServiceClient returns the ServiceClient of the router service.
+func (r *routerServiceDiscovery) GetServiceClient() ServiceClient {
+	return r.GetServiceClientByKind(ForwardAPIKind)
+}
+
+// GetServiceClientByKind returns the ServiceClient of the router service.
+func (r *routerServiceDiscovery) GetServiceClientByKind(_ APIKind) ServiceClient {
+	client := r.balancer.get()
+	if client == nil {
+		return nil
+	}
+	return client
+}
+
+// GetOrCreateGRPCConn creates a gRPC connection to the router service.
+func (r *routerServiceDiscovery) GetOrCreateGRPCConn(url string) (*grpc.ClientConn, error) {
+	return grpcutil.GetOrCreateGRPCConn(r.ctx, &r.clientConns, url, r.tlsCfg, r.option.GRPCDialOptions...)
+}
+
+// ScheduleCheckMemberChanged schedules a check for member changes.
+func (r *routerServiceDiscovery) ScheduleCheckMemberChanged() {
+	select {
+	case <-r.parentCtx.Done():
+		log.Info("[router service] service discovery is shutting down")
+	case r.checkMembershipCh <- struct{}{}:
+	default:
+	}
+}
+
+// CheckMemberChanged checks if there is any membership change among the members of the router service.
+func (r *routerServiceDiscovery) CheckMemberChanged() error {
+	if err := retry.WithConfig(r.ctx, r.updateMember); err != nil {
+		log.Warn("[router service] failed to update member", errs.ZapError(err))
+		return err
+	}
+	return nil
+}
+
+// AddMembersChangedCallback adds a callback to the router service discovery.
+func (r *routerServiceDiscovery) AddMembersChangedCallback(cb func()) {
+	r.callbacks.addMembersChangedCallback(cb)
+}
+
+// NewRouterServiceDiscovery returns a new client-side service discovery for the independent router service.
+func NewRouterServiceDiscovery(
+	ctx context.Context, metaCli metastorage.Client, serviceDiscovery ServiceDiscovery,
+	tlsCfg *tls.Config, option *opt.Option,
+) ServiceDiscovery {
+	balancer := newServiceBalancer(emptyErrorFn)
+	c := &routerServiceDiscovery{
+		parentCtx:         ctx,
+		ServiceDiscovery:  serviceDiscovery,
+		metaCli:           metaCli,
+		tlsCfg:            tlsCfg,
+		option:            option,
+		checkMembershipCh: make(chan struct{}, 1),
+		balancer:          balancer,
+		callbacks:         newServiceCallbacks(),
+	}
+	// Start with the default keyspace group. The actual keyspace group, to which the keyspace belongs,
+	// will be discovered later.
+	c.defaultDiscoveryKey = fmt.Sprintf(servicePathFormat, c.GetClusterID())
+
+	log.Info("[router service] created router service discovery",
+		zap.Uint64("cluster-id", c.GetClusterID()),
+		zap.Uint32("keyspace-id", c.GetKeyspaceID()),
+		zap.String("default-discovery-key", c.defaultDiscoveryKey))
+	return c
+}
+
+// Init initialize the concrete client underlying
+func (r *routerServiceDiscovery) Init() error {
+	log.Info("[router service] initializing router service discovery",
+		zap.Int("max-retry-times", r.option.MaxRetryTimes),
+		zap.Duration("retry-interval", initRetryInterval))
+	r.ctx, r.cancel = context.WithCancel(r.parentCtx)
+	if err := r.CheckMemberChanged(); err != nil {
+		// Initial check failed, log and continue to run the background loop.
+		log.Warn("[router service] failed to initialize router service discovery", zap.Error(err))
+	}
+	r.wg.Add(2)
+	go r.startCheckMemberLoop()
+	go r.nodeHealthCheckLoop()
+	return nil
+}
+
+func (r *routerServiceDiscovery) updateMember() error {
+	urls, err := metastorage.DiscoveryMSAddrs(r.ctx, r.defaultDiscoveryKey, r.metaCli)
+	if err != nil {
+		return err
+	}
+	if len(urls) == 0 {
+		log.Info("[router service] discovery returned no router service endpoints, maybe not deployed yet")
+		return nil
+	}
+	changed := r.nodesChanged(urls)
+	if !changed {
+		return nil
+	}
+	r.updateURLs(urls)
+	r.updateNodes(urls)
+	return nil
+}
+
+func (r *routerServiceDiscovery) updateNodes(urls []string) {
+	nodes := make(map[string]*serviceClient)
+	r.nodes.Range(func(key, value any) bool {
+		nodes[key.(string)] = value.(*serviceClient)
+		return true
+	})
+
+	for _, url := range urls {
+		if len(url) > 0 {
+			newURL := tlsutil.PickMatchedURL([]string{url}, r.tlsCfg)
+			if client, ok := r.nodes.Load(newURL); ok {
+				if client.(*serviceClient).GetClientConn() == nil {
+					conn, err := r.GetOrCreateGRPCConn(url)
+					if err != nil || conn == nil {
+						log.Warn("[router service] failed to connect router service",
+							zap.String("new-url", newURL), errs.ZapError(err))
+						continue
+					}
+					node := newPDServiceClient(url, r.GetServingURL(), conn, false)
+					r.nodes.Store(url, node)
+				}
+			} else {
+				conn, err := r.GetOrCreateGRPCConn(url)
+				if err != nil || conn == nil {
+					log.Warn("[router service] failed to connect follower",
+						zap.String("url", url), errs.ZapError(err))
+				}
+				nodeClient := newPDServiceClient(url, r.GetServingURL(), conn, false)
+				r.nodes.LoadOrStore(url, nodeClient)
+			}
+		}
+	}
+	clients := make([]ServiceClient, 0, len(urls))
+	r.nodes.Range(func(_, value any) bool {
+		clients = append(clients, value.(*serviceClient))
+		return true
+	})
+	log.Info("[router service] updating nodes succeeded",
+		zap.Strings("urls", urls),
+		zap.Int("clients-length", len(clients)))
+	r.balancer.set(clients)
+}
+
+func (r *routerServiceDiscovery) nodesChanged(urls []string) bool {
+	sort.Strings(urls)
+	oldURLs := r.GetServiceURLs()
+	return !reflect.DeepEqual(oldURLs, urls)
+}
+
+func (r *routerServiceDiscovery) updateURLs(urls []string) {
+	sort.Strings(urls)
+	r.sortedUrls.Store(urls)
+	// Run callbacks to reflect the membership changes in the leader and followers.
+	r.callbacks.onMembersChanged()
+	log.Info("[router service] update member sorted urls", zap.Strings("new-sorted-urls", urls))
+}
+
+func (r *routerServiceDiscovery) startCheckMemberLoop() {
+	defer r.wg.Done()
+
+	ctx, cancel := context.WithCancel(r.ctx)
+	defer cancel()
+	ticker := time.NewTicker(MemberUpdateInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-r.checkMembershipCh:
+		case <-ticker.C:
+		case <-ctx.Done():
+			log.Info("[router service] exit check member loop")
+			return
+		}
+		// Make sure queryRetryMaxTimes * queryRetryInterval is far less than memberUpdateInterval,
+		// so that we can speed up the process of router service discovery when failover happens on the
+		// router service side and also ensures it won't call updateMember too frequently during normal time.
+		if err := r.CheckMemberChanged(); err != nil {
+			log.Warn("[router service] failed to update member", errs.ZapError(err))
+		}
+	}
+}
+
+// Close releases all resources
+func (r *routerServiceDiscovery) Close() {
+	log.Info("[router service] closing router service discovery")
+	if r.cancel != nil {
+		r.cancel()
+	}
+
+	r.clientConns.Range(func(key, cc any) bool {
+		if err := cc.(*grpc.ClientConn).Close(); err != nil {
+			log.Warn("[router service] failed to close gRPC clientConn", errs.ZapError(errs.ErrCloseGRPCConn, err))
+		}
+		r.clientConns.Delete(key)
+		return true
+	})
+	r.sortedUrls.Store([]string{})
+	r.balancer.clean()
+	r.nodes.Clear()
+	r.wg.Wait()
+	log.Info("[router service] is closed")
+}
+
+func (r *routerServiceDiscovery) nodeHealthCheckLoop() {
+	defer r.wg.Done()
+
+	nodeCheckLoopCtx, nodeCheckLoopCancel := context.WithCancel(r.ctx)
+	defer nodeCheckLoopCancel()
+
+	ticker := time.NewTicker(MemberHealthCheckInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-r.ctx.Done():
+			log.Info("[router service] exit health check member loop")
+			return
+		case <-ticker.C:
+			r.checkNodeHealth(nodeCheckLoopCtx)
+		}
+	}
+}
+
+func (r *routerServiceDiscovery) checkNodeHealth(ctx context.Context) {
+	r.nodes.Range(func(_, value any) bool {
+		// To ensure that the leader's healthy check is not delayed, shorten the duration.
+		ctx, cancel := context.WithTimeout(ctx, MemberHealthCheckInterval/3)
+		defer cancel()
+		serviceClient := value.(*serviceClient)
+		serviceClient.checkNetworkAvailable(ctx)
+		return true
+	})
+	r.balancer.check()
+}
