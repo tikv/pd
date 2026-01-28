7a66bfb0214b7332f9d09524e39576076fcad600
tongjian <1045931706@qq.com>
2026-01-28 15:04:31 +0800

    core: region heartbeat with bucket meta (#10120)
    close tikv/pd#10117
    
    Signed-off-by: tongjian <1045931706@qq.com>
    Signed-off-by: bufferflies <1045931706@qq.com>

--- pkg/syncer/client.go
+++ pkg/syncer/client.go
@@ -248,6 +248,7 @@ func (s *RegionSyncer) StartSyncWithLeader(addr string) {
 					if hasBuckets {
 						if old := origin.GetBuckets(); buckets[i].GetVersion() > old.GetVersion() {
 							region.UpdateBuckets(buckets[i], old)
+							region.SetBucketMeta(buckets[i])
 						}
 					}
 					if saveKV {
