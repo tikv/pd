135e89996ceaca472f9af30a25291980e75b3d5a
tongjian <1045931706@qq.com>
2026-02-24 15:26:30 +0800

    go runtime (#484)
    Signed-off-by: bufferflies <1045931706@qq.com>

--- pkg/mcs/utils/util.go
+++ pkg/mcs/utils/util.go
@@ -28,6 +28,7 @@ import (
 	"github.com/pingcap/kvproto/pkg/diagnosticspb"
 	"github.com/pingcap/log"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/soheilhy/cmux"
 	"github.com/tikv/pd/pkg/errs"
@@ -49,6 +50,10 @@ import (
 
 // PromHandler is a handler to get prometheus metrics.
 func PromHandler() gin.HandlerFunc {
+	prometheus.DefaultRegisterer.Unregister(collectors.NewGoCollector())
+	if err := prometheus.Register(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(collectors.MetricsGC, collectors.MetricsMemory, collectors.MetricsScheduler))); err != nil {
+		log.Warn("go runtime collectors have already registered", errs.ZapError(err))
+	}
 	return func(c *gin.Context) {
 		// register promhttp.HandlerOpts DisableCompression
 		promhttp.InstrumentMetricHandler(prometheus.DefaultRegisterer, promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{
