97e0827efecaff33fefb35fab75d20a2fe6e4cd8
ystaticy <y_static_y@sina.com>
2026-02-28 14:28:34 +0800

    fix logs
    Signed-off-by: ystaticy <y_static_y@sina.com>

--- pkg/keyspace/metrics.go
+++ pkg/keyspace/metrics.go
@@ -120,12 +120,10 @@ func (t *createKeyspaceTracer) onStepFinished(step string) {
 	duration := now.Sub(t.lastCheckTime)
 	t.lastCheckTime = now
 	createKeyspaceStepDuration.WithLabelValues(step).Observe(duration.Seconds())
-	if duration > time.Second {
-		log.Warn("[create-keyspace] step slow",
-			zap.String("step", step),
-			zap.Uint32("keyspace-id", t.keyspaceID),
-			zap.String("keyspace-name", t.keyspaceName),
-			zap.Duration("duration", duration),
-		)
-	}
+	log.Debug("[create-keyspace] step completed",
+		zap.String("step", step),
+		zap.Uint32("keyspace-id", t.keyspaceID),
+		zap.String("keyspace-name", t.keyspaceName),
+		zap.Duration("duration", duration),
+	)
 }
