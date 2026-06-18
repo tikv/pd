697ac219fb9292b20135acee299176cb0e670a53
Ryan Leung <rleungx@gmail.com>
2026-06-18 14:19:11 +0800

    tests: move keyspace load by ID test
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- tests/server/keyspace/keyspace_service_test.go
+++ tests/server/keyspace/keyspace_service_test.go
@@ -12,57 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package server_test
+package keyspace
 
 import (
 	"context"
-	"testing"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/kvproto/pkg/keyspacepb"
 	"github.com/pingcap/kvproto/pkg/pdpb"
 
-	"github.com/tikv/pd/pkg/keyspace"
+	keyspacepkg "github.com/tikv/pd/pkg/keyspace"
 	"github.com/tikv/pd/pkg/utils/testutil"
 	"github.com/tikv/pd/server"
-	"github.com/tikv/pd/server/config"
-	"github.com/tikv/pd/tests"
 )
 
-func TestLoadKeyspaceByID(t *testing.T) {
-	re := require.New(t)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cluster, err := tests.NewTestCluster(ctx, 1, func(conf *config.Config, _ string) {
-		conf.Keyspace.WaitRegionSplit = false
-	})
-	re.NoError(err)
-	defer cluster.Destroy()
-	re.NoError(cluster.RunInitialServers())
-	re.NotEmpty(cluster.WaitLeader())
-
-	leader := cluster.GetLeaderServer()
-	re.NoError(leader.BootstrapCluster())
+func (suite *keyspaceTestSuite) TestLoadKeyspaceByIDGRPC() {
+	re := suite.Require()
 	service := &server.KeyspaceServer{
-		GrpcServer: &server.GrpcServer{Server: leader.GetServer()},
+		GrpcServer: &server.GrpcServer{Server: suite.server.GetServer()},
 	}
+	ctx := context.Background()
 
-	created, err := leader.GetKeyspaceManager().CreateKeyspace(&keyspace.CreateKeyspaceRequest{
+	created, err := suite.manager.CreateKeyspace(&keyspacepkg.CreateKeyspaceRequest{
 		Name: "grpc_load_by_id",
 	})
 	re.NoError(err)
 
 	_, err = service.LoadKeyspaceByID(ctx, &keyspacepb.LoadKeyspaceByIDRequest{
-		Header: testutil.NewRequestHeader(leader.GetClusterID() + 1),
+		Header: testutil.NewRequestHeader(suite.server.GetClusterID() + 1),
 		Id:     created.GetId(),
 	})
 	re.Error(err)
 
 	resp, err := service.LoadKeyspaceByID(ctx, &keyspacepb.LoadKeyspaceByIDRequest{
-		Header: testutil.NewRequestHeader(leader.GetClusterID()),
+		Header: testutil.NewRequestHeader(suite.server.GetClusterID()),
 		Id:     created.GetId() + 1000,
 	})
 	re.NoError(err)
@@ -70,7 +53,7 @@ func TestLoadKeyspaceByID(t *testing.T) {
 	re.Nil(resp.GetKeyspace())
 
 	resp, err = service.LoadKeyspaceByID(ctx, &keyspacepb.LoadKeyspaceByIDRequest{
-		Header: testutil.NewRequestHeader(leader.GetClusterID()),
+		Header: testutil.NewRequestHeader(suite.server.GetClusterID()),
 		Id:     created.GetId(),
 	})
 	re.NoError(err)
@@ -82,7 +65,7 @@ func TestLoadKeyspaceByID(t *testing.T) {
 		re.NoError(failpoint.Disable("github.com/tikv/pd/server/skipKeyspaceRegionCheck"))
 	}()
 	resp, err = service.LoadKeyspaceByID(ctx, &keyspacepb.LoadKeyspaceByIDRequest{
-		Header: testutil.NewRequestHeader(leader.GetClusterID()),
+		Header: testutil.NewRequestHeader(suite.server.GetClusterID()),
 		Id:     created.GetId(),
 	})
 	re.NoError(err)
