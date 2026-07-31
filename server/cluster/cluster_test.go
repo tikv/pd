1bacbb04cb975887e0031e37174abe56e59a49bf
lhy1024 <19542290+lhy1024@users.noreply.github.com>
2026-07-31 10:51:46 +0800

    server/cluster: reuse store snapshot during state checks
    Pass the store snapshot collected by checkStores into each store state check instead of rebuilding the complete store slice for every preparing store.
    
    Ref #9574
    
    Signed-off-by: lhy1024 <19542290+lhy1024@users.noreply.github.com>

--- server/cluster/cluster_test.go
+++ server/cluster/cluster_test.go
@@ -4230,9 +4230,10 @@ func TestCheckStoresUpCountWithLowSpace(t *testing.T) {
 	re.Equal(int(storeCount), cluster.GetStoreCount())
 
 	upStoreCount := 0
+	stores := cluster.GetStores()
 	regionSizes := newRegionSizeCache(cluster.GetRegionSizeByRange)
-	for _, s := range cluster.GetStores() {
-		isUp, _ := cluster.checkStore(s.GetID(), regionSizes)
+	for _, s := range stores {
+		isUp, _ := cluster.checkStore(s.GetID(), stores, regionSizes)
 		if isUp {
 			upStoreCount++
 		}
@@ -4251,9 +4252,10 @@ func TestCheckStoresUpCountWithLowSpace(t *testing.T) {
 		}
 		re.NoError(cluster.HandleStoreHeartbeat(req, resp))
 	}
+	stores = cluster.GetStores()
 	regionSizes = newRegionSizeCache(cluster.GetRegionSizeByRange)
-	for _, s := range cluster.GetStores() {
-		isUp, _ := cluster.checkStore(s.GetID(), regionSizes)
+	for _, s := range stores {
+		isUp, _ := cluster.checkStore(s.GetID(), stores, regionSizes)
 		if isUp {
 			upStoreCount++
 		}
