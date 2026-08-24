1a3fe5fb66718e091e1177223af55ea120723dc7
bufferflies <1045931706@qq.com>
2026-08-24 09:14:13 +0200

    mcs/scheduling: prevent scheduler update notifier from blocking (#10869)
    This backport (#11171) of #10900 adds scheduleMu and holds it while
    notifying schedulers of an update. On this release branch,
    tryNotifySchedulersUpdating still does a blocking channel send, so a
    full/stale notifier can block that send while scheduleMu is held,
    stalling every other schedule-config writer (including watched
    default-store-limit updates). Port #10869's fix (already merged on
    master; release-8.5 backport open as #10955): make the notification
    non-blocking and clear a primary's notifier on step-down.
    
    Signed-off-by: bufferflies <1045931706@qq.com>

--- pkg/mcs/scheduling/server/config/config_test.go
+++ pkg/mcs/scheduling/server/config/config_test.go
@@ -17,12 +17,79 @@ package config
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 
 	"github.com/tikv/pd/pkg/core/storelimit"
 	sc "github.com/tikv/pd/pkg/schedule/config"
+	"github.com/tikv/pd/pkg/schedule/types"
+	"github.com/tikv/pd/pkg/utils/testutil"
 )
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m, testutil.LeakOptions...)
+}
+
+func TestSetScheduleConfigDoesNotBlockWhenSchedulersUpdatingNotifierIsFull(t *testing.T) {
+	re := require.New(t)
+	persistConfig := NewPersistConfig(&Config{}, nil)
+	notifier := make(chan struct{}, 1)
+	notifier <- struct{}{}
+	persistConfig.SetSchedulersUpdatingNotifier(notifier)
+
+	done := make(chan struct{})
+	go func() {
+		persistConfig.SetScheduleConfig(newScheduleConfig(types.BalanceLeaderScheduler))
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+		re.Fail("SetScheduleConfig should not block on a full schedulers updating notifier")
+	}
+}
+
+func TestClearSchedulersUpdatingNotifier(t *testing.T) {
+	re := require.New(t)
+	persistConfig := NewPersistConfig(&Config{}, nil)
+	oldNotifier := make(chan struct{}, 1)
+	newNotifier := make(chan struct{}, 1)
+
+	persistConfig.SetSchedulersUpdatingNotifier(oldNotifier)
+	persistConfig.SetSchedulersUpdatingNotifier(newNotifier)
+	persistConfig.ClearSchedulersUpdatingNotifier(oldNotifier)
+	persistConfig.SetScheduleConfig(newScheduleConfig(types.BalanceLeaderScheduler))
+
+	select {
+	case <-oldNotifier:
+		re.Fail("old notifier should not receive scheduler update after a newer notifier is registered")
+	default:
+	}
+	select {
+	case <-newNotifier:
+	case <-time.After(time.Second):
+		re.Fail("new notifier should receive scheduler update")
+	}
+
+	persistConfig.ClearSchedulersUpdatingNotifier(newNotifier)
+	persistConfig.SetScheduleConfig(newScheduleConfig(types.BalanceRegionScheduler))
+	select {
+	case <-newNotifier:
+		re.Fail("cleared notifier should not receive scheduler update")
+	default:
+	}
+}
+
+func newScheduleConfig(schedulerType types.CheckerSchedulerType) *sc.ScheduleConfig {
+	return &sc.ScheduleConfig{
+		Schedulers: []sc.SchedulerConfig{
+			{Type: types.SchedulerTypeCompatibleMap[schedulerType]},
+		},
+	}
+}
 
 func TestPersistConfigDefaultStoreLimit(t *testing.T) {
 	re := require.New(t)
